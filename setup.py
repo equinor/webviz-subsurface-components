5d95e40505e07c82f934e6d5cebbfe13f769d935
Anders Fredrik Kiær <akia@statoil.com>
2019-04-04 18:18:39 +0200

    Add pypi deployment

--- setup.py
+++ setup.py
@@ -1,20 +1,49 @@
-import json
 import os
+import json
 from setuptools import setup
 
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 with open(os.path.join('webviz_subsurface_components', 'package.json')) as f:
     package = json.load(f)
 
-package_name = package["name"].replace(" ", "_").replace("-", "_")
+package_name = package['name'].replace(' ', '_').replace('-', '_')
+
+tests_require = [
+  'chromedriver-binary',
+  'dash>=0.38.0',
+  'ipdb',
+  'percy',
+  'selenium',
+  'flake8',
+  'pylint',
+  'pytest-dash>=2.1.1'
+]
 
 setup(
     name=package_name,
-    version=package["version"],
     author=package['author'],
     packages=[package_name],
     include_package_data=True,
     license=package['license'],
-    description=package['description'] if 'description' in package else package_name,
-    install_requires=[]
+    description=package['description'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/equinor/webviz-subsurface-components',
+    install_requires=['dash>=0.38.0'],
+    tests_require=tests_require,
+    extras_require={'tests': tests_require},
+    setup_requires=['setuptools_scm>=3.2.0'],
+    use_scm_version=True,
+    classifiers=[
+     'Programming Language :: Python :: 3',
+     'Operating System :: OS Independent',
+     'Natural Language :: English',
+     'Topic :: Multimedia :: Graphics',
+     'Topic :: Scientific/Engineering',
+     'Topic :: Scientific/Engineering :: Visualization',
+     'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)'
+    ]
 )
--- webviz_subsurface_components/__init__.py
+++ webviz_subsurface_components/__init__.py
@@ -4,11 +4,19 @@ import os as _os
 import sys as _sys
 import json
 
+from pkg_resources import get_distribution, DistributionNotFound
+
 import dash as _dash
 
 # noinspection PyUnresolvedReferences
 from ._imports_ import *
 from ._imports_ import __all__
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
 
 if not hasattr(_dash, 'development'):
     print('Dash was not successfully imported. '
@@ -22,7 +30,6 @@ with open(_filepath) as f:
     package = json.load(f)
 
 package_name = package['name'].replace(' ', '_').replace('-', '_')
-__version__ = package['version']
 
 _current_path = _os.path.dirname(_os.path.abspath(__file__))
 
@@ -33,13 +40,11 @@ _js_dist = [
     {
         'relative_package_path': 'webviz_subsurface_components.min.js',
         'dev_package_path': 'webviz_subsurface_components.dev.js',
-        
         'namespace': package_name
     }
 ]
 
 _css_dist = []
-
 
 for _component in __all__:
     setattr(locals()[_component], '_js_dist', _js_dist)
