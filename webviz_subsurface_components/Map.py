80c480fa773bf6bf1a28a4344d661c1d06de4eaa
Anders Fredrik Kiær <akia@statoil.com>
2019-05-29 14:17:39 +0200

    Build

--- webviz_subsurface_components/Map.py
+++ webviz_subsurface_components/Map.py
@@ -8,23 +8,20 @@ class Map(Component):
 
 
 Keyword arguments:
-- layerNames (list; optional): The name of individual layers
-- data (string; required): The data the Map component should render.
-It should be JSON stringified before hand
 - id (string; required): The ID of this component, used to identify dash components
 in callbacks. The ID needs to be unique across all of the
 components in an app.
+- data (string; required): The data the Map component should render.
+It should be JSON stringified before hand
 - height (number; optional): The height of the Map component
-
-Available events: """
+- layerNames (list; optional): The name of individual layers"""
     @_explicitize_args
-    def __init__(self, layerNames=Component.UNDEFINED, data=Component.REQUIRED, id=Component.REQUIRED, height=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['height', 'data', 'id', 'layerNames']
+    def __init__(self, id=Component.REQUIRED, data=Component.REQUIRED, height=Component.UNDEFINED, layerNames=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'data', 'height', 'layerNames']
         self._type = 'Map'
         self._namespace = 'webviz_subsurface_components'
         self._valid_wildcard_attributes =            []
-        self.available_events = []
-        self.available_properties = ['height', 'data', 'id', 'layerNames']
+        self.available_properties = ['id', 'data', 'height', 'layerNames']
         self.available_wildcard_properties =            []
 
         _explicit_args = kwargs.pop('_explicit_args')
@@ -32,31 +29,8 @@ Available events: """
         _locals.update(kwargs)  # For wildcard attrs
         args = {k: _locals[k] for k in _explicit_args if k != 'children'}
 
-        for k in [u'data', u'id']:
+        for k in ['id', 'data']:
             if k not in args:
                 raise TypeError(
                     'Required argument `' + k + '` was not specified.')
         super(Map, self).__init__(**args)
-
-    def __repr__(self):
-        if(any(getattr(self, c, None) is not None
-               for c in self._prop_names
-               if c is not self._prop_names[0])
-           or any(getattr(self, c, None) is not None
-                  for c in self.__dict__.keys()
-                  if any(c.startswith(wc_attr)
-                  for wc_attr in self._valid_wildcard_attributes))):
-            props_string = ', '.join([c+'='+repr(getattr(self, c, None))
-                                      for c in self._prop_names
-                                      if getattr(self, c, None) is not None])
-            wilds_string = ', '.join([c+'='+repr(getattr(self, c, None))
-                                      for c in self.__dict__.keys()
-                                      if any([c.startswith(wc_attr)
-                                      for wc_attr in
-                                      self._valid_wildcard_attributes])])
-            return ('Map(' + props_string +
-                   (', ' + wilds_string if wilds_string != '' else '') + ')')
-        else:
-            return (
-                'Map(' +
-                repr(getattr(self, self._prop_names[0], None)) + ')')
--- webviz_subsurface_components/Morris.py
+++ webviz_subsurface_components/Morris.py
@@ -8,21 +8,18 @@ class Morris(Component):
 
 
 Keyword arguments:
-- output (list; optional)
-- parameter (string; optional)
 - id (string; required)
+- parameter (string; optional)
+- output (list; optional)
 - parameters (list; optional)
-- height (number; optional)
-
-Available events: """
+- height (number; optional)"""
     @_explicitize_args
-    def __init__(self, output=Component.UNDEFINED, parameter=Component.UNDEFINED, id=Component.REQUIRED, parameters=Component.UNDEFINED, height=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['output', 'parameter', 'id', 'parameters', 'height']
+    def __init__(self, id=Component.REQUIRED, parameter=Component.UNDEFINED, output=Component.UNDEFINED, parameters=Component.UNDEFINED, height=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'parameter', 'output', 'parameters', 'height']
         self._type = 'Morris'
         self._namespace = 'webviz_subsurface_components'
         self._valid_wildcard_attributes =            []
-        self.available_events = []
-        self.available_properties = ['output', 'parameter', 'id', 'parameters', 'height']
+        self.available_properties = ['id', 'parameter', 'output', 'parameters', 'height']
         self.available_wildcard_properties =            []
 
         _explicit_args = kwargs.pop('_explicit_args')
@@ -30,31 +27,8 @@ Available events: """
         _locals.update(kwargs)  # For wildcard attrs
         args = {k: _locals[k] for k in _explicit_args if k != 'children'}
 
-        for k in [u'id']:
+        for k in ['id']:
             if k not in args:
                 raise TypeError(
                     'Required argument `' + k + '` was not specified.')
         super(Morris, self).__init__(**args)
-
-    def __repr__(self):
-        if(any(getattr(self, c, None) is not None
-               for c in self._prop_names
-               if c is not self._prop_names[0])
-           or any(getattr(self, c, None) is not None
-                  for c in self.__dict__.keys()
-                  if any(c.startswith(wc_attr)
-                  for wc_attr in self._valid_wildcard_attributes))):
-            props_string = ', '.join([c+'='+repr(getattr(self, c, None))
-                                      for c in self._prop_names
-                                      if getattr(self, c, None) is not None])
-            wilds_string = ', '.join([c+'='+repr(getattr(self, c, None))
-                                      for c in self.__dict__.keys()
-                                      if any([c.startswith(wc_attr)
-                                      for wc_attr in
-                                      self._valid_wildcard_attributes])])
-            return ('Morris(' + props_string +
-                   (', ' + wilds_string if wilds_string != '' else '') + ')')
-        else:
-            return (
-                'Morris(' +
-                repr(getattr(self, self._prop_names[0], None)) + ')')
