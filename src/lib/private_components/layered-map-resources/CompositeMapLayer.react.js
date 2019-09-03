048e338b5bbd4ec2832e4b8dcba84bfe2d670292
Hans Kallekleiv <hakal@statoil.com>
2019-09-03 13:56:00 +0200

    Added missing key in map layer item list

--- src/lib/private_components/layered-map-resources/CompositeMapLayer.react.js
+++ src/lib/private_components/layered-map-resources/CompositeMapLayer.react.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 import PropTypes from 'prop-types'
 import ImageOverlayWebGL from './ImageOverlayWebGL.react'
 import Colormap from './Colormap.react'
@@ -15,72 +15,77 @@ const yx = ([x,y]) => {return [y, x]}
 
 class CompositeMapLayer extends Component {
 
-    render_tooltip(item){
+    renderTooltip(item){
         if ('tooltip' in item){
             return <Tooltip sticky={true}>{item.tooltip}</Tooltip>
         }
         return null
     }
 
+    renderItems(item, index) {
+        switch(item.type) {
+            case 'polyline':
+                return (
+                    <Polyline
+                      color={item.color}
+                      positions={item.positions.map(xy => yx(xy))}
+                      key={index}
+                    >
+                        {this.renderTooltip(item)}
+                    </Polyline>
+                )
+            case 'polygon':
+                return (
+                    <Polygon
+                      color={item.color}
+                      positions={item.positions.map(xy => yx(xy))}
+                      key={index}
+                    >
+                        {this.renderTooltip(item)}
+                    </Polygon>
+                )
+            case 'circle':
+                return (
+                    <Circle
+                      color={item.color}
+                      center={yx(item.center)}
+                      radius={item.radius}
+                      key={index}
+                    >
+                        {this.renderTooltip(item)}
+                    </Circle>
+                )
+            case 'image':
+                return (
+                    <>
+                        <ImageOverlayWebGL
+                          url={item.url}
+                          colormap={item.colormap}
+                          bounds={item.bounds.map(xy => yx(xy))}
+                          hillShading={this.props.hillShading}
+                          key={index}
+                        />
+                        { 'colormap' in item && <Colormap colormap={item.colormap} unit={item.unit} minvalue={item.minvalue} maxvalue={item.maxvalue} position='bottomleft' key={'colormap' + index} /> }
+                    </>
+                )
+            default:
+                return null
+        }
+    }
+
     render() {
         return (
             <LayerGroup>
-                {
-                    this.props.layer.data.map((item, index) => {
-                        switch(item.type) {
-                            case 'polyline':
-                                return (
-                                    <Polyline
-                                      color={item.color}
-                                      positions={item.positions.map(xy => yx(xy))}
-                                      key={index}
-                                    >
-                                        {this.render_tooltip(item)}
-                                    </Polyline>
-                                )
-                            case 'polygon':
-                                return (
-                                    <Polygon
-                                      color={item.color}
-                                      positions={item.positions.map(xy => yx(xy))}
-                                      key={index}
-                                    >
-                                        {this.render_tooltip(item)}
-                                    </Polygon>
-                                )
-                            case 'circle':
-                                return (
-                                    <Circle
-                                      color={item.color}
-                                      center={yx(item.center)}
-                                      radius={item.radius}
-                                      key={index}
-                                    >
-                                        {this.render_tooltip(item)}
-                                    </Circle>
-                                )
-                            case 'image':
-                                return (
-                                    <>
-                                        <ImageOverlayWebGL
-                                          url={item.url}
-                                          colormap={item.colormap}
-                                          bounds={item.bounds.map(xy => yx(xy))}
-                                          hillShading={this.props.hillShading}
-                                          key={index}
-                                        />
-                                        { 'colormap' in item && <Colormap colormap={item.colormap} unit={item.unit} minvalue={item.minvalue} maxvalue={item.maxvalue} position='bottomleft' key={'colormap' + index} /> }
-                                    </>
-                                )
-                            default:
-                                return null
-                        }
-                    })
-                }
+                {this.props.layer.data.map((item, index) => {
+                    return (
+                        <Fragment key={item.type}>
+                            {this.renderItems(item, index)}
+                        </Fragment>
+                    )
+                })}
             </LayerGroup>
         )
     }
-
 }
 
 CompositeMapLayer.propTypes = {
