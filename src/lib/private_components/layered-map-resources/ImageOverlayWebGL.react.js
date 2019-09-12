4d03e6500e7208ee94a613131cd08d0a9cf1c1c7
Anders Fredrik Kiær <akia@statoil.com>
2019-09-12 14:34:55 +0200

    Simplify

--- src/lib/private_components/layered-map-resources/ImageOverlayWebGL.react.js
+++ src/lib/private_components/layered-map-resources/ImageOverlayWebGL.react.js
@@ -6,23 +6,16 @@ import alter_image from './alter_image'
 
 class ImageOverlayWebGL extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {drawMethod: (canvas) => alter_image(this.props.url, this.props.colormap, this.props.hillShading, canvas)}
-    }
-
-    componentDidUpdate(prevProps) {
-        if (this.props.hillShading !== prevProps.hillShading || this.props.url !== prevProps.url || this.props.colormap !== prevProps.colormap) {
-            this.setState({drawMethod: (canvas) => alter_image(this.props.url, this.props.colormap, this.props.hillShading, canvas)})
-        }
-    }
-
     render() {
         if (typeof this.props.colormap === 'undefined'){
             return <ImageOverlay url={this.props.url} bounds={this.props.bounds} />
         }
-        return <CanvasOverlay drawMethod={this.state.drawMethod} bounds={this.props.bounds} />
+        return <CanvasOverlay
+            drawMethod={(canvas) => alter_image(this.props.url, this.props.colormap, this.props.hillShading, canvas)}
+            bounds={this.props.bounds}
+        />
     }
+
 }
 
 ImageOverlayWebGL.propTypes = {
