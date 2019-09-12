841ac344ba69b4ed04244bacf126150d0b23d4b3
Anders Fredrik Kiær <31612826+anders-kiaer@users.noreply.github.com>
2019-09-12 15:08:52 +0200

    Remove react depreciation warnings (#46)

--- src/lib/private_components/layered-map-resources/Switch.react.js
+++ src/lib/private_components/layered-map-resources/Switch.react.js
@@ -1,6 +1,6 @@
 import L from 'leaflet'
 import React from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import { withLeaflet, MapControl } from 'react-leaflet'
 import PropTypes from 'prop-types'
 import MaterialSwitch from '@material-ui/core/Switch'
@@ -12,6 +12,9 @@ class Switch extends MapControl {
     constructor(props){
         super(props)
         this.state = {checked: this.props.checked}
+
+        const { map } = this.props.leaflet
+        this.leafletElement.addTo(map)
     }
 
     handleChange(){
@@ -29,14 +32,9 @@ class Switch extends MapControl {
         return new MapInfo({ position: props.position })
     }
 
-    componentWillMount() {
-        const { map } = this.props.leaflet;
-        this.leafletElement.addTo(map);
-    }
-
     componentDidMount() {
         // Overriding default MapControl implementation. We need to do the
-        // addTo(map) call in componentWillMount in order for the portal
+        // addTo(map) call in the constructor in order for the portal
         // DOM node to be available for the render function.
     }
 
@@ -63,4 +61,4 @@ Switch.propTypes = {
     checked: PropTypes.bool
 };
 
-export default withLeaflet(Switch);
+export default withLeaflet(Switch)
