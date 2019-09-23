a46d1fa57bae197363f630370306715278dfa799
Anders Fredrik Kiær <akia@statoil.com>
2019-09-23 21:17:40 +0200

    black format

--- setup.py
+++ setup.py
@@ -3,26 +3,24 @@ import json
 from setuptools import setup
 
 
-with open('README.md', 'r') as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open(os.path.join('webviz_subsurface_components', 'package.json')) as f:
+with open(os.path.join("webviz_subsurface_components", "package.json")) as f:
     package = json.load(f)
 
-package_name = package['name'].replace(' ', '_').replace('-', '_')
+package_name = package["name"].replace(" ", "_").replace("-", "_")
 
-install_requires = [
-    'dash~=1.1'
-]
+install_requires = ["dash~=1.1"]
 
 tests_require = [
-    'chromedriver-binary>=74.0.3729.6.0',
-    'ipdb',
-    'percy',
-    'selenium',
-    'flake8',
-    'pylint',
-    'black'
+    "chromedriver-binary>=74.0.3729.6.0",
+    "ipdb",
+    "percy",
+    "selenium",
+    "flake8",
+    "pylint",
+    "black",
 ]
 
 # 'dash[testing]' to be added in tests_require when
@@ -30,30 +28,26 @@ tests_require = [
 
 setup(
     name=package_name,
-    author=package['author'],
+    author=package["author"],
     packages=[package_name],
     include_package_data=True,
-    license=package['license'],
-    description=package['description'],
+    license=package["license"],
+    description=package["description"],
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/equinor/webviz-subsurface-components',
+    long_description_content_type="text/markdown",
+    url="https://github.com/equinor/webviz-subsurface-components",
     install_requires=install_requires,
     tests_require=tests_require,
-    extras_require={
-        'tests': tests_require,
-        'dependencies': install_requires
-    },
-    setup_requires=['setuptools_scm~=3.2'],
+    extras_require={"tests": tests_require, "dependencies": install_requires},
+    setup_requires=["setuptools_scm~=3.2"],
     use_scm_version=True,
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-        'Natural Language :: English',
-        'Topic :: Multimedia :: Graphics',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Visualization',
-        'License :: OSI Approved :: '\
-        'GNU Lesser General Public License v3 (LGPLv3)'
-    ]
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "License :: OSI Approved :: " "GNU Lesser General Public License v3 (LGPLv3)",
+    ],
 )
