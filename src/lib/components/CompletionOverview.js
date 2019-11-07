bef9adf37d79d570697e3063b6f5ee182608a6c4
Anders Fredrik Kiær <akia@statoil.com>
2019-11-07 21:23:12 +0100

    Remove comment

--- src/lib/components/CompletionOverview.js
+++ src/lib/components/CompletionOverview.js
@@ -2,13 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import render_completion from "../private_components/completion_overview/render_completion";
 
-// For en gitt sortering har bronnene et gitt x-koordinat.
-//   - Initier én SVG-gruppe per brønn. Behold disse i en dictionary.
-//   - Lag også rects for hvert lagt (men som i utgangspunktet ikke vises).
-
-// For time-slider: Når flyttes over bestemt tidsintervall:
-//   - Fjern/legg til kompletteringene på tidspunktet den er på.
-//
 
 class CompletionOverview extends Component {
     constructor(props) {
