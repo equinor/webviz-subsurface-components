24dd94f99f63eab97a8381519af60df1d5c682cb
Anders Fredrik Kiær <31612826+anders-kiaer@users.noreply.github.com>
2019-11-11 15:21:05 +0100

    Comply with black also on examples and tests (#74)

--- examples/example_subsurface_map.py
+++ examples/example_subsurface_map.py
@@ -3,14 +3,15 @@ import dash
 import dash_html_components as html
 import webviz_subsurface_components
 
-with open('../src/demo/example-data/subsurface-map.json') as json_file:  
+
+with open("../src/demo/example-data/subsurface-map.json", "r") as json_file:
     data = json.load(json_file)
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(children=[
-    webviz_subsurface_components.Map(id='reek-map', data=data)
-    ])
+app.layout = html.Div(
+    children=[webviz_subsurface_components.Map(id="reek-map", data=data)]
+)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
