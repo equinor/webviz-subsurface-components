2fcb9d8e4f971b535c54c81036a8d68f077d4f4a
Anders Hallem Iversen <31648998+Andorr@users.noreply.github.com>
2020-07-27 09:54:29 +0200

    Update src/lib/components/NewLayeredMap/components/Controls/components/DrawControls.js

--- src/lib/components/NewLayeredMap/components/Controls/components/DrawControls.js
+++ src/lib/components/NewLayeredMap/components/Controls/components/DrawControls.js
@@ -40,7 +40,7 @@ class DrawControls extends Component {
 
     componentDidUpdate(prevProps) {
         if(this.props.drawPolygon !== prevProps.drawPolygon
-           || this.props.drawPolyline != prevProps.drawPolyline
+           || this.props.drawPolyline !== prevProps.drawPolyline
            || this.props.drawMarker != prevProps.drawMarker
         ) {
             this.props.map.removeControl(this.state.drawControl);
