692043c3eaa6e364b0d105f083a5a524b6a1fcd0
Havard Bjerke <havard.bjerke@emerson.com>
2021-03-10 10:34:53 +0100

    Added colors and shadow to well heads and trajectories.

--- src/lib/components/DeckGLMap/layers/wells/wellsLayer.js
+++ src/lib/components/DeckGLMap/layers/wells/wellsLayer.js
@@ -1,14 +1,32 @@
+import { CompositeLayer } from "@deck.gl/core";
 import { GeoJsonLayer } from "@deck.gl/layers";
 
-const defaultProps = {
-    pickable: true,
-    pointRadiusMinPixels: 7,
-    pointRadiusMaxPixels: 13,
-    lineWidthMinPixels: 1,
-    lineWidthMaxPixels: 2,
-};
+export default class WellsLayer extends CompositeLayer {
 
-export default class WellsLayer extends GeoJsonLayer {}
+    renderLayers() {
+        const shadow = new GeoJsonLayer({
+            id: "well-shadow",
+            data: this.props.data,
+            pickable: true,
+            pointRadiusMinPixels: 9,
+            pointRadiusMaxPixels: 15,
+            lineWidthMinPixels: 5,
+            lineWidthMaxPixels: 6,
+        });
+
+        const colored = new GeoJsonLayer({
+            data: this.props.data,
+            pickable: true,
+            pointRadiusMinPixels: 7,
+            pointRadiusMaxPixels: 13,
+            lineWidthMinPixels: 3,
+            lineWidthMaxPixels: 4,
+            getFillColor: d => d.properties.color,
+            getLineColor: d => d.properties.color,
+        });
+
+        return [shadow, colored];
+    }
+}
 
 WellsLayer.layerName = "WellsLayer";
-WellsLayer.defaultProps = defaultProps;
