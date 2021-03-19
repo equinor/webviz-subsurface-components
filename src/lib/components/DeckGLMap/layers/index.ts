f4656e7b82365f3d409df0e2cb08e18cf1241e63
hkfb <hkfbjerke@gmail.com>
2021-03-19 10:05:21 +0100

    Added layer for displaying wells as GeoJson (#313)
    * Added layer for displaying wells as GeoJson.
    
    * Replaced Emerald wells with wells from Volve project.
    
    * Added colors and shadow to well heads and trajectories.
    
    * Exposed opacity, line width and point radius properties.
    
    * Added tooltips for well names again.
    
    * Fixed javascript linting errors.
    
    * Fixed typescript error.
    
    * Made well head and trajectory outlines optional.
    
    Also fixed propagation of properties to sublayers from the wells
    composite layer.
    
    Co-authored-by: Havard Bjerke <havard.bjerke@emerson.com>
    Co-authored-by: alexandruandrei91 <57130720+alexandruandrei91@users.noreply.github.com>

--- src/lib/components/DeckGLMap/layers/index.ts
+++ src/lib/components/DeckGLMap/layers/index.ts
@@ -1,2 +1,3 @@
 export { default as ColormapLayer } from "./colormap/colormapLayer";
 export { default as Hillshading2DLayer } from "./hillshading2d/hillshading2dLayer";
+export { default as WellsLayer } from "./wells/wellsLayer";
