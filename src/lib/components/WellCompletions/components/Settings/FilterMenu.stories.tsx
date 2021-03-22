0a7035370bdaaaebb85f4f9033454221a6802e44
Yue Zhang <yue.zhang@emerson.com>
2021-03-22 22:29:15 +0000

    added redux tests and setup react component test examples

--- src/lib/components/WellCompletions/components/Settings/FilterMenu.stories.tsx
+++ src/lib/components/WellCompletions/components/Settings/FilterMenu.stories.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { exampleDataDecorator } from "../../test/storybookDataDecorator";
 import { withReduxDecorator } from "../../test/storybookReduxAddon";
 =======
 import { exampleDataDecorator } from "../../storyUtil/inputDataDecorator";
 import { withReduxDecorator } from "../../storyUtil/reduxAddon";
 >>>>>>> Add storybooks for well completions component
+=======
+import { exampleDataDecorator } from "../../test/storybookDataDecorator";
+import { withReduxDecorator } from "../../test/storybookReduxAddon";
+>>>>>>> added redux tests and setup react component test examples
 import FilterMenu from "./FilterMenu";
 
 export default {
