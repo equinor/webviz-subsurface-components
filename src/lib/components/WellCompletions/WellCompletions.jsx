ac0080f9740b82299a9d770255a60c68ac874ca5
Yue Zhang <yue.zhang@emerson.com>
2021-03-30 11:43:59 +0100

    Add input data version check

--- src/lib/components/WellCompletions/WellCompletions.jsx
+++ src/lib/components/WellCompletions/WellCompletions.jsx
@@ -4,25 +4,13 @@
  * https://github.com/plotly/dash/issues/719
  */
 
-import Ajv from "ajv";
 import PropTypes from "prop-types";
-import React, { useMemo } from "react";
+import React from "react";
 import WellCompletionComponent from "./components/WellCompletionComponent";
-
-const inputSchema = require("../../../../inputSchema/wellCompletions.json");
-const ajv = new Ajv();
 
 export const DataContext = React.createContext();
 const WellCompletions = props => {
-    const valid = useMemo(() => {
-        //check against the json schema
-        const validate = ajv.compile(inputSchema);
-        const valid = validate(props.data);
-        //If input data does not satisfy the schema
-        if (!valid) alert(JSON.stringify(validate.errors));
-        return valid;
-    }, [props.data]);
-    return valid && <WellCompletionComponent id={props.id} data={props.data} />;
+    return <WellCompletionComponent id={props.id} data={props.data} />;
 };
 
 WellCompletions.propTypes = {
