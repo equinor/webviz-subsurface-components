65366c36a0423686b9ef36307ca365e03abc1c25
hkfb <hkfbjerke@gmail.com>
2021-04-06 15:39:53 +0200

    Port wells layer to typescript. (#328)
    * Added layer for displaying wells as GeoJson.
    
    * Replaced Emerald wells with wells from Volve project.
    
    * Added colors and shadow to well heads and trajectories.
    
    * Exposed opacity, line width and point radius properties.
    
    * Added tooltips for well names again.
    
    * Fixed javascript linting errors.
    
    * Fixed typescript error.
    
    * Made well head and trajectory outlines optional.
    
    Also fixed propagation of properties to sublayers from the wells
    composite layer.
    
    * Ported wells layer from javascript to typescript.
    
    * Fix formatting.
    
    * Fixed typescript compiler warnings.
    
    * Removed wells layer javascript implementation.
    
    * Use custom data type for wells layer properties.
    
    * Refactored fill color function to lambda.
    
    * Applied eslint formatting.
    
    Co-authored-by: Havard Bjerke <havard.bjerke@emerson.com>
    Co-authored-by: alexandruandrei91 <57130720+alexandruandrei91@users.noreply.github.com>

---
+++ src/lib/components/DeckGLMap/layers/wells/wellsLayer.ts
@@ -0,0 +1,56 @@
+import { CompositeLayer } from "@deck.gl/core";
+import { CompositeLayerProps } from "@deck.gl/core/lib/composite-layer";
+import { GeoJsonLayer, GeoJsonLayerProps } from "@deck.gl/layers";
+import { RGBAColor } from "@deck.gl/core/utils/color";
+
+export interface WellsLayerProps<D> extends CompositeLayerProps<D> {
+    pointRadiusScale: number;
+    lineWidthScale: number;
+    outline: boolean;
+}
+
+interface WellDataType {
+    type: string;
+    geometry: Record<string, unknown>;
+    properties: {
+        name: string;
+        color: RGBAColor;
+    };
+}
+
+export default class WellsLayer extends CompositeLayer<
+    WellDataType,
+    WellsLayerProps<WellDataType>
+> {
+    renderLayers(): GeoJsonLayer<WellDataType>[] {
+        const properties: GeoJsonLayerProps<WellDataType> = {
+            id: "outline",
+            data: this.props.data,
+            pickable: true,
+            pointRadiusUnits: "pixels",
+            lineWidthUnits: "pixels",
+            pointRadiusScale: this.props.pointRadiusScale,
+            lineWidthScale: this.props.lineWidthScale,
+        };
+
+        const outline = new GeoJsonLayer<WellDataType>(
+            this.getSubLayerProps(properties)
+        );
+
+        properties.id = "colors";
+        properties.getFillColor = (d: WellDataType): RGBAColor =>
+            d.properties.color;
+        properties.getLineColor = properties.getFillColor;
+        properties.pointRadiusScale = (properties.pointRadiusScale || 8) - 1;
+        properties.lineWidthScale = (properties.lineWidthScale || 5) - 1;
+
+        const colors = new GeoJsonLayer<WellDataType>(
+            this.getSubLayerProps(properties)
+        );
+
+        if (this.props.outline) return [outline, colors];
+        else return [colors];
+    }
+}
+
+WellsLayer.layerName = "WellsLayer";
