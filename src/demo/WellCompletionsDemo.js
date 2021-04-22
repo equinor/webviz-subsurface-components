cae8fde9c0f5267280621f1ddbc335108276d7c1
Yue Zhang <yue.zhang@emerson.com>
2021-04-22 13:29:57 +0100

    Adjust zone name space

--- src/demo/WellCompletionsDemo.js
+++ src/demo/WellCompletionsDemo.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import WellCompletions from "../lib/components/WellCompletions";
 
 const data = require("./example-data/well-completions-kh.json");
+//const data = require("./example-data/well-completions-test.json");
 //const data = require("./example-data/well-completions-6-digits.json");
 
 class WellCompletionsDemo extends Component {
