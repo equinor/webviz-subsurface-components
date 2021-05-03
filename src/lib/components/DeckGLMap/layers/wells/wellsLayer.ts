c00c210332c05ec933fc979220aba6e5b51e6eb2
Havard Bjerke <havard.bjerke@emerson.com>
2021-05-03 15:28:01 +0200

    Use GeoJson feature instead of custom wells data type.

--- src/lib/components/DeckGLMap/layers/wells/wellsLayer.ts
+++ src/lib/components/DeckGLMap/layers/wells/wellsLayer.ts
@@ -4,20 +4,13 @@ import { GeoJsonLayer, GeoJsonLayerProps } from "@deck.gl/layers";
 import { CompositeLayerProps } from "@deck.gl/core/lib/composite-layer";
 import { RGBAColor } from "@deck.gl/core/utils/color";
 
+import { Feature } from "geojson";
+
 export interface WellsLayerProps<D> extends CompositeLayerProps<D> {
     pointRadiusScale: number;
     lineWidthScale: number;
     outline: boolean;
-    selectedFeature: string;
-}
-
-export interface WellDataType {
-    type: string;
-    geometry: Record<string, unknown>;
-    properties: {
-        name: string;
-        color: RGBAColor;
-    };
+    selectedFeature: Feature;
 }
 
 function handleClick(event) {
@@ -31,11 +24,11 @@ const defaultProps = {
 };
 
 export default class WellsLayer extends CompositeLayer<
-    WellDataType,
-    WellsLayerProps<WellDataType>
+    Feature,
+    WellsLayerProps<Feature>
 > {
-    renderLayers(): GeoJsonLayer<WellDataType>[] {
-        const properties: GeoJsonLayerProps<WellDataType> = {
+    renderLayers(): GeoJsonLayer<Feature>[] {
+        const properties: GeoJsonLayerProps<Feature> = {
             id: "outline",
             data: this.props.data,
             pickable: true,
@@ -45,18 +38,18 @@ export default class WellsLayer extends CompositeLayer<
             lineWidthScale: this.props.lineWidthScale,
         };
 
-        const outline = new GeoJsonLayer<WellDataType>(
+        const outline = new GeoJsonLayer<Feature>(
             this.getSubLayerProps(properties)
         );
 
         properties.id = "colors";
-        properties.getFillColor = (d: WellDataType): RGBAColor =>
-            d.properties.color;
+        properties.getFillColor = (d: Feature): RGBAColor =>
+            d?.properties?.color;
         properties.getLineColor = properties.getFillColor;
         properties.pointRadiusScale = (properties.pointRadiusScale || 8) - 1;
         properties.lineWidthScale = (properties.lineWidthScale || 5) - 1;
 
-        const colors = new GeoJsonLayer<WellDataType>(
+        const colors = new GeoJsonLayer<Feature>(
             this.getSubLayerProps(properties)
         );
 
@@ -64,7 +57,9 @@ export default class WellsLayer extends CompositeLayer<
         properties.data = this.props.selectedFeature;
         properties.pointRadiusScale = properties.pointRadiusScale + 2;
         properties.lineWidthScale = properties.lineWidthScale + 2;
-        const highlight = new GeoJsonLayer<WellDataType>(properties);
+        const highlight = new GeoJsonLayer<Feature>(
+            this.getSubLayerProps(properties)
+        );
 
         if (this.props.outline) return [outline, colors, highlight];
         else return [colors, highlight];
