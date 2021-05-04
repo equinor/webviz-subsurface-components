6f069604718427c38f53958b3573e5fe92292224
Yue Zhang <yue.zhang@emerson.com>
2021-05-04 14:15:02 +0100

    Turn off track if time aggregation is not on

--- src/lib/components/WellCompletions/components/Settings/TimeRangeSelector.tsx
+++ src/lib/components/WellCompletions/components/Settings/TimeRangeSelector.tsx
@@ -1,12 +1,17 @@
-import { Slider } from "@equinor/eds-core-react";
-import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import {
+    createStyles,
+    makeStyles,
+    Slider,
+    Theme,
+    // eslint-disable-next-line prettier/prettier
+    withStyles
+} from "@material-ui/core";
 import { debounce, isEqual } from "lodash";
 import React, { useCallback, useContext, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateTimeIndexRange } from "../../redux/actions";
 import { WellCompletionsState } from "../../redux/store";
 import { DataContext } from "../DataLoader";
-
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
@@ -15,6 +20,18 @@ const useStyles = makeStyles((theme: Theme) =>
         },
     })
 );
+const EdsSlider = withStyles({
+    root: {
+        color: "#007079",
+    },
+    valueLabel: {
+        top: 22,
+        "& *": {
+            background: "transparent",
+            color: "#000",
+        },
+    },
+})(Slider);
 const TimeRangeSelector: React.FC = React.memo(() => {
     const classes = useStyles();
     const data = useContext(DataContext);
@@ -29,17 +46,14 @@ const TimeRangeSelector: React.FC = React.memo(() => {
     ) as [number, number];
     const times = useMemo(() => data.timeSteps, [data]);
     // handlers
-    const outputFunction = useCallback(
-        (step: number) => (times ? times[step] : ""),
-        [times]
-    );
+    const outputFunction = useCallback((step: number) => times[step], [times]);
     const onChange = useCallback(
         debounce(
             (_, value) =>
                 dispatch(
                     updateTimeIndexRange(
                         timeAggregation === "None"
-                            ? [0, Math.min(...value)]
+                            ? [0, value]
                             : [Math.min(...value), Math.max(...value)]
                     )
                 ),
@@ -50,35 +64,25 @@ const TimeRangeSelector: React.FC = React.memo(() => {
         ),
         [dispatch, timeAggregation]
     );
-
-    //If data not loaded yet
-    if (!times) return <div />;
-
     return (
         <div className={classes.root}>
             <span>Time Steps</span>
-            {timeAggregation === "None" && (
-                <Slider
-                    value={Math.max(...timeIndexRange)}
-                    onChange={onChange}
-                    ariaLabelledby="time-step-slider-label"
-                    min={0}
-                    max={times.length - 1}
-                    step={1}
-                    outputFunction={outputFunction}
-                />
-            )}
-            {timeAggregation !== "None" && (
-                <Slider
-                    value={timeIndexRange}
-                    onChange={onChange}
-                    ariaLabelledby="time-step-slider-range-label"
-                    min={0}
-                    max={times.length - 1}
-                    step={1}
-                    outputFunction={outputFunction}
-                />
-            )}
+            <EdsSlider
+                track={timeAggregation === "None" ? false : "normal"}
+                aria-labelledby="time-step-slider-label"
+                value={
+                    timeAggregation === "None"
+                        ? Math.max(...timeIndexRange)
+                        : timeIndexRange.slice()
+                }
+                valueLabelDisplay="on"
+                onChange={onChange}
+                min={0}
+                max={times.length - 1}
+                step={1}
+                marks={true}
+                valueLabelFormat={outputFunction}
+            />
         </div>
     );
 });
