62c0a35ba5c7793d897ba6303b39a358182998cb
hkfb <hkfbjerke@gmail.com>
2021-05-04 15:48:53 +0200

    Highlight and select wells (#360)
    * Add a drawing layer and a mechanism to comunicate with the dash backend
    
    * Pin react-leaflet-draw version due to incompatibility with leaflet version
    
    * Format python code
    
    * Adds selecting wells.
    
    The selected well is highlighted and serialized as a GeoJson
    feature in the layer's json spec.
    
    * Fixed eslint formatting.
    
    * Fixed typescript condition.
    
    * Fixed black error condition.
    
    * Fixed pylint error condition.
    
    * Use GeoJson feature instead of custom wells data type.
    
    Co-authored-by: Andrei Alexandru <andrei.alexandru@emerson.com>
    Co-authored-by: Havard Bjerke <havard.bjerke@emerson.com>

--- src/lib/components/DeckGLMap/layers/wells/wellsLayer.ts
+++ src/lib/components/DeckGLMap/layers/wells/wellsLayer.ts
@@ -4,27 +4,31 @@ import { GeoJsonLayer, GeoJsonLayerProps } from "@deck.gl/layers";
 import { CompositeLayerProps } from "@deck.gl/core/lib/composite-layer";
 import { RGBAColor } from "@deck.gl/core/utils/color";
 
+import { Feature } from "geojson";
+
 export interface WellsLayerProps<D> extends CompositeLayerProps<D> {
     pointRadiusScale: number;
     lineWidthScale: number;
     outline: boolean;
+    selectedFeature: Feature;
 }
 
-export interface WellDataType {
-    type: string;
-    geometry: Record<string, unknown>;
-    properties: {
-        name: string;
-        color: RGBAColor;
-    };
+function handleClick(event) {
+    this.setLayerProps("wells-layer", { selectedFeature: event.object });
+    return true;
 }
+
+const defaultProps = {
+    onClick: handleClick,
+    autoHighlight: true,
+};
 
 export default class WellsLayer extends CompositeLayer<
-    WellDataType,
-    WellsLayerProps<WellDataType>
+    Feature,
+    WellsLayerProps<Feature>
 > {
-    renderLayers(): GeoJsonLayer<WellDataType>[] {
-        const properties: GeoJsonLayerProps<WellDataType> = {
+    renderLayers(): GeoJsonLayer<Feature>[] {
+        const properties: GeoJsonLayerProps<Feature> = {
             id: "outline",
             data: this.props.data,
             pickable: true,
@@ -34,24 +38,33 @@ export default class WellsLayer extends CompositeLayer<
             lineWidthScale: this.props.lineWidthScale,
         };
 
-        const outline = new GeoJsonLayer<WellDataType>(
+        const outline = new GeoJsonLayer<Feature>(
             this.getSubLayerProps(properties)
         );
 
         properties.id = "colors";
-        properties.getFillColor = (d: WellDataType): RGBAColor =>
-            d.properties.color;
+        properties.getFillColor = (d: Feature): RGBAColor =>
+            d?.properties?.color;
         properties.getLineColor = properties.getFillColor;
         properties.pointRadiusScale = (properties.pointRadiusScale || 8) - 1;
         properties.lineWidthScale = (properties.lineWidthScale || 5) - 1;
 
-        const colors = new GeoJsonLayer<WellDataType>(
+        const colors = new GeoJsonLayer<Feature>(
             this.getSubLayerProps(properties)
         );
 
-        if (this.props.outline) return [outline, colors];
-        else return [colors];
+        // Highlight the selected well.
+        properties.data = this.props.selectedFeature;
+        properties.pointRadiusScale = properties.pointRadiusScale + 2;
+        properties.lineWidthScale = properties.lineWidthScale + 2;
+        const highlight = new GeoJsonLayer<Feature>(
+            this.getSubLayerProps(properties)
+        );
+
+        if (this.props.outline) return [outline, colors, highlight];
+        else return [colors, highlight];
     }
 }
 
 WellsLayer.layerName = "WellsLayer";
+WellsLayer.defaultProps = defaultProps;
