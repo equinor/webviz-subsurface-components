c7e84459e9d95dece047073672f94a467ba343ae
Jørgen Herje <jorgen.herje@ceetronsolutions.com>
2021-06-01 16:18:43 +0200

    Fix linting errors

--- src/lib/index.js
+++ src/lib/index.js
@@ -13,7 +13,7 @@ import DeckGLMap from "./components/DeckGLMap";
 import PriorPosteriorDistribution from "./components/PriorPosteriorDistribution";
 import VectorSelector from "./components/VectorSelector";
 import WellCompletions from "./components/WellCompletions";
-import {VectorCalculator} from "./components/VectorCalculator";
+import { VectorCalculator } from "./components/VectorCalculator";
 
 export {
     HistoryMatch,
@@ -25,5 +25,5 @@ export {
     DeckGLMap,
     VectorSelector,
     WellCompletions,
-    VectorCalculator
+    VectorCalculator,
 };
