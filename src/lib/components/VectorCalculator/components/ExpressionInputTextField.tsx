0a1db1a07006aae5cfe8087b5f6d1e4d7fc35951
Jørgen Herje <jorgen.herje@ceetronsolutions.com>
2021-06-01 16:08:27 +0200

    Updated code formatting

--- src/lib/components/VectorCalculator/components/ExpressionInputTextField.tsx
+++ src/lib/components/VectorCalculator/components/ExpressionInputTextField.tsx
@@ -12,59 +12,58 @@ interface ExpressionInputTextFieldProps {
     onValidChanged: (isValid: boolean) => void;
 }
 
-export const ExpressionInputTextField: React.FC<ExpressionInputTextFieldProps> = (
-    props: ExpressionInputTextFieldProps
-) => {
-    const { expression, disabled } = props;
+export const ExpressionInputTextField: React.FC<ExpressionInputTextFieldProps> =
+    (props: ExpressionInputTextFieldProps) => {
+        const { expression, disabled } = props;
 
-    const [textFieldVariantState, setTextFieldVariantState] = React.useState<
-        "success" | "error" | "warning" | "default"
-    >("default");
-    const [textFieldIconState, setTextFieldIconState] = React.useState<
-        ReactNode | undefined
-    >(undefined);
+        const [textFieldVariantState, setTextFieldVariantState] =
+            React.useState<"success" | "error" | "warning" | "default">(
+                "default"
+            );
+        const [textFieldIconState, setTextFieldIconState] =
+            React.useState<ReactNode | undefined>(undefined);
 
-    Icon.add({ error_filled });
-    Icon.add({ thumbs_up });
+        Icon.add({ error_filled });
+        Icon.add({ thumbs_up });
 
-    const textFieldVariant = (isValid: boolean): "error" | "success" => {
-        if (!isValid) {
-            return "error";
-        }
-        return "success";
+        const textFieldVariant = (isValid: boolean): "error" | "success" => {
+            if (!isValid) {
+                return "error";
+            }
+            return "success";
+        };
+
+        const textFieldIcon = (isValid: boolean): ReactNode | undefined => {
+            if (!isValid) {
+                return <Icon key="error" name="error_filled" />;
+            }
+            return <Icon key="thumbs" name="thumbs_up" />;
+        };
+
+        React.useEffect(() => {
+            const isValid = parseExpression(expression);
+            setTextFieldVariantState(textFieldVariant(isValid));
+            setTextFieldIconState(textFieldIcon(isValid));
+            props.onValidChanged(isValid);
+        }, [expression]);
+
+        const handleInputChange = (
+            e: React.ChangeEvent<HTMLInputElement & HTMLTextAreaElement>
+        ): void => {
+            const newExpression: string = e.target.value;
+            props.onExpressionChange(newExpression);
+        };
+
+        return (
+            <TextField
+                id="expression_input_field"
+                label="Expression"
+                placeholder="New expression"
+                onChange={handleInputChange}
+                value={expression}
+                disabled={disabled}
+                variant={textFieldVariantState}
+                inputIcon={textFieldIconState}
+            />
+        );
     };
-
-    const textFieldIcon = (isValid: boolean): ReactNode | undefined => {
-        if (!isValid) {
-            return <Icon key="error" name="error_filled" />;
-        }
-        return <Icon key="thumbs" name="thumbs_up" />;
-    };
-
-    React.useEffect(() => {
-        const isValid = parseExpression(expression);
-        setTextFieldVariantState(textFieldVariant(isValid));
-        setTextFieldIconState(textFieldIcon(isValid));
-        props.onValidChanged(isValid);
-    }, [expression]);
-
-    const handleInputChange = (
-        e: React.ChangeEvent<HTMLInputElement & HTMLTextAreaElement>
-    ): void => {
-        const newExpression: string = e.target.value;
-        props.onExpressionChange(newExpression);
-    };
-
-    return (
-        <TextField
-            id="expression_input_field"
-            label="Expression"
-            placeholder="New expression"
-            onChange={handleInputChange}
-            value={expression}
-            disabled={disabled}
-            variant={textFieldVariantState}
-            inputIcon={textFieldIconState}
-        />
-    );
-};
