ca411275a235a19262fdce949128d9631661aac9
nilscb <Nils.Bonnevie@emerson.com>
2021-06-01 16:35:43 +0200

    Remove zoom functionality

--- src/lib/components/DeckGLMap/layers/piechart/pieChartLayer.ts
+++ src/lib/components/DeckGLMap/layers/piechart/pieChartLayer.ts
@@ -1,53 +1,47 @@
+import { CompositeLayer, COORDINATE_SYSTEM } from "@deck.gl/core";
 import { SolidPolygonLayer, SolidPolygonLayerProps } from "@deck.gl/layers";
-import { COORDINATE_SYSTEM } from "@deck.gl/core";
-
-const defaultProps = {
-    pickable: true,
-    coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
-    getFillColor: (d) => d.properties.color,
-    getPolygon: (d) => d.polygon,
-};
 
 export type PieChartLayerProps<D> = SolidPolygonLayerProps<D>;
-export default class PieChartLayer extends SolidPolygonLayer<
+
+// INPUT format (PieData):
+// {
+//     "pies": [ {"x": 433600, "y": 6477600, "R": 20, "fractions": [{"value":21, "idx":0}, {"value":13, "idx":1}, {"value":8, "idx":2}]},
+//               {"x": 434556, "y": 6478951, "R": 40, "fractions": [{"value":12, "idx":0}, {"value":18, "idx":1}, {"value":7, "idx":2}]},
+//               {"x": 437086, "y": 6477198, "R": 30, "fractions": [{"value":16, "idx":0}, {"value":20, "idx":1}, {"value":6, "idx":2}]}
+//             ],
+
+//     "properties": [{"color": [255, 0, 0], "label":"oil"}, {"color": [0, 0, 255], "label":"water"}, {"color": [0, 255, 0], "label":"gas"}, {"color": [0, 255, 255], "label":"sand"}]
+// }
+interface PieData {
+    pies: [
+        {
+            x: number;
+            y: number;
+            R: number;
+            fractions: [{ value: number; idx: number }];
+        }
+    ];
+
+    properties: [{ color: [number, number, number]; label: string }];
+}
+
+const defaultProps = {};
+export default class PieChartLayer extends CompositeLayer<
     unknown,
     PieChartLayerProps<unknown>
 > {
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    shouldUpdateState({ changeFlags }) {
-        return changeFlags.viewportChanged;
-    }
+    renderLayers(): SolidPolygonLayer<unknown>[] {
+        const properties: PieChartLayerProps<unknown> = {
+            id: "pie-layer",
+            data: makePies(this.props.data as PieData),
+            pickable: true,
+            getFillColor: (d: {
+                properties: { color: [number, number, number]; label: string };
+            }) => d?.properties?.color ?? [0, 0, 0],
+        };
 
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    updateState({ props, oldProps, context, changeFlags }) {
-        super.updateState({ props, oldProps, context, changeFlags });
-
-        const zoom = context?.viewport?.zoom ?? 1;
-
-        if (props.data?.pies !== undefined) {
-            // Data is read and it is in proprietary format
-            // We need to store this data. Zoom is realtive to it.
-            // Store in new prop 'data_orig'
-            let new_props = Object.assign(
-                { ...props },
-                { data_orig: props.data }
-            );
-
-            const new_data = makePies(props.data, zoom);
-            new_props = Object.assign({ ...new_props }, { data: new_data });
-            props.setLayerProps(props.id, new_props);
-            return;
-        }
-
-        if (props.data_orig !== undefined) {
-            const zoomed_data = makePies(props.data_orig, zoom);
-
-            const new_props = Object.assign(
-                { ...props },
-                { data: zoomed_data }
-            );
-            props.setLayerProps(props.id, new_props);
-        }
+        const layer = new SolidPolygonLayer(this.getSubLayerProps(properties));
+        return [layer];
     }
 }
 
@@ -56,21 +50,14 @@ PieChartLayer.defaultProps = defaultProps;
 
 //================= Local help functions. ==================
 
-// INPUT format:
-// {
-//     "pies": [ {"x": 433600, "y": 6477600, "R": 20, "props": [{"frac": 21, "color": [255, 0, 0], "label":"oil"}, {"frac": 7, "color": [0, 0, 255], "label":"water"}, {"frac": 11, "color": [0, 255, 0], "label":"gas"}]},
-//               {"x": 434556, "y": 6478951, "R": 40, "props": [{"frac": 21, "color": [255, 0, 0], "label":"oil"}, {"frac": 9, "color": [0, 0, 255], "label":"water"}, {"frac":  8, "color": [0, 255, 0], "label":"gas"}, {"frac": 8, "color": [255, 255, 0], "label":"sand"}]},
-//               ...
-//             ]
-// }
-function makePies(data, zoom) {
+function makePies(data: PieData) {
     if (data.pies === undefined) {
         return;
     }
 
     const polygons: unknown[] = [];
     for (const pie of data.pies) {
-        const pie_polygons = makePie(pie, zoom);
+        const pie_polygons = makePie(pie, data.properties);
         for (const polygon of pie_polygons) {
             polygons.push(polygon);
         }
@@ -79,43 +66,42 @@ function makePies(data, zoom) {
 }
 
 // return array of one pie's polygon's
-function makePie(pie, zoom) {
+function makePie(pie, properties) {
     const dA = 10; // delta angle
 
     const x = pie.x;
     const y = pie.y;
     const R = pie.R;
-    const R_zoom = R / Math.pow(2, zoom);
-
-    const pie_props = pie.properties;
 
     // Normalize
     let sum = 0;
-    for (const prop of pie_props) {
-        sum += prop.frac;
+    for (const frac of pie.fractions) {
+        sum += frac.value;
     }
 
     const pie_polygons: unknown[] = [];
     let start_a = -90.0;
-    for (let i = 0; i < pie_props.length; i++) {
-        const frac = pie_props[i].frac / sum;
+    for (let i = 0; i < pie.fractions.length; i++) {
+        const frac = pie.fractions[i].value / sum;
         const end_a = start_a + frac * 360.0;
         const coordinates: number[][] = [];
         coordinates.push([x, y]);
         for (let a = start_a; a < end_a; a += dA) {
             const rad = (a * (2.0 * Math.PI)) / 360.0;
-            const xx = R_zoom * Math.cos(rad) + x;
-            const yy = R_zoom * Math.sin(rad) + y;
+            const xx = R * Math.cos(rad) + x;
+            const yy = R * Math.sin(rad) + y;
             coordinates.push([xx, yy]);
         }
         const rad = (end_a * (2.0 * Math.PI)) / 360.0;
-        const xx = R_zoom * Math.cos(rad) + x;
-        const yy = R_zoom * Math.sin(rad) + y;
+        const xx = R * Math.cos(rad) + x;
+        const yy = R * Math.sin(rad) + y;
         coordinates.push([xx, yy]);
         coordinates.push([x, y]);
 
-        const col = pie_props[i].color;
-        const label = pie_props[i].label;
+        const prop = properties[pie.fractions[i].idx];
+        const col = prop?.color ?? [255, 0, 255]; // magenta
+        const label = prop?.label ?? "no label";
+
         pie_polygons.push({
             polygon: coordinates,
             properties: {
