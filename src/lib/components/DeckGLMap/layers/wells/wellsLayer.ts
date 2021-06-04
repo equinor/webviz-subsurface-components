90da556148dd07f0c7509f2d446d201a42801205
Shadab Khan <shadabskhan@hotmail.com>
2021-06-04 12:40:32 +0530

    1. Used resample log data 2. Used name to select log insted of log index 3. Improvised color selection 4. Resolved typescript errors

--- src/lib/components/DeckGLMap/layers/wells/wellsLayer.ts
+++ src/lib/components/DeckGLMap/layers/wells/wellsLayer.ts
@@ -1,5 +1,4 @@
 import { CompositeLayer } from "@deck.gl/core";
-
 import { CompositeLayerProps } from "@deck.gl/core/lib/composite-layer";
 import { GeoJsonLayer, PathLayer } from "@deck.gl/layers";
 import { RGBAColor } from "@deck.gl/core/utils/color";
@@ -82,65 +81,6 @@ function getLogColor(d: LogCurveDataType, log_name: string): RGBAColor[] {
 function getLogWidth(d: LogCurveDataType, log_name: string): number[] | null {
     const log_id = getLogIDByName(d, log_name);
     return log_id ? d?.data[log_id] : null;
-}
-
-export interface LogCurveDataType {
-    header: {
-        name: string;
-    };
-    curves: {
-        name: string;
-        description: string;
-    };
-    data: [number[]];
-}
-
-let COLOR_MAP = [
-    [28, 255, 12, 200],
-    [196, 255, 7, 200],
-    [148, 129, 255, 200],
-    [118, 90, 254, 200],
-    [78, 254, 17, 200],
-    [19, 242, 255, 200],
-    [26, 70, 255, 200],
-    [255, 171, 156, 200],
-    [138, 83, 255, 200]
-]
-
-/*
-function transpose(mat) {
-    for (var i = 0; i < mat.length; i++) {
-        for (var j = 0; j < i; j++) {
-            const tmp = mat[i][j];
-            mat[i][j] = mat[j][i];
-            mat[j][i] = tmp;
-        }
-    }
-}
-
-function getLogPath(d: WellDataType, lc_idx: number) {
-    if (d.properties != undefined) {
-        let found = LOG_DATA.find(item => item.header.well == d.properties.name)
-        if (found != undefined) {
-            //transpose(found.data)
-            return found.data[lc_idx];
-        }
-    }
-    return [];
-}
-*/
-
-function getLogColor(d: LogCurveDataType, lc_idx: number) {
-    //console.log(d.curves[lc_idx].name)
-    if (d.curves[lc_idx] == undefined ||
-        d.curves[lc_idx].description == "continuous") {
-        return []
-    }
-    let color: number[][] = [];
-    d.data[lc_idx].forEach(value => {
-        color.push(COLOR_MAP[value]);
-    });
-    return color;
 }
 
 const defaultProps = {
