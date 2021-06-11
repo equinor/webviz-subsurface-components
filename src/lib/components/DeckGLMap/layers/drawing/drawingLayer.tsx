92479c4dbbf313058fccd4ab1b0b5c24a5cd8e19
Andrei Alexandru <Andrei.Alexandru@Emerson.com>
2021-06-11 16:08:35 +0200

    Add `modify`mode to the drawing layer

--- src/lib/components/DeckGLMap/layers/drawing/drawingLayer.tsx
+++ src/lib/components/DeckGLMap/layers/drawing/drawingLayer.tsx
@@ -1,31 +1,62 @@
-import { CompositeLayer } from "deck.gl";
+import { COORDINATE_SYSTEM, RGBAColor } from "@deck.gl/core";
+import { UpdateStateInfo } from "@deck.gl/core/lib/layer";
 import { CompositeLayerProps } from "@deck.gl/core/lib/composite-layer";
 import {
-    ViewMode,
-    DrawPointMode,
     DrawLineStringMode,
+    DrawPointMode,
     DrawPolygonMode,
+    EditAction,
+    Feature,
+    FeatureCollection,
+    ImmutableFeatureCollection,
+    ModeProps,
+    ModifyMode,
+    TransformMode,
+    ViewMode,
 } from "@nebula.gl/edit-modes";
-
-import { FeatureCollection } from "geojson";
-
 import { EditableGeoJsonLayer } from "@nebula.gl/layers";
-
-import { COORDINATE_SYSTEM } from "@deck.gl/core";
-
+import { CompositeLayer, PickInfo } from "deck.gl";
 import { patchLayerProps } from "../utils/layerTools";
+
+class CustomModifyMode extends ModifyMode {
+    handleKeyUp(event: KeyboardEvent, props: ModeProps<FeatureCollection>) {
+        super.handleKeyUp(event, props);
+
+        if (event.key === "Delete") {
+            const updatedData = new ImmutableFeatureCollection(props.data)
+                .deleteFeatures(props.selectedIndexes)
+                .getObject();
+
+            if (updatedData) {
+                props.onEdit({
+                    updatedData,
+                    editType: "removeFeature",
+                    editContext: {
+                        featureIndexes: props.selectedIndexes,
+                    },
+                });
+            }
+        }
+    }
+}
 
 // Mapping of mode name to mode class
 const MODE_MAP = {
     view: ViewMode,
+    modify: CustomModifyMode,
+    transform: TransformMode,
     drawPoint: DrawPointMode,
     drawLineString: DrawLineStringMode,
     drawPolygon: DrawPolygonMode,
 };
 
+const UNSELECTED_LINE_COLOR: RGBAColor = [0x50, 0x50, 0x50, 0xcc];
+const SELECTED_LINE_COLOR: RGBAColor = [0x0, 0x0, 0x0, 0xff];
+
 const defaultProps = {
     pickable: true,
     mode: "drawLineString",
+    selectedFeatureIndexes: [],
     data: {
         type: "FeatureCollection",
         features: [],
@@ -34,43 +65,104 @@ const defaultProps = {
 
 export interface DrawingLayerProps<D> extends CompositeLayerProps<D> {
     mode: string;
+    selectedFeatureIndexes: number[];
 }
 
 export default class DrawingLayer extends CompositeLayer<
     FeatureCollection,
     DrawingLayerProps<FeatureCollection>
 > {
-    initializeState(): void {
-        super.initializeState();
+    updateState(
+        info: UpdateStateInfo<DrawingLayerProps<FeatureCollection>>
+    ): void {
+        super.updateState(info);
 
-        this.setState({
-            selectedFeatureIndexes: [],
-        });
+        if (info.changeFlags.dataChanged) {
+            this.setState({
+                data: this.props.data,
+            });
+        }
+    }
+
+    onClick(info: PickInfo<FeatureCollection>): boolean {
+        if (this.props.mode === "view" || this.props.mode === "modify") {
+            const featureIndex = this.state.data.features.indexOf(info.object);
+            if (featureIndex >= 0) {
+                patchLayerProps(this, {
+                    ...this.props,
+                    selectedFeatureIndexes: [info.index],
+                } as DrawingLayerProps<FeatureCollection>);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    _onEdit(editAction: EditAction<FeatureCollection>): void {
+        switch (editAction.editType) {
+            case "addFeature":
+                patchLayerProps(this, {
+                    ...this.props,
+                    data: editAction.updatedData,
+                    selectedFeatureIndexes:
+                        editAction.editContext.featureIndexes,
+                } as DrawingLayerProps<FeatureCollection>);
+                break;
+            case "removeFeature":
+                patchLayerProps(this, {
+                    ...this.props,
+                    data: editAction.updatedData,
+                    selectedFeatureIndexes: [],
+                } as DrawingLayerProps<FeatureCollection>);
+                break;
+            case "removePosition":
+            case "finishMovePosition":
+                patchLayerProps(this, {
+                    ...this.props,
+                    data: editAction.updatedData,
+                } as DrawingLayerProps<FeatureCollection>);
+                break;
+            case "movePosition":
+                // Don't use patchLayerProps to avoid an expensive roundtrip,
+                // since this is done on every mouse move when editing.
+                this.setState({ data: editAction.updatedData });
+                break;
+        }
+    }
+
+    _getLineColor(feature: Feature): RGBAColor {
+        const is_feature_selected = this.props.selectedFeatureIndexes.some(
+            (i) => this.state.data.features[i] === feature
+        );
+        if (is_feature_selected) {
+            return SELECTED_LINE_COLOR;
+        } else {
+            return UNSELECTED_LINE_COLOR;
+        }
     }
 
     renderLayers(): [EditableGeoJsonLayer?] {
         if (this.props.visible == false) {
             return [];
         }
-
         return [
             new EditableGeoJsonLayer(
                 this.getSubLayerProps({
-                    data: this.props.data,
+                    data: this.state.data,
                     mode: MODE_MAP[this.props.mode],
-                    selectedFeatureIndexes: this.state.selectedFeatureIndexes,
+                    modeConfig: {
+                        viewport: this.context.viewport,
+                    },
+                    selectedFeatureIndexes: this.props.selectedFeatureIndexes,
                     coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
-                    onEdit: ({ updatedData, editType }) => {
-                        if (editType === "addFeature" && this.props.id) {
-                            patchLayerProps<
-                                DrawingLayer,
-                                DrawingLayerProps<FeatureCollection>
-                            >(this, {
-                                ...this.props,
-                                data: updatedData,
-                                mode: "view",
-                            });
-                        }
+                    onEdit: (editAction) => this._onEdit(editAction),
+                    _subLayerProps: {
+                        geojson: {
+                            autoHighlight: true,
+                            getLineColor: (feature) =>
+                                this._getLineColor(feature),
+                        },
                     },
                 })
             ),
