9add3dec4de5d86450f41d7a897ac4f97413b505
Jørgen Herje <jorgen.herje@ceetronsolutions.com>
2021-06-15 15:33:14 +0200

    Initial implementation of expression parser
    Utilizing expr-eval parser library to parse expression. Wrapper made for
    aditional  parsing criterias.

--- src/lib/components/VectorCalculator/utils/VectorCalculatorRegex.ts
+++ src/lib/components/VectorCalculator/utils/VectorCalculatorRegex.ts
@@ -4,34 +4,3 @@ export const parseName = (name: string): boolean => {
     );
     return regex.test(name);
 };
-
-export const parseExpression = (expression: string): boolean => {
-    /// Regex parsing equations
-    const regex = new RegExp(
-        /^-?(\((-?[a-zA-Z]{1}|[\d.]{1,})([+*/\-^]{1}([a-zA-Z]{1}|[\d.]{1,})){0,}\)|([a-zA-Z]{1}|[\d.]{1,})){1}(([+*/\-^]{1}\(([a-zA-Z]{1}|[\d.]{1,})([+*/\-^]{1}([a-zA-Z]{1}|[\d.]{1,})){0,}\))|([+*/\-^]{1}([a-zA-Z]{1}|[\d.]{1,}))){0,}$/
-    );
-    return regex.test(expression);
-};
-
-// NOTE: Assume single character representation for variables and only one occurrence
-// of each character
-export const retrieveVariablesFromValidExpression = (
-    expression: string
-): string[] => {
-    const res: string[] = [];
-
-    // Retreive list of variables for valid expression - assuming single character variables
-    const regex = /[a-zA-Z]/g;
-    const variables = expression.match(regex);
-    if (variables === null) {
-        return res;
-    }
-
-    // Ensure only one occurence of each variable
-    for (const variable of variables) {
-        if (!res.includes(variable)) {
-            res.push(variable);
-        }
-    }
-    return res;
-};
