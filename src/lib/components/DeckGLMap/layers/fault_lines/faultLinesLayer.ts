1b83bcf503dc73d7777aacac0aea628595f9c8d6
nilscb <Nils.Bonnevie@emerson.com>
2021-06-15 15:09:29 +0200

    Fault lines add tranparent fill color

--- src/lib/components/DeckGLMap/layers/fault_lines/faultLinesLayer.ts
+++ src/lib/components/DeckGLMap/layers/fault_lines/faultLinesLayer.ts
@@ -1,10 +1,21 @@
 import { GeoJsonLayer, GeoJsonLayerProps } from "@deck.gl/layers";
 import { COORDINATE_SYSTEM } from "@deck.gl/core";
+import { RGBAColor } from "@deck.gl/core/utils/color";
+import { Feature } from "geojson";
+
+const getColor = (d: Feature): RGBAColor => {
+    const c: RGBAColor = d?.properties?.color;
+    const r = c[0] ?? 0;
+    const g = c[1] ?? 0;
+    const b = c[2] ?? 0;
+    return [r, g, b, 30]; // make fill color transparent
+};
 
 const defaultProps = {
     pickable: true,
     coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
     getLineColor: (d) => d.properties.color,
+    getFillColor: getColor,
 };
 
 export type FaultLinesLayerProps<D> = GeoJsonLayerProps<D>;
