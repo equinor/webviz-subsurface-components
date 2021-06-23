c657e0895a9f91e4f7a6091e5e79064d18943dea
avanti-em <81212785+avanti-em@users.noreply.github.com>
2021-06-23 14:21:01 +0530

    Well completion unit tests (#406)

--- src/lib/components/WellCompletions/components/Settings/WellAttributesSelector.test.tsx
+++ src/lib/components/WellCompletions/components/Settings/WellAttributesSelector.test.tsx
@@ -1,9 +1,13 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "jest-styled-components";
 import React from "react";
-import { Wrapper } from "../../test/TestWrapper";
+import { testStore, Wrapper } from "../../test/TestWrapper";
 import WellAttributesSelector from "./WellAttributesSelector";
+
+//Manually add the scrollTo because it is not implemented in jsdom
+//Without it, the SmartNodeSelector reports TypeError: suggestionsRef.current.scrollTo is not a function
+Element.prototype.scrollTo = jest.fn();
 
 describe("test well attributes selector", () => {
     it("snapshot test", () => {
@@ -11,5 +15,21 @@ describe("test well attributes selector", () => {
             Wrapper({ children: <WellAttributesSelector /> })
         );
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it("click to dispatch redux action", async () => {
+        render(<WellAttributesSelector />, {
+            wrapper: Wrapper,
+        });
+
+        const attributeFilter = screen.getByRole("textbox");
+        fireEvent.change(attributeFilter, {
+            target: { value: "type:Injector" },
+        });
+        expect(testStore.dispatch).toHaveBeenCalledTimes(1);
+        expect(testStore.dispatch).toBeCalledWith({
+            payload: ["type:Injector"],
+            type: "ui/updateFilterByAttributes",
+        });
     });
 });
--- src/lib/components/WellCompletions/components/Settings/ZoneSelector.test.tsx
+++ src/lib/components/WellCompletions/components/Settings/ZoneSelector.test.tsx
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "jest-styled-components";
 import React from "react";
 import { testStore, Wrapper } from "../../test/TestWrapper";
@@ -24,12 +25,46 @@ describe("test Zone Selector", () => {
         });
     });
 
-    it("click to dispatch redux action to remove one zone", async () => {
+    it("select 'zone1' check box to dispatch redux action and show only zone1", async () => {
         render(<ZoneSelector />, {
             wrapper: Wrapper,
         });
 
-        fireEvent.click(screen.getByText(/select zone\(s\)\.\.\./i));
-        expect(screen.getByPlaceholderText("Search...")).toBeInTheDocument();
+        userEvent.click(screen.getByRole("button", { name: /remove all/i }));
+        const zoneFilter = screen.getByText(/select zone\(s\)\.\.\./i);
+        userEvent.click(zoneFilter);
+        const dropdown = screen.getByPlaceholderText("Search...");
+        await waitFor(() => expect(dropdown).toBeVisible());
+        userEvent.type(dropdown, "{down}");
+        userEvent.type(dropdown, "{enter}");
+
+        expect(testStore.dispatch).toHaveBeenCalledTimes(3);
+        expect(testStore.dispatch).toHaveBeenNthCalledWith(1, {
+            payload: [],
+            type: "ui/updateFilteredZones",
+        });
+        expect(testStore.dispatch).toHaveBeenNthCalledWith(2, {
+            payload: [],
+            type: "ui/updateFilteredZones",
+        });
+        expect(testStore.dispatch).toHaveBeenNthCalledWith(3, {
+            payload: [
+                "zone1.1",
+                "zone1.2",
+                "zone1.3",
+                "zone2.1",
+                "zone2.2",
+                "zone2.3",
+                "zone3.1",
+                "zone3.2",
+                "zone3.3",
+                "zone4.1",
+                "zone4.2",
+                "zone4.3",
+                "zone4.4",
+                "zone4.5",
+            ],
+            type: "ui/updateFilteredZones",
+        });
     });
 });
