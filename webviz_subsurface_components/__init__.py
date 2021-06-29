198f1fbacbd25ab5fff13b72ce66ffb04ce96463
alexandruandrei91 <57130720+alexandruandrei91@users.noreply.github.com>
2021-06-29 15:32:06 +0200

    Refactoring codebase (Part1) (#424)
    * Move react stuff to react/
    * Update output dir for dash components
    * Update license in package.json
    * Update package name to use the same convention as core components
    * Disable generation of R dash components
    * Temporary disable the python tests

--- webviz_subsurface_components/__init__.py
+++ webviz_subsurface_components/__init__.py
@@ -38,7 +38,13 @@ _filepath = _os.path.abspath(_os.path.join(_basepath, "package.json"))
 with open(_filepath) as f:
     package = json.load(f)
 
-package_name = package["name"].replace(" ", "_").replace("-", "_")
+package_name = (
+    package["name"]
+    .replace(" ", "_")
+    .replace("-", "_")
+    .replace("/", "_")
+    .replace("@", "")
+)
 
 _current_path = _os.path.dirname(_os.path.abspath(__file__))
 
