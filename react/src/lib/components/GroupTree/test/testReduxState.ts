8f296660900c7d7153d58c1fefa26b0163d318ca
avanti-em <81212785+avanti-em@users.noreply.github.com>
2021-07-12 12:49:55 +0530

    initial group tree component tests (#447)
    * initial group tree component tests
    
    * fix lint errors
    
    * fix lint errors 2
    
    Co-authored-by: alexandruandrei91 <57130720+alexandruandrei91@users.noreply.github.com>

---
+++ react/src/lib/components/GroupTree/test/testReduxState.ts
@@ -0,0 +1,10 @@
+import { UISettings } from "../redux/types";
+
+export const testState = {
+    id: "test",
+    ui: {
+        currentIteration: "Iter_0",
+        currentDateTime: "01/01/2000",
+        currentFlowRate: "oilrate",
+    } as UISettings,
+};
