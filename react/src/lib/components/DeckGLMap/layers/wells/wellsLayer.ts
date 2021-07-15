893154b7a658245514b6ecc883472d9dc46cd3db
nilscb <nils.christian.bonnevie@gmail.com>
2021-07-15 14:46:56 +0200

    Catmull 3D spline interpolation

--- react/src/lib/components/DeckGLMap/layers/wells/wellsLayer.ts
+++ react/src/lib/components/DeckGLMap/layers/wells/wellsLayer.ts
@@ -6,11 +6,10 @@ import { PickInfo } from "deck.gl";
 import { subtract, distance, dot } from "mathjs";
 import { interpolateRgbBasis } from "d3-interpolate";
 import { color } from "d3-color";
-
 import { Feature } from "geojson";
-
 import { LayerPickInfo, PropertyDataType } from "../utils/layerTools";
 import { patchLayerProps } from "../utils/layerTools";
+import { Position2D, Position3D } from "@deck.gl/core/utils/positions";
 
 export interface WellsLayerProps<D> extends CompositeLayerProps<D> {
     pointRadiusScale: number;
@@ -70,10 +69,15 @@ export default class WellsLayer extends CompositeLayer<
     }
 
     renderLayers(): (GeoJsonLayer<Feature> | PathLayer<LogCurveDataType>)[] {
+        //const now = Date.now();
+        const refine = true;
+        const data = splineRefine(this.props.data, refine);
+        //console.log("time elapsed:", Date.now() - now);
+
         const outline = new GeoJsonLayer<Feature>(
             this.getSubLayerProps({
                 id: "outline",
-                data: this.props.data,
+                data,
                 pickable: false,
                 stroked: false,
                 pointRadiusUnits: "pixels",
@@ -87,7 +91,7 @@ export default class WellsLayer extends CompositeLayer<
         const colors = new GeoJsonLayer<Feature>(
             this.getSubLayerProps({
                 id: "colors",
-                data: this.props.data,
+                data,
                 pickable: true,
                 stroked: false,
                 pointRadiusUnits: "pixels",
@@ -145,6 +149,7 @@ export default class WellsLayer extends CompositeLayer<
         if (this.props.logCurves) {
             layers.splice(1, 0, log_layer);
         }
+
         return layers;
     }
 
@@ -347,4 +352,160 @@ function getLogProperty(
 
     if (log_value) return { name: prop_name, value: log_value };
     else return null;
+}
+
+function CatmullRom(
+    P0: Position3D,
+    P1: Position3D,
+    P2: Position3D,
+    P3: Position3D,
+    t: number
+): Position3D {
+    const alpha = 0.5;
+    const tt = t * t;
+    const ttt = t * t * t;
+
+    // disable eslint for some lines due to readability.
+    const dist_p0_p1 = Math.sqrt((P1[0]-P0[0])*(P1[0]-P0[0]) + (P1[1]-P0[1])*(P1[1]-P0[1]) + (P1[2]-P0[2])*(P1[2]-P0[2]) ); // eslint-disable-line
+    const dist_p1_p2 = Math.sqrt((P1[0]-P2[0])*(P1[0]-P2[0]) + (P1[1]-P2[1])*(P1[1]-P2[1]) + (P1[2]-P2[2])*(P1[2]-P2[2]) ); // eslint-disable-line
+    const dist_p2_p3 = Math.sqrt((P3[0]-P2[0])*(P3[0]-P2[0]) + (P3[1]-P2[1])*(P3[1]-P2[1]) + (P3[2]-P2[2])*(P3[2]-P2[2]) ); // eslint-disable-line
+
+    const t01 = Math.pow(dist_p0_p1, alpha);
+    const t12 = Math.pow(dist_p1_p2, alpha);
+    const t23 = Math.pow(dist_p2_p3, alpha);
+
+    const m1_x = (P2[0] - P1[0] + t12 * ((P1[0] - P0[0]) / t01 - (P2[0] - P0[0]) / (t01 + t12))); // eslint-disable-line
+    const m1_y = (P2[1] - P1[1] + t12 * ((P1[1] - P0[1]) / t01 - (P2[1] - P0[1]) / (t01 + t12))); // eslint-disable-line
+    const m1_z = (P2[2] - P1[2] + t12 * ((P1[2] - P0[2]) / t01 - (P2[2] - P0[2]) / (t01 + t12))); // eslint-disable-line
+
+    const m2_x = (P2[0] - P1[0] + t12 * ((P3[0] - P2[0]) / t23 - (P3[0] - P1[0]) / (t12 + t23))); // eslint-disable-line
+    const m2_y = (P2[1] - P1[1] + t12 * ((P3[1] - P2[1]) / t23 - (P3[1] - P1[1]) / (t12 + t23))); // eslint-disable-line
+    const m2_z = (P2[2] - P1[2] + t12 * ((P3[2] - P2[2]) / t23 - (P3[2] - P1[2]) / (t12 + t23))); // eslint-disable-line
+
+    const a_x = 2 * (P1[0] - P2[0]) + m1_x + m2_x;
+    const a_y = 2 * (P1[1] - P2[1]) + m1_y + m2_y;
+    const a_z = 2 * (P1[2] - P2[2]) + m1_z + m2_z;
+
+    const b_x = -3 * (P1[0] - P2[0]) - m1_x - m1_x - m2_x;
+    const b_y = -3 * (P1[1] - P2[1]) - m1_y - m1_y - m2_y;
+    const b_z = -3 * (P1[2] - P2[2]) - m1_z - m1_z - m2_z;
+
+    const c_x = m1_x;
+    const c_y = m1_y;
+    const c_z = m1_z;
+
+    const d_x = P1[0];
+    const d_y = P1[1];
+    const d_z = P1[2];
+
+    const x = a_x * ttt + b_x * tt + c_x * t + d_x;
+    const y = a_y * ttt + b_y * tt + c_y * t + d_y;
+    const z = a_z * ttt + b_z * tt + c_z * t + d_z;
+
+    return [x, y, z] as Position3D;
+}
+
+function splineRefine(data, refine: boolean) {
+    const ts = refine ? [0.2, 0.4, 0.6, 0.8] : [];
+
+    if (data["features"] === undefined) {
+        console.log("NO DATA")
+        return;
+    }
+
+    const no_wells = data["features"].length;
+    console.log("no_wells", no_wells)
+
+    for (let well_no = 0; well_no < no_wells; well_no++) {
+        const mds = data["features"][well_no]["properties"]["md"];
+
+        const coords = data["features"][well_no]["geometry"]["geometries"][1]["coordinates"]; // eslint-disable-line
+
+        const n = coords.length;
+        if (n < 3) {
+            continue;
+        }
+
+        // Point before first.
+        const x0 = coords[0][0] - coords[1][0] + coords[0][0];
+        const y0 = coords[0][1] - coords[1][1] + coords[0][1];
+        const z0 = coords[0][2] - coords[1][2] + coords[0][2];
+        const P_first: Position3D = [x0, y0, z0];
+
+        // Point after last.
+        const xn = coords[n - 1][0] - coords[n - 2][0] + coords[n - 1][0];
+        const yn = coords[n - 1][1] - coords[n - 2][1] + coords[n - 1][1];
+        const zn = coords[n - 1][2] - coords[n - 2][2] + coords[n - 1][2];
+        const P_n: Position3D = [xn, yn, zn];
+
+        const md_first = 0.25 * (mds[0][0] - mds[0][1]) + mds[0][0];
+        const md_n = 0.25 * (mds[0][n - 1] - mds[0][n - 2]) + mds[0][n - 1];
+
+        const newCoordinates: [Position3D?] = [];
+        const newMds: number[][] = [];
+        newMds.push([]);
+
+        for (let i = 0; i < n - 2; i += 1) {
+            let P0: Position3D, P1: Position3D, P2: Position3D, P3: Position3D;
+            let md0: number, md1: number, md2: number, md3: number;
+
+            if (i === 0) {
+                P0 = P_first;
+                P1 = coords[i + 0];
+                P2 = coords[i + 1];
+                P3 = coords[i + 2];
+
+                md0 = md_first;
+                md1 = mds[0][i + 0];
+                md2 = mds[0][i + 1];
+                md3 = mds[0][i + 2];
+            } else if (i === n - 3) {
+                P0 = coords[i + 0];
+                P1 = coords[i + 1];
+                P2 = coords[i + 2];
+                P3 = P_n;
+
+                md0 = mds[0][i + 0];
+                md1 = mds[0][i + 1];
+                md2 = mds[0][i + 2];
+                md3 = md_n;
+            } else {
+                P0 = coords[i + 0];
+                P1 = coords[i + 1];
+                P2 = coords[i + 2];
+                P3 = coords[i + 3];
+
+                md0 = mds[0][i + 0];
+                md1 = mds[0][i + 1];
+                md2 = mds[0][i + 2];
+                md3 = mds[0][i + 3];
+            }
+
+            newCoordinates.push(P1);
+            newMds[0].push(md1);
+
+            for (let t_i = 0; t_i < ts.length; t_i += 1) {
+                const t = ts[t_i];
+                const [x, y, z] = CatmullRom(P0, P1, P2, P3, t);
+                const md = mds[0][i];
+
+                newCoordinates.push([x, y, z] as Position3D);
+                newMds[0].push(md);
+            }
+        }
+
+        newCoordinates.push(coords[n - 1]);
+        newMds[0].push(mds[n - 1]);
+
+        // Convert well path to 2D.
+        const coords2D: Position2D[] = newCoordinates.map((e: Position3D) => {
+            return [e[0], e[1]] as Position2D;
+        });
+
+        data["features"][well_no]["geometry"]["geometries"][1]["coordinates"] =
+            coords2D;
+        data["features"][well_no]["properties"]["md"] = newMds;
+    }
+
+    return data;
 }
