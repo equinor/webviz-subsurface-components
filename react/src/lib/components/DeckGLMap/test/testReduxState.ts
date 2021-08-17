d348fd142d512150778fe6edfe8b12b2b4d0a492
Sanghavi <Avanti.Sanghavi@emerson.com>
2021-08-17 13:40:47 +0530

    map tests improvement

--- react/src/lib/components/DeckGLMap/test/testReduxState.ts
+++ react/src/lib/components/DeckGLMap/test/testReduxState.ts
@@ -1,11 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const exampleData = require("../../../../demo/example-data/deckgl-map.json");
+import * as exampleData from "../../../../demo/example-data/deckgl-map.json";
 
-const recordData = exampleData[0];
-const demoData = JSON.stringify(recordData, null, 2);
-demoData as unknown as Record<string, unknown>; // trying to convert the string "demodata" to type Record<K,T>
+const recordData = exampleData[0].deckglSpecBase;
 
-//export const testState = demoData
-console.log(typeof demoData); // but type is still 'string'
-
-export const testState = {}; // exporting an empty object instead of 'demoData' because 'string' type is not acceptable in "./TestWrapper.tsx"
+export const testState = recordData;
+console.log(recordData);
