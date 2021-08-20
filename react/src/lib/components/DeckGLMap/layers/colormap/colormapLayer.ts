2aaf72e5c7ef424eb5060f37f05a7ec7e10de98b
Andrei Alexandru <andrei.alexandru@emerson.com>
2021-08-20 11:12:08 +0200

    More doc

--- react/src/lib/components/DeckGLMap/layers/colormap/colormapLayer.ts
+++ react/src/lib/components/DeckGLMap/layers/colormap/colormapLayer.ts
@@ -1,5 +1,3 @@
-// DeckGL typescript declarations are not great, so for now it's just js.
-
 import { BitmapLayer, BitmapLayerProps } from "@deck.gl/layers";
 
 import GL from "@luma.gl/constants";
@@ -22,9 +20,34 @@ const DEFAULT_TEXTURE_PARAMETERS = {
     [GL.TEXTURE_WRAP_T]: GL.CLAMP_TO_EDGE,
 };
 
+// Most props are inherited from DeckGL's BitmapLayer. For a full list, see
+// https://deck.gl/docs/api-reference/layers/bitmap-layer
+//
+// The property map is encoded in an image and sent in the `image` prop of the BitmapLayer.
+// The same approach is used in DeckGL's TerrainLayer: https://deck.gl/docs/api-reference/geo-layers/terrain-layer
+//
+// The image format is based on Tizen's terrain format and Mapbox's TerrainRGB:
+// https://github.com/tilezen/joerd/blob/master/docs/formats.md
+// https://docs.mapbox.com/help/troubleshooting/access-elevation-data/
+//
+// The main idea is that property values (floats) are encoded in the R G and B channels of an image.
+// We parametrize decoding, so we can support both Tizen and Mapbox terrains by adapting
+// the valueDecoder for each format.
+// We also support and use by default a format that optimizes for precision.
+// By default, the value decoder will map RGB(0, 0, 0) to the minimum value in valueRange
+// and RGB(255, 255, 255) to the maximum value in valueRange, thus giving us the full
+// > 16mil possible values for any property value range.
+// We also support property maps with an alpha channel. See colormap.fs.glsl for more details.
 export interface ColormapLayerProps<D> extends BitmapLayerProps<D> {
+    // Image containing the colors that will be applied to te values.
+    // e.g.: https://cdn.jsdelivr.net/gh/kylebarron/deck.gl-raster/assets/colormaps/plasma.png
+    // No size requirements. Alpha channel (opacity) supported.
     colormap: unknown;
+
+    // Min and max property values.
     valueRange: [number, number];
+
+    // See ValueDecoder in propertyMapTools.ts
     valueDecoder: ValueDecoder;
 }
 
@@ -35,7 +58,7 @@ const defaultProps = {
         type: "object",
         value: {
             rgbScaler: [1, 1, 1],
-            // Scale [0, 256*256*256-1] to [0, 1]
+            // By default, scale the [0, 256*256*256-1] decoded values to [0, 1]
             floatScaler: 1.0 / (256.0 * 256.0 * 256.0 - 1.0),
             offset: 0,
             step: 0,
@@ -64,6 +87,7 @@ export default class ColormapLayer extends BitmapLayer<
         super.draw({
             uniforms: {
                 ...uniforms,
+                // Send the colormap texture to the shader.
                 colormap: new Texture2D(context.gl, {
                     data: this.props.colormap,
                     parameters: DEFAULT_TEXTURE_PARAMETERS,
@@ -77,7 +101,9 @@ export default class ColormapLayer extends BitmapLayer<
     // eslint-disable-next-line
     getShaders(): any {
         const parentShaders = super.getShaders();
+        // Overwrite the BitmapLayer's default fragment shader with ours, that does colormapping.
         parentShaders.fs = fsColormap;
+        // Add the decoder shader module to our colormap shader, so we can use the decoder function from our shader.
         parentShaders.modules.push(decoder);
         return parentShaders;
     }
@@ -91,11 +117,15 @@ export default class ColormapLayer extends BitmapLayer<
             ...defaultProps.valueDecoder.value,
             ...this.props.valueDecoder,
         };
+        // The picked color is the one in raw image, not the one after colormapping.
+        // We just need to decode that RGB color into a property float value.
         const val = decodeRGB(info.color, mergedDecoder, this.props.valueRange);
 
         return {
             ...info,
-            index: 0, // Picking color doesn't represent object index in this layer
+            // Picking color doesn't represent object index in this layer.
+            // For more details, see https://deck.gl/docs/developer-guide/custom-layers/picking
+            index: 0,
             propertyValue: val,
         };
     }
