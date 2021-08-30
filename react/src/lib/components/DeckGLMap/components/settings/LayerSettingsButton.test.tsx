e0be287e9b36ceecf950a79b933076c182d8e804
avanti-em <81212785+avanti-em@users.noreply.github.com>
2021-08-30 13:17:14 +0530

    Map component tests (#517)

--- react/src/lib/components/DeckGLMap/components/settings/LayerSettingsButton.test.tsx
+++ react/src/lib/components/DeckGLMap/components/settings/LayerSettingsButton.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import "jest-styled-components";
 import React from "react";
 import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 import { testStore, Wrapper } from "../../test/TestWrapper";
 import LayerSettingsButton from "./LayerSettingsButton";
 
@@ -19,12 +20,16 @@ describe("test layers settings button", () => {
         );
         expect(container.firstChild).toMatchSnapshot();
     });
-    xit("click to dispatch redux action", async () => {
+    it("click to dispatch redux action", async () => {
         render(
-            <LayerSettingsButton
-                layerId="colormap-layer"
-                layerType="ColormapLayer"
-            />
+            Wrapper({
+                children: (
+                    <LayerSettingsButton
+                        layerId="drawing-layer"
+                        layerType="DrawingLayer"
+                    />
+                ),
+            })
         );
         userEvent.click(screen.getByRole("button"));
         expect(screen.getByText(/draw mode/i)).toBeVisible();
@@ -37,8 +42,42 @@ describe("test layers settings button", () => {
         );
         expect(testStore.dispatch).toHaveBeenCalledTimes(1);
         expect(testStore.dispatch).toBeCalledWith({
-            payload: ["colormap-layer", "view"],
+            payload: ["drawing-layer", "view"],
             type: "spec/updateDrawingMode",
         });
+    });
+    it("should close menu when clicked on backdrop", async () => {
+        render(
+            Wrapper({
+                children: (
+                    <LayerSettingsButton
+                        layerId="drawing-layer"
+                        layerType="DrawingLayer"
+                    />
+                ),
+            })
+        );
+        userEvent.click(screen.getByRole("button"));
+        const layer_settings_menu = screen.getByRole("menu");
+        expect(layer_settings_menu).toBeInTheDocument();
+        userEvent.click(document.body);
+        await waitFor(() => expect(layer_settings_menu).not.toBeVisible());
+    });
+    it("should close menu when clicked twice on layers button", async () => {
+        render(
+            Wrapper({
+                children: (
+                    <LayerSettingsButton
+                        layerId="drawing-layer"
+                        layerType="DrawingLayer"
+                    />
+                ),
+            })
+        );
+        userEvent.click(screen.getByRole("button"));
+        const layer_settings_menu = screen.getByRole("menu");
+        expect(layer_settings_menu).toBeInTheDocument();
+        userEvent.click(screen.getByRole("button"));
+        await waitFor(() => expect(layer_settings_menu).not.toBeVisible());
     });
 });
