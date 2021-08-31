29120a1023877a1a37935fc6b917f3159b935bc3
shruthirai <44650767+shruthirai@users.noreply.github.com>
2021-08-31 09:03:27 +0200

    Scale bar feature (#520)
    * Ruler with screen distance in unit
    
    * Made changes to css and removed unwanted files
    
    * Fixing lint issues
    
    * Fixed lint errors
    
    * Changed variable name
    
    * Changing width of ruler
    
    * Dynamic change of ruler width
    
    * Code Refactor
    
    * Assigned the value for props
    
    * Changed the variable name as per review
    
    Co-authored-by: Shruthi Rai <shruthi.rai@emerson.com>

--- react/src/lib/components/DeckGLMap/components/DistanceScale.tsx
+++ react/src/lib/components/DeckGLMap/components/DistanceScale.tsx
@@ -1,26 +1,50 @@
 import React from "react";
 
 interface scaleProps {
+    // Needed the zoom value to calculate width in units
     zoom: number;
+    // Scale increment value
+    incrementValue: number;
+    // Scale bar width in pixels per unit value
+    widthPerUnit: number;
 }
-const DistanceScale: React.FC<scaleProps> = ({ zoom }: scaleProps) => {
-    const [width] = React.useState(100);
-    const pixelPerUnit = width / Math.pow(2, zoom);
-    const scaleBarStyle = {
-        width: width,
+
+const roundToStep = function (num: number, step: number) {
+    return Math.floor(num / step + 0.5) * step;
+};
+
+const DistanceScale: React.FC<scaleProps> = ({
+    zoom,
+    incrementValue,
+    widthPerUnit,
+}: scaleProps) => {
+    const [rulerWidth, setRulerWidth] = React.useState<number>(0);
+    const widthInUnits = widthPerUnit / Math.pow(2, zoom);
+    const scaleRulerStyle = {
+        width: rulerWidth,
         height: "4px",
         border: "2px solid gray",
         borderTop: "none",
         display: "inline-block",
         marginLeft: "3px",
+        marginRight: "3px",
         right: 0,
         bottom: 0,
     };
 
+    const scaleValue =
+        widthInUnits < incrementValue
+            ? Math.round(widthInUnits)
+            : roundToStep(widthInUnits, incrementValue);
+
+    React.useEffect(() => {
+        setRulerWidth(scaleValue * Math.pow(2, zoom));
+    }, [zoom]);
+
     return (
         <div style={{ bottom: 0, right: 0, position: "absolute" }}>
-            <label>{parseFloat(pixelPerUnit.toFixed(2))}</label>
-            <div style={scaleBarStyle}></div>
+            <label>{scaleValue}</label>
+            <div style={scaleRulerStyle}></div>
         </div>
     );
 };
