7c7b9dce7ec69429565cdfc67ab3ede327cf6077
nilscb <nils.christian.bonnevie@gmail.com>
2021-09-20 12:46:05 +0200

    Tooltip on edges + handle empty data.

--- react/src/lib/components/GroupTree/components/Plot/group_tree.js
+++ react/src/lib/components/GroupTree/components/Plot/group_tree.js
@@ -337,7 +337,9 @@ export default class GroupTree {
          * @param nodes - list of nodes in a tree
          */
         function updateNodes(nodes) {
-            const node = self._svg.selectAll("g.node").data(nodes, (d) => d.id);
+            const node = self._svg
+                .selectAll("g.node")
+                .data(nodes, (d) => d.id);
 
             const nodeEnter = node
                 .enter()
