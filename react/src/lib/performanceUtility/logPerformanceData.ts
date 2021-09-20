7bfe5d4e71d5e694eb2571dba50b3f6fef8f1cd8
Sanghavi <Avanti.Sanghavi@emerson.com>
2021-09-20 10:35:52 +0530

    lof performance data to a list

---
+++ react/src/lib/performanceUtility/logPerformanceData.ts
@@ -0,0 +1,6 @@
+const logToDatabase = ( id: any,
+    phase: any,
+    actualDuration: any,
+    baseDuration: any,) => {console.log(id, phase, actualDuration, baseDuration)};
+
+export default logToDatabase;
--- react/src/lib/performanceUtility/onRenderFunction.ts
+++ react/src/lib/performanceUtility/onRenderFunction.ts
@@ -1,13 +1,17 @@
+import logToDatabase from "./logPerformanceData";
 const logTimes = (
     id: any,
     phase: any,
     actualDuration: any,
     baseDuration: any,
 ) => {
-    console.log(
-        `${id}'s phase: ${phase}\nActual time: ${actualDuration} \nBase time: ${baseDuration}`
-    );
-    //return[id, phase, actualDuration, baseDuration]
+    // console.log(
+    //     `${id}'s phase: ${phase}\nActual time: ${actualDuration} \nBase time: ${baseDuration}`
+    // );
+    logToDatabase(id,
+        phase,
+        actualDuration,
+        baseDuration)
 };
 
 export default logTimes;
