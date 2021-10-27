9534f569f98e7293e30aa17f60c289170e8511ca
toF <83337645+t0oF@users.noreply.github.com>
2021-10-27 13:57:26 +0200

    Fix shaders for demos and client code (#613)
    * Fix shaders for demos and client code
    
    * Fix coding style
    
    Co-authored-by: winkler <christophe.winkler@emerson.com>

--- react/.storybook/main.js
+++ react/.storybook/main.js
@@ -16,6 +16,9 @@ module.exports = {
         config.module.rules.push({
             test: /\.scss$/,
             use: ["vue-style-loader", "css-loader", "sass-loader"],
+        }, {
+            test: /\.(fs|vs).glsl$/i,
+            use: ["raw-loader"]            
         });
         return {
             ...config,
