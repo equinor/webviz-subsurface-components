d3219ff61c8161fd2e500d2e92af09239fd55b6d
nilscb <nils.christian.bonnevie@gmail.com>
2021-12-10 10:45:35 +0100

    Small shader improvment

--- react/src/lib/components/DeckGLMap/layers/terrain/terrainmap.fs.glsl
+++ react/src/lib/components/DeckGLMap/layers/terrain/terrainmap.fs.glsl
@@ -3,7 +3,7 @@
 
 precision highp float;
 
-uniform bool hasTexture; // denne er true
+uniform bool hasTexture;
 uniform sampler2D sampler;
 uniform bool flatShading;
 uniform float opacity;
@@ -40,17 +40,19 @@ void main(void) {
       return;
    }
 
-   float op = color.w;
-   float floatScaler =  1.0 / (256.0 * 256.0 * 256.0 - 1.0);
-   vec3 rgb = color.rgb;
-   rgb *= vec3(16711680.0, 65280.0, 255.0); //255*256*256, 255*256, 255
-   float propertyValue = (rgb.r + rgb.g + rgb.b) * floatScaler;
+   if (hasTexture) {
+      float op = color.w;
+      float floatScaler =  1.0 / (256.0 * 256.0 * 256.0 - 1.0);
+      vec3 rgb = color.rgb;
+      rgb *= vec3(16711680.0, 65280.0, 255.0); //255*256*256, 255*256, 255
+      float propertyValue = (rgb.r + rgb.g + rgb.b) * floatScaler;
 
-   // temporary hardcoded colors.
-   float r = 1.0 - propertyValue * 0.4;
-   float g = 1.0 - propertyValue * 0.4;
-   float b = 1.0 * propertyValue;
-   color = vec4(r, g, b, op ); 
+      // temporary hardcoded colors.
+      float r = 1.0 - propertyValue * 0.4;
+      float g = 1.0 - propertyValue * 0.4;
+      float b = 1.0 * propertyValue;
+      color = vec4(r, g, b, op);
+   }
 
    vec3 lightColor = lighting_getLightColor(color.rgb, cameraPosition, position_commonspace.xyz, normal);
    fragColor = vec4(lightColor, color.a * opacity);
