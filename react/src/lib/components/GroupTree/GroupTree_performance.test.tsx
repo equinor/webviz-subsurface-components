f401705f054b21435affa58b4132a17086a0f75b
Sanghavi <Avanti.Sanghavi@emerson.com>
2021-12-28 12:02:42 +0530

    fix group tree performance test

--- react/src/lib/components/GroupTree/GroupTree_performance.test.tsx
+++ react/src/lib/components/GroupTree/GroupTree_performance.test.tsx
@@ -2,6 +2,7 @@ import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/react";
 import "jest-styled-components";
 import React, { Profiler } from "react";
+import * as core from "@actions/core";
 import { Wrapper } from "../GroupTree/test/TestWrapper";
 import GroupTree from "./GroupTree";
 import logTimes, { obj } from "../../performanceUtility/onRenderFunction";
@@ -57,5 +58,24 @@ describe("Test GroupTree perfomance", () => {
         );
     });
 
-    expect(obj.perf_metrics[0][2]).toBeLessThan(100);
+    const no_of_renders = obj.perf_metrics.length;
+    if (no_of_renders > 2) {
+        core.setFailed(
+            "GroupTree Component seems to have performance issues. Actual number of renders = " +
+                no_of_renders +
+                " .Expected number of renders <= 2 "
+        );
+    }
+    for (let i = 0; i < no_of_renders; i++) {
+        core.info(
+            "Render number: " + (i + 1) + " | Metrics: " + obj.perf_metrics[i]
+        );
+        if (obj.perf_metrics[i][2] > 100) {
+            core.setFailed(
+                "GroupTree Component seems to have performance issues. Actual render time:" +
+                    obj.perf_metrics[i][2] +
+                    " Expected render time - less than 100ms"
+            );
+        }
+    }
 });
