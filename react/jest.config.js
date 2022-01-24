eb2234a26728517c4f7d19b85f1fb6fc325ea72d
Sanghavi <Avanti.Sanghavi@emerson.com>
2022-01-24 13:07:43 +0530

    well-logs-tests

--- react/jest.config.js
+++ react/jest.config.js
@@ -4,7 +4,7 @@
  */
 module.exports = {
     moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|scss)$":
             "<rootDir>/__mocks__/fileMock.js",
         "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
     },
