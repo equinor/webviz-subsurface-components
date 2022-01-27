5ad03cc9a047363192cce3121ebc21a7f9562e68
Sanghavi <Avanti.Sanghavi@emerson.com>
2022-01-27 14:59:53 +0530

    fix failing snapshots test

--- react/src/lib/components/WellCompletions/components/WellCompletionsViewer.test.tsx
+++ react/src/lib/components/WellCompletions/components/WellCompletionsViewer.test.tsx
@@ -13,6 +13,16 @@ window.ResizeObserver =
         unobserve: jest.fn(),
     }));
 
+/**
+a random UUID is generated every time a tooltip is rendered, 
+which changes Jest snapshots every time they're run.
+Below snippet will replace the random class names with
+generic class name like "t00000000-0000-4000-8000-000000000000"
+ */
+jest.mock("crypto", () => ({
+    randomBytes: (num: number) => new Array(num).fill(0),
+}));
+
 describe("Test Well Completions viewer", () => {
     it("snapshot test with data", () => {
         const { container } = render(
