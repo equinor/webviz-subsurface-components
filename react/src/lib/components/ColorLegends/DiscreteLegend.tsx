400965f3c0c7fcb5b199d50e624de6690335139a
Shruthi Rai <shruthi.rai@emerson.com>
2022-01-28 07:20:08 +0100

    Make color scale select component available through Dash

---
+++ react/src/lib/components/ColorLegends/DiscreteLegend.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { DiscreteColorLegend } from "@emerson-eps/color-tables";
+import colorTables from "@emerson-eps/color-tables/src/component/color-tables.json";
+
+const DiscreteLegendWrapper = () => {
+    const discreteData = {
+        Above_BCU: [[255, 13, 186, 255], 0],
+        ABOVE: [[255, 64, 53, 255], 1],
+        H12: [[247, 255, 164, 255], 2],
+        BELOW: [[73, 255, 35, 255], 14],
+        H3: [[255, 144, 1, 255], 11],
+    };
+    const colorName = "Stratigraphy";
+    const dataObjectName = "Wells / ZONELOG";
+    const position = [16, 10];
+    const horizontal = true;
+
+    return (
+        <DiscreteColorLegend
+            discreteData={discreteData}
+            dataObjectName={dataObjectName}
+            position={position}
+            colorName={colorName}
+            colorTables={colorTables}
+            horizontal={horizontal}
+        />
+    );
+}
+
+export default DiscreteLegendWrapper;
