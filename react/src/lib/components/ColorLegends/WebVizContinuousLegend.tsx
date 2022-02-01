25565ee2a1ddce1c90d9afc740a46bafc5a5e1ad
Shruthi Rai <shruthi.rai@emerson.com>
2022-02-01 05:56:44 +0100

    Make color scale component available through Dash

---
+++ react/src/lib/components/ColorLegends/WebVizContinuousLegend.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement } from "react";
+import { ContinuousLegend } from "@emerson-eps/color-tables";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colorTables = require("@emerson-eps/color-tables/src/component/color-tables.json");
+
+const ContinuousLegendWrapper: React.FC = (): ReactElement => {
+    const min = 0;
+    const max = 0.35;
+    const dataObjectName = "Wells / PORO";
+    const position = [16, 10];
+    const horizontal = true;
+    const colorName = "Rainbow";
+
+    return (
+        <ContinuousLegend
+            min={min}
+            max={max}
+            dataObjectName={dataObjectName}
+            position={position}
+            colorName={colorName}
+            colorTables={colorTables}
+            horizontal={horizontal}
+        />
+    );
+};
+
+export default ContinuousLegendWrapper;
