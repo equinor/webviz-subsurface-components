7629cb13302f2aa08335fc9c7b256d45c7dbdfd1
Havard Bjerke <havard.bjerke@emerson.com>
2022-02-02 09:48:42 +0100

    Removed addon-redux dependency. Fixes #720.

--- react/src/lib/components/WellCompletions/redux/store.ts
+++ react/src/lib/components/WellCompletions/redux/store.ts
@@ -1,5 +1,4 @@
 import { configureStore, EnhancedStore } from "@reduxjs/toolkit";
-import { enhancer } from "addon-redux";
 import { rootReducer } from "./reducer";
 
 export type WellCompletionsState = ReturnType<typeof rootReducer>;
@@ -9,5 +8,4 @@ export const createReduxStore = (
     configureStore({
         reducer: rootReducer,
         preloadedState,
-        enhancers: [enhancer],
     });
