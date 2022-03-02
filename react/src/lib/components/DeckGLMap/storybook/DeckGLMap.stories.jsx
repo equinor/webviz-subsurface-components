4d3adfcc7cac370773a48b796359903f0b934595
Havard Bjerke <havard.bjerke@emerson.com>
2022-03-02 18:11:19 +0100

    Added axes story.

--- react/src/lib/components/DeckGLMap/storybook/DeckGLMap.stories.jsx
+++ react/src/lib/components/DeckGLMap/storybook/DeckGLMap.stories.jsx
@@ -267,16 +267,57 @@ KhMapFlat.parameters = {
 };
 
 // Map3DLayer. Properties encoded in RGB.
-const map3DLayer = exampleData[0].layers[3];
-export const KhMapMesh = EditDataTemplate.bind({});
+const meshMapLayer = {
+    "@@type": "Map3DLayer",
+    id: "mesh-layer",
+    bounds: [432205, 6475078, 437720, 6481113],
+    meshMaxError: 100,
+    mesh: "hugin_depth_25_m_normalized_margin.png",
+    meshValueRange: [2782, 3513],
+    propertyTexture: "kh_netmap_25_m_normalized_margin.png",
+    propertyValueRange: [2782, 3513],
+    rotDeg: 0,
+    contours: [0, 50.0],
+    isContoursDepth: true,
+    colorMapName: "Physics",
+    colorMapRange: [2782, 3513],
+};
+export const KhMapMesh = MinimalTemplate.bind({});
 KhMapMesh.args = {
-    ...exampleData[0],
+    id: "kh-mesh-map",
     layers: [
         {
-            ...map3DLayer,
-            visible: true,
+            ...meshMapLayer,
         },
     ],
+    bounds: [432150, 6475800, 439400, 6481500],
+    views: {
+        layout: [1, 1],
+        viewports: [
+            {
+                id: "view_1",
+                show3D: true,
+                layerIds: [],
+            },
+        ],
+    },
+};
+
+const axes = {
+    "@@type": "AxesLayer",
+    id: "axes-layer",
+    bounds: [432205, 6475078, -3500, 437720, 6481113, 0],
+};
+export const Axes = MinimalTemplate.bind({});
+KhMapMesh.args = {
+    id: "axes",
+    layers: [
+        {
+            ...meshMapLayer,
+            ...axes,
+        },
+    ],
+    bounds: [432150, 6475800, 439400, 6481500],
     views: {
         layout: [1, 1],
         viewports: [
@@ -377,24 +418,16 @@ MultiView.args = {
 
 // Experimental MapLayer. This is newer Float32 resolution for properties.
 const mapLayer = {
-        "@@type": "MapLayer",
-        id: "map-layer-float32",
-        mesh: "./volve_hugin_depth_absolute.png",
-        bounds: [
-          432205,
-          6475078,
-          437720,
-          6481113
-        ],
-        meshMaxError: 100,
-        propertyTexture: "./volve_property_ieee_float.png",
-        rotDeg: 0,
-        contours: [0, 20.0],
-        colorMapName: "Physics",
-        colorMapRange: [
-          -3071,
-          41048
-        ],
+    "@@type": "MapLayer",
+    id: "map-layer-float32",
+    mesh: "./volve_hugin_depth_absolute.png",
+    bounds: [432205, 6475078, 437720, 6481113],
+    meshMaxError: 100,
+    propertyTexture: "./volve_property_ieee_float.png",
+    rotDeg: 0,
+    contours: [0, 20.0],
+    colorMapName: "Physics",
+    colorMapRange: [-3071, 41048],
 };
 export const ExperimentalMapLayerFloat32Property = EditDataTemplate.bind({});
 ExperimentalMapLayerFloat32Property.args = {
@@ -428,4 +461,3 @@ ExperimentalMapLayerFloat32Property.parameters = {
         iframeHeight: 500,
     },
 };
-
