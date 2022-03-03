4395a96f6b4d9bc262806bf05afbc392d55f0a08
Havard Bjerke <havard.bjerke@emerson.com>
2022-03-03 13:52:28 +0100

    Fixed dash generator error for Continuous legend wrapper.

--- react/src/lib/components/ColorLegends/WebVizContinuousLegend.tsx
+++ react/src/lib/components/ColorLegends/WebVizContinuousLegend.tsx
@@ -1,12 +1,16 @@
-import React, { ReactElement } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import { ContinuousLegend } from "@emerson-eps/color-tables";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const colorTables = require("@emerson-eps/color-tables/src/component/color-tables.json");
 
-const ContinuousLegendWrapper: React.FC = (): ReactElement => {
-    const min = 0;
-    const max = 0.35;
-    const dataObjectName = "Wells / PORO";
+interface LegendProps {
+    title: string;
+    min: number;
+    max: number;
+};
+
+const ContinuousLegendWrapper: React.FC<LegendProps> = ({title, min, max}) => {
     const position = [16, 10];
     const horizontal = true;
     const colorName = "Rainbow";
@@ -15,13 +19,19 @@ const ContinuousLegendWrapper: React.FC = (): ReactElement => {
         <ContinuousLegend
             min={min}
             max={max}
-            dataObjectName={dataObjectName}
+            dataObjectName={title}
             position={position}
             colorName={colorName}
             colorTables={colorTables}
             horizontal={horizontal}
         />
     );
+};
+
+ContinuousLegendWrapper.propTypes = {
+    title: PropTypes.string.isRequired,
+    min: PropTypes.number.isRequired,
+    max: PropTypes.number.isRequired,
 };
 
 export default ContinuousLegendWrapper;
