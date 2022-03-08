aae67b4c0563ee83876f5f763dbfa78399b708c4
Shruthi Rai <shruthi.rai@emerson.com>
2022-03-08 14:09:58 +0100

    Resolve error

--- examples/example_webviz_continuouslegend.py
+++ examples/example_webviz_continuouslegend.py
@@ -4,48 +4,35 @@
 #
 # Copyright (C) 2020 - Equinor ASA.
 
-import io
-from dash import Dash, html, Input, Output, State, callback
+import Dash
 import webviz_core_components as wcc
 import webviz_subsurface_components as wsc
 
 if __name__ == "__main__":
-    Color_Tables = "https://raw.githubusercontent.com/emerson-eps/color-tables/main/react-app/src/component/color-tables.json"
+    COLOR_TABLES = (
+        "https://raw.githubusercontent.com/emerson-eps/color-tables/"
+        "main/react-app/src/component/color-tables.json"
+    )
 
     legend_obj = wsc.WebVizContinuousLegend(
         min=0,
         max=0.35,
-        title="Wells / PORO",
+        title="ContinuousLegend",
         position=[16, 10],
         colorName="Rainbow",
         horizontal=True,
-        colorTables=Color_Tables,
+        colorTables=COLOR_TABLES,
     )
 
     app = Dash(__name__)
 
-    # app.layout = wcc.FlexBox(
-    #     children=[
-    #         wcc.Frame(
-    #             style={
-    #                 "flex": 1,
-    #             },
-    #             children=[
-    #                 colormap_dropdown,
-    #                 html.Img(
-    #                     id="colormap-img",
-    #                 ),
-    #             ],
-    #         ),
-    #         wcc.Frame(
-    #             style={"flex": 10, "height": "90vh"},
-    #             children=[legend_obj],
-    #         ),
-    #     ]
-    # )
-    app.layout = html.Div(
-        style={"height": "80vh"},
-        children=legend_obj,
+    app.layout = wcc.FlexBox(
+        children=[
+            wcc.Frame(
+                style={"flex": 10, "height": "90vh"},
+                children=[legend_obj],
+            ),
+        ]
     )
 
     app.run_server(debug=True)
--- examples/example_webviz_discretelegend.py
+++ examples/example_webviz_discretelegend.py
@@ -4,54 +4,42 @@
 #
 # Copyright (C) 2020 - Equinor ASA.
 
-import io
-from dash import Dash, html, Input, Output, State, callback
+import Dash
 import webviz_core_components as wcc
 import webviz_subsurface_components as wsc
 
 if __name__ == "__main__":
-    Color_Tables = "https://raw.githubusercontent.com/emerson-eps/color-tables/main/react-app/src/component/color-tables.json"
+    COLOR_TABLES = (
+        "https://raw.githubusercontent.com/emerson-eps/color-tables/"
+        "main/react-app/src/component/color-tables.json"
+    )
+
     Discrete_Data = {
-        Above_BCU: [[255, 13, 186, 255], 0],
-        ABOVE: [[255, 64, 53, 255], 1],
-        H12: [[247, 255, 164, 255], 2],
-        BELOW: [[73, 255, 35, 255], 14],
-        H3: [[255, 144, 1, 255], 11],
+        "Above_BCU": [[255, 13, 186, 255], 0],
+        "ABOVE": [[255, 64, 53, 255], 1],
+        "H12": [[247, 255, 164, 255], 2],
+        "BELOW": [[73, 255, 35, 255], 14],
+        "H3": [[255, 144, 1, 255], 11],
     }
 
     legend_obj = wsc.WebVizDiscreteLegend(
         discreteData=Discrete_Data,
-        title="Wells / ZONELOG",
+        title="DiscreteLegend",
         position=[16, 10],
         colorName="Stratigraphy",
         horizontal=True,
-        colorTables=Color_Tables,
+        colorTables=COLOR_TABLES,
     )
 
     app = Dash(__name__)
 
-    # app.layout = wcc.FlexBox(
-    #     children=[
-    #         wcc.Frame(
-    #             style={
-    #                 "flex": 1,
-    #             },
-    #             children=[
-    #                 colormap_dropdown,
-    #                 html.Img(
-    #                     id="colormap-img",
-    #                 ),
-    #             ],
-    #         ),
-    #         wcc.Frame(
-    #             style={"flex": 10, "height": "90vh"},
-    #             children=[legend_obj],
-    #         ),
-    #     ]
-    # )
-    app.layout = html.Div(
-        style={"height": "80vh"},
-        children=legend_obj,
+    app.layout = wcc.FlexBox(
+        children=[
+            wcc.Frame(
+                style={"flex": 10, "height": "90vh"},
+                children=[legend_obj],
+            ),
+        ]
     )
 
     app.run_server(debug=True)
