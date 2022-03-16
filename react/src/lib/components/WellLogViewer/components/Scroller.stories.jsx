bde2f013e59e432fce4abcf8b8c0ec99b7fab731
Vladimir-Kokin <86053741+Vladimir-Kokin@users.noreply.github.com>
2022-03-16 21:03:16 +0300

    scroller example

--- react/src/lib/components/WellLogViewer/components/Scroller.stories.jsx
+++ react/src/lib/components/WellLogViewer/components/Scroller.stories.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import Scroller from "./Scroller";
 
-const ComponentCode = "<Scroller><div>Hello world!</div></Scroller>";
+const ComponentCode =
+    'const infoRef = React.useRef(); \r\n' +
+    'const setInfo = function (info) { \r\n' +
+    '    if (infoRef.current) infoRef.current.innerHTML = info; \r\n' +
+    '}; \r\n' +
+    'return ( \r\n' +
+    '    <div style={{ height: "92vh" }}> \r\n' +
+    '        <Scroller \r\n' +
+    '            ref={(el) => { \r\n' +
+    '                el.zoom(10, 10); \r\n' +
+    '                el.scrollTo(0.2, 0.2); \r\n' +
+    '            }} \r\n' +
+    '            onScroll={(x, y) => { \r\n' +
+    '                setInfo( \r\n' +
+    '                    "Scroll position X=" + \r\n' +
+    '                        x.toFixed(2) + \r\n' +
+    '                        ", Y=" + \r\n' +
+    '                        y.toFixed(2) \r\n' +
+    '                ); \r\n' +
+    '            }} \r\n' +
+    '        > \r\n' +
+    '            <div ref={infoRef}></div> \r\n' +
+    '        </Scroller> \r\n' +
+    '    </div> \r\n' +
+    '); \r\n'
 
 export default {
     component: Scroller,
@@ -34,16 +58,17 @@ const Template = (args) => {
             <Scroller
                 ref={(el) => {
                     el.zoom(10, 10);
+                    el.scrollTo(0.2, 0.2);
                 }}
                 onScroll={(x, y) => {
                     setInfo(
                         "Scroll position X=" +
-                            x.toFixed(3) +
+                            x.toFixed(2) +
                             ", Y=" +
-                            y.toFixed(3)
+                            y.toFixed(2)
                     );
+                    args.onScroll(x,y) // for storybook addon Actions Tab
                 }}
-                {...args}
             >
                 <div ref={infoRef}></div>
             </Scroller>
