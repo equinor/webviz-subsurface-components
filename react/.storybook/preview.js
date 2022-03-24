ae6bb75f1c17bdffa03fcc860a02b811f3f43d22
Sanghavi <Avanti.Sanghavi@emerson.com>
2022-03-24 11:47:28 +0530

    remove storybook performance add-on and capture new snapshots

--- react/.storybook/preview.js
+++ react/.storybook/preview.js
@@ -1,7 +1,3 @@
-import { addDecorator } from "@storybook/react";
-import { withPerformance } from "storybook-addon-performance";
-
-addDecorator(withPerformance);
 export const parameters = {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
