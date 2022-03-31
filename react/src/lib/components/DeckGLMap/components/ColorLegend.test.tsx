790cbb4289146ec40682e04bec3111eeee52c166
Sanghavi <Avanti.Sanghavi@emerson.com>
2022-03-31 13:46:31 +0530

    fix failing tests

--- react/src/lib/components/DeckGLMap/components/ColorLegend.test.tsx
+++ react/src/lib/components/DeckGLMap/components/ColorLegend.test.tsx
@@ -4,14 +4,20 @@ import "@testing-library/jest-dom";
 import React from "react";
 import ColorLegend from "./ColorLegend";
 import { testState } from "../test/testReduxState";
+import { Layer } from "deck.gl";
+import { LayerProps } from "@deck.gl/core/lib/layer";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const example_color_table = require("../../../../demo/example-data/color-tables.json");
 describe("Test Color Legend", () => {
     it("snapshot test", () => {
+        type NewType = LayerProps<unknown>;
+
         const { container } = render(
             <ColorLegend
-                layers={testState.layers}
+                layers={
+                    testState.layers as unknown as Layer<unknown, NewType>[]
+                }
                 colorTables={example_color_table}
                 visible={true}
                 position={[16, 10]}
