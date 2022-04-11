16b21b51bee375f8f3b1bcc91b0a9097c8d5fdb1
Shadab Khan <Shadab.Khan@Emerson.com>
2022-04-11 10:24:21 +0530

    Refactor schema storybook

---
+++ react/src/lib/components/DeckGLMap/storybook/schemaValidation/DeckGLMapSchema.stories.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import DeckGLMap from "../../DeckGLMap";
+import { sampleLogData, sampleWellsData } from "./sampleData";
+import exampleData from "../../../../../demo/example-data/deckgl-map.json";
+
+export default {
+    component: DeckGLMap,
+    title: "DeckGLMap/schemaValidation",
+};
+
+// Template for when edited data needs to be captured.
+const DeckGLMapTemplate = (args) => {
+    return <DeckGLMap {...args} />;
+};
+
+export const wellsLayerValidation = DeckGLMapTemplate.bind();
+wellsLayerValidation.args = {
+    ...exampleData[0],
+    layers: [
+        {
+            ...exampleData[0].layers[4],
+            data: sampleWellsData,
+            logData: sampleLogData,
+        },
+    ],
+    legend: {
+        visible: false,
+    },
+    checkDatafileSchema: true,
+};
