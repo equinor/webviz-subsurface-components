5205dc07db0eeb22a2a8af5fa5f2339102caa8d8
nilscb <nils.christian.bonnevie@gmail.com>
2022-05-04 21:53:05 +0200

    Small fix

--- react/src/lib/components/DeckGLMap/layers/grid/vertex_lines.glsl.ts
+++ react/src/lib/components/DeckGLMap/layers/grid/vertex_lines.glsl.ts
@@ -4,13 +4,12 @@ const gridVertex = `#version 300 es
 precision highp float;
 
 in vec3 positions;
-
-in vec3 color; // XXX denne kan nok fjernes
+in vec3 color;
 
 out vec3 vColor;
 
 void main(void) {
-   vColor = vec3(0.25, 0.25, 0.25); // color
+   vColor = vec3(0.0, 0.0, 0.0);
 
    vec3 position_commonspace = project_position(positions);
    gl_Position = project_common_position_to_clipspace(vec4(position_commonspace, 0.0));
