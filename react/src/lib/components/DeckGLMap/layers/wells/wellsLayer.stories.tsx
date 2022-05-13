01a2c27ac2e44467555f431e130d659acf609e94
Håvard Bjerke <hkfbjerke@gmail.com>
2022-05-13 15:41:11 +0200

    Added storybook example for color table selector (#989)
    * Added storybook examples for discrete and continuous well logs.
    
    * Added storybook examples for discrete and continuous well logs.
    
    * Added story for selecting color table.
    
    * Applied eslint formatting.
    
    * Fixed color table selector logic.
    
    * Applied eslint formatting.
    
    Co-authored-by: Havard Bjerke <havard.bjerke@emerson.com>

--- react/src/lib/components/DeckGLMap/layers/wells/wellsLayer.stories.tsx
+++ react/src/lib/components/DeckGLMap/layers/wells/wellsLayer.stories.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import DeckGLMap from "../../DeckGLMap";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { NativeSelect } from "@equinor/eds-core-react";
 
 export default {
     component: DeckGLMap,
+    title: "DeckGLMap / Wells Layer",
 } as ComponentMeta<typeof DeckGLMap>;
 
 const Template: ComponentStory<typeof DeckGLMap> = (args) => (
@@ -29,13 +31,65 @@ const defaultProps = {
     ],
 };
 
-// Volve kh netmap data, flat surface
+const continuousLogsLayer = {
+    ...defaultProps.layers[0],
+    refine: false,
+    outline: false,
+    logData: "./volve_logs.json",
+    logrunName: "BLOCKING",
+    logName: "PORO",
+    logColor: "Physics",
+};
+
+// Volve wells default example.
 export const VolveWells = Template.bind({});
 VolveWells.args = defaultProps;
 VolveWells.parameters = {
     docs: {
         description: {
             story: "Volve wells example",
+        },
+        inlineStories: false,
+        iframeHeight: 500,
+    },
+};
+
+// Volve wells with logs.
+//
+export const DiscreteWellLogs = Template.bind({});
+DiscreteWellLogs.args = {
+    ...defaultProps,
+    layers: [
+        {
+            ...defaultProps.layers[0],
+            refine: false,
+            outline: false,
+            logData: "./volve_logs.json",
+            logrunName: "BLOCKING",
+            logName: "ZONELOG",
+            logColor: "Stratigraphy",
+        },
+    ],
+};
+DiscreteWellLogs.parameters = {
+    docs: {
+        description: {
+            story: "Volve wells example with well logs.",
+        },
+        inlineStories: false,
+        iframeHeight: 500,
+    },
+};
+
+export const ContinuousWellLogs = Template.bind({});
+ContinuousWellLogs.args = {
+    ...defaultProps,
+    layers: [continuousLogsLayer],
+};
+ContinuousWellLogs.parameters = {
+    docs: {
+        description: {
+            story: "Volve wells example with well logs.",
         },
         inlineStories: false,
         iframeHeight: 500,
@@ -227,4 +281,42 @@ Wells3dDashed.parameters = {
         inlineStories: false,
         iframeHeight: 500,
     },
+};
+
+export const ContinuousColorTable: React.FC = () => {
+    const [colorTable, setColorTable] = useState("Physics");
+
+    const mapProps = React.useMemo(() => {
+        return {
+            ...defaultProps,
+            layers: [
+                {
+                    ...continuousLogsLayer,
+                    logColor: colorTable,
+                },
+            ],
+        };
+    }, [colorTable]);
+
+    const handleOnChange = (event: React.FormEvent) => {
+        setColorTable((event.target as HTMLInputElement)?.value);
+    };
+    return (
+        <>
+            <NativeSelect
+                id={"test"}
+                label={"Color table"}
+                value={colorTable}
+                onChange={handleOnChange}
+            >
+                <option key={"Physics"}>{"Physics"}</option>
+                <option key={"Rainbow"}>{"Rainbow"}</option>
+            </NativeSelect>
+            {
+                <div style={{ height: "80vh", position: "relative" }}>
+                    <DeckGLMap {...mapProps} />
+                </div>
+            }
+        </>
+    );
 };
