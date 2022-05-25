2ca794eb9e83f20700c97deea3670b878d2ec45a
Vladimir-Kokin <86053741+Vladimir-Kokin@users.noreply.github.com>
2022-05-25 22:23:10 +0300

    Update WellLogViewer.test.tsx

--- react/src/lib/components/WellLogViewer/WellLogViewer.test.tsx
+++ react/src/lib/components/WellLogViewer/WellLogViewer.test.tsx
@@ -7,7 +7,7 @@ import WellLogViewer from "./WellLogViewer";
 import { WellLogController } from "./components/WellLogView";
 
 const exampleTemplate = require("../../../demo/example-data/welllog_template_1.json");
-const exampleWellLog = require("../../../demo/example-data/L898MUD.json");
+const exampleWellLog = require("../../../demo/example-data/L898MUD.json")[0];
 const exampleColorTable = require("../../../demo/example-data/color-tables.json");
 
 window.ResizeObserver =
