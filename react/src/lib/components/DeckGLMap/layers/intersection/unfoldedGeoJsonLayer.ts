22842b6844b6e317ce29db37b44f43a757685092
Havard Bjerke <havard.bjerke@emerson.com>
2022-06-17 16:52:57 +0200

    Applied lint conditions.

--- react/src/lib/components/DeckGLMap/layers/intersection/unfoldedGeoJsonLayer.ts
+++ react/src/lib/components/DeckGLMap/layers/intersection/unfoldedGeoJsonLayer.ts
@@ -1,5 +1,4 @@
-import { GeoJsonLayer, GeoJsonLayerProps, PathLayer } from "@deck.gl/layers";
-import UnfoldedPathLayer from "./unfoldedPathLayer";
+import { GeoJsonLayer, GeoJsonLayerProps } from "@deck.gl/layers";
 import { Feature } from "geojson";
 import { zip } from "lodash";
 import { distance } from "mathjs";
@@ -9,21 +8,21 @@ interface UnfoldedGeoJsonLayerProps<D> extends GeoJsonLayerProps<D> {
 }
 
 function getUnfoldedPath(object: any) {
-	const worldCoordinates = object.geometry.coordinates;
-	const z = worldCoordinates.map(v => v[2]);
-	const delta = worldCoordinates.map((v, i, coordinates) => {
-		const prev = coordinates[i - 1] || v;
-		return distance([prev[0], prev[1]], [v[0], v[1]]);
-	});
-	let a = [];
-	delta.forEach(d => {
-		const prev = a.at(-1) || 0;
-		a.push(d + prev);
-	})
-	const planeY = 2000;
-	const vAbscissa = zip(a, [...a].fill(planeY), z);
+    const worldCoordinates = object.geometry.coordinates;
+    const z = worldCoordinates.map((v) => v[2]);
+    const delta = worldCoordinates.map((v, i, coordinates) => {
+        const prev = coordinates[i - 1] || v;
+        return distance([prev[0], prev[1]], [v[0], v[1]]);
+    });
+    const a = [];
+    delta.forEach((d) => {
+        const prev = a.at(-1) || 0;
+        a.push(d + prev);
+    });
+    const planeY = 2000;
+    const vAbscissa = zip(a, [...a].fill(planeY), z);
 
-	return vAbscissa;
+    return vAbscissa;
 }
 
 export default class UnfoldedGeoJsonLayer<D = Feature> extends GeoJsonLayer<
@@ -38,12 +37,11 @@ export default class UnfoldedGeoJsonLayer<D = Feature> extends GeoJsonLayer<
         const path_layer_id = layers.findIndex(
             (layer) => layer?.[1].constructor.name == "PathLayer"
         );
-	const pathLayer = layers[path_layer_id]?.[1];
-	const unfoldedPathLayer = pathLayer.clone(
-		{
-			getPath: getUnfoldedPath,
-			});
-	return [unfoldedPathLayer];
+        const pathLayer = layers[path_layer_id]?.[1];
+        const unfoldedPathLayer = pathLayer.clone({
+            getPath: getUnfoldedPath,
+        });
+        return [unfoldedPathLayer];
     }
 }
 
