55e878c41b73f6e093039f4556e5e2f11e3ff413
Havard Bjerke <havard.bjerke@emerson.com>
2022-07-05 14:39:22 +0200

    Fixed colored label example.

--- react/src/lib/components/DeckGLMap/layers/axes/axesLayer.stories.tsx
+++ react/src/lib/components/DeckGLMap/layers/axes/axesLayer.stories.tsx
@@ -1,47 +1,60 @@
 import React from "react";
 import DeckGL from "@deck.gl/react";
-import {OrthographicView} from '@deck.gl/core';
-import {GeoJsonLayer} from '@deck.gl/layers';
-import {rgb} from "d3-color";
+import { OrthographicView } from "@deck.gl/core";
+import { rgb } from "d3-color";
+import { RGBAColor } from "@deck.gl/core/utils/color";
 
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import AxesLayer from "./axesLayer";
+import { DeckGLProps } from "@deck.gl/react/deckgl";
 
 export default {
     component: DeckGL,
     title: "DeckGLMap / Axes",
-
 } as ComponentMeta<typeof DeckGL>;
 
 const layerProps = {
-	name: "axes",
-        bounds: [-100, -100, 0, 100, 100, 100] as [number, number, number, number, number, number],
+    name: "axes",
+    bounds: [-100, -100, 0, 100, 100, 100] as [
+        number,
+        number,
+        number,
+        number,
+        number,
+        number
+    ],
 };
 
 function getRgba(color: string) {
-	const c = rgb(color);
-	return [c.r, c.g, c.b, c.opacity * 255];
+    const c = rgb(color);
+    return [c.r, c.g, c.b, c.opacity * 255];
 }
 
 export const Baseline: ComponentStory<typeof DeckGL> = (args) => {
-	args.layers = [new AxesLayer({...layerProps})];
-	args.views = new OrthographicView();
-        return <DeckGL {...args} />;
+    args.layers = [new AxesLayer({ ...layerProps })];
+    args.views = [new OrthographicView({})];
+    return <DeckGL {...args} />;
+};
+
+function ColoredLabels(props: { labelColor: string }) {
+    const layers = [
+        new AxesLayer({
+            ...layerProps,
+            labelColor: getRgba(props.labelColor) as RGBAColor,
+        }),
+    ];
+    const views = [new OrthographicView({})];
+    return <DeckGL layers={layers} views={views} />;
 }
 
-export const DarkMode: ComponentStory<typeof DeckGL> = (args) => {
-	args.layers = [new AxesLayer({
-		...layerProps,
-		labelColor: getRgba(args.labelColor),
-		})];
-	args.views = new OrthographicView();
-        return <DeckGL {...args} />;
-}
+export const DarkMode: ComponentStory<typeof ColoredLabels> = (args) => {
+    return <ColoredLabels {...args} />;
+};
 
 DarkMode.args = {
-	labelColor: "white",
-}
+    labelColor: "white",
+};
 
 DarkMode.parameters = {
-	backgrounds: {default: "dark",},
-}
+    backgrounds: { default: "dark" },
+};
