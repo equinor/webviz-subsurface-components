45638ef926f8ebae3167fa6475cc0f3c83e24bb2
Sanghavi <Avanti.Sanghavi@alias.local>
2022-09-15 12:40:04 +0530

    fix cypress tests except workflow file

--- react/cypress/component/defaultMap.cy.js
+++ react/cypress/component/defaultMap.cy.js
@@ -2,36 +2,15 @@
 import * as React from "react";
 import { mount } from "@cypress/react";
 import { composeStories } from "@storybook/testing-react";
-import { render } from "@testing-library/react";
 
 import * as stories from "../../src/lib/components/DeckGLMap/storybook/DeckGLMap.stories";
-import * as storiesC from "../../src/lib/components/DeckGLMap/DeckGLMap.stories";
-import { Map } from "../../../react/src/lib/components/DeckGLMap/components/Map";
 
-const { Default, MultiView } = composeStories(stories);
-const { TooltipApi, TooltipStyle } = composeStories(storiesC);
+const { Default } = composeStories(stories);
 
-//render().unmount();
-
-describe("Map and well", () => {
-    it("should diplay default", () => {
-        mount(<Map />);
-        cy.wait(10000);
-    });
-    it("should diplay default", () => {
+describe("Map Story Tests", () => {
+    it("should diplay default story", () => {
         mount(<Default />);
-        cy.wait(10000);
-    });
-    it("should display tooltpi", () => {
-        mount(<TooltipApi />);
-        cy.wait(10000);
-    });
-    it("should display tooltpi", () => {
-        mount(<TooltipStyle />);
-        cy.wait(10000);
-    });
-    it("should display multiwell", () => {
-        mount(<MultiView />);
-        cy.wait(10000);
+        cy.get("svg[role='progressbar']")
+        cy.get("svg[role='progressbar']", {timeout: 30000}).should("not.exist")
     });
 });
