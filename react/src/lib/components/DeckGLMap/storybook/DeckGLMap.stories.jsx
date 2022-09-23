b513526eb2fba62a1fb4cfeea73e682757a48c51
shruthirai <44650767+shruthirai@users.noreply.github.com>
2022-09-23 11:14:13 +0200

    Feature for editing breakpoints (#1201)
    * Feature for editing breakpoints
    
    * Fix for legend update on changing scale
    
    * fix colormaplayer update issue based on color selector
    
    * Keep endbreakpoints fixed
    
    * UI Improvement
    
    * removed sorting
    
    * Add comments
    
    * make breakpoint prop as optional
    
    Co-authored-by: Shruthi Rai <shruthi.rai@emerson.com>
    Co-authored-by: Shadab Khan <shadabskhan@hotmail.com>

--- react/src/lib/components/DeckGLMap/storybook/DeckGLMap.stories.jsx
+++ react/src/lib/components/DeckGLMap/storybook/DeckGLMap.stories.jsx
@@ -668,6 +668,12 @@ const mapDataTemplate = (args) => {
     const [getColorName, setColorName] = React.useState("Rainbow");
     const [colorRange, setRange] = React.useState();
     const [isAuto, setAuto] = React.useState();
+    const [breakPoint, setBreakPoint] = React.useState();
+
+    // user defined breakpoint(domain)
+    const userDefinedBreakPoint = React.useCallback((data) => {
+        if (data) setBreakPoint(data);
+    }, []);
 
     // Get selected legend color name from color selector
     const colorNameFromSelector = React.useCallback((data) => {
@@ -688,9 +694,11 @@ const mapDataTemplate = (args) => {
                 colorRange && isAuto == false
                     ? colorRange
                     : layers[0].colorMapRange,
+            // Passing "breakpoint" is temporary solution for now since the colortable does not save the edited breakpoints
+            // When save functionality of breakpoint is done, prop "breakpoint" will be removed from here
+            breakPoint: breakPoint ? breakPoint : [],
         },
     ];
-
     return (
         <div>
             <div
@@ -703,8 +711,10 @@ const mapDataTemplate = (args) => {
             >
                 <ColorLegend
                     {...args}
+                    colorName={getColorName}
                     getColorName={colorNameFromSelector}
                     getColorRange={userDefinedRange}
+                    getBreakpointValue={userDefinedBreakPoint}
                 />
             </div>
             <DeckGLMap {...args} layers={updatedLayerData} />
