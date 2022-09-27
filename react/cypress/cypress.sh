2faadaf7f1c49de09c16ddd0521ec622d2838a30
Sanghavi <Avanti.Sanghavi@alias.local>
2022-09-27 13:40:47 +0530

    fix cypress.sh

--- react/cypress/cypress.sh
+++ react/cypress/cypress.sh
@@ -1,16 +1,16 @@
 git ls-files --others --exclude-standard > files.txt
 while IFS= read -r file
 do
-    if [[ $file = cypress-visual-screenshots/diff/* ]]; then
+    if [[ $file = react/cypress-visual-screenshots/diff/* ]]; then
         echo $file >> files2.txt
     fi
         done < files.txt
 
 while IFS= read -r file
 do
-    snapshot_name=${file:32}
-    baseline_location=cypress-visual-screenshots/baseline/${snapshot_name}
-    actual_location=cypress-visual-screenshots/comparison/${snapshot_name}
+    snapshot_name=${file:44}
+    baseline_location=react/cypress-visual-screenshots/baseline/${snapshot_name}
+    actual_location=react/cypress-visual-screenshots/comparison/${snapshot_name}
 
     # move the actual snapshot to base directory
     mv $actual_location $baseline_location
