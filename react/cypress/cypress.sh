c38b0dc23fb272a8cea88c9b149968184ea7b911
Sanghavi <Avanti.Sanghavi@alias.local>
2022-09-27 14:15:46 +0530

    investigation on snapshot snap

--- react/cypress/cypress.sh
+++ react/cypress/cypress.sh
@@ -1,4 +1,4 @@
-git ls-files --others --exclude-standard > files.txt
+echo git ls-files --others --exclude-standard > files.txt
 while IFS= read -r file
 do
     if [[ $file = react/cypress-visual-screenshots/diff/* ]]; then
@@ -9,8 +9,11 @@ do
 while IFS= read -r file
 do
     snapshot_name=${file:44}
+    echo $snapshot_name
     baseline_location=react/cypress-visual-screenshots/baseline/${snapshot_name}
+    echo $baseline_location
     actual_location=react/cypress-visual-screenshots/comparison/${snapshot_name}
+    echo $actual_location
 
     # move the actual snapshot to base directory
     mv $actual_location $baseline_location
