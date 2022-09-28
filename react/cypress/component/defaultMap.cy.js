f43eef8ef9932abccb30c170165cb78a3dae4090
Sanghavi <Avanti.Sanghavi@alias.local>
2022-09-28 09:21:15 +0530

    add hard coded wait

--- react/cypress/component/defaultMap.cy.js
+++ react/cypress/component/defaultMap.cy.js
@@ -15,6 +15,7 @@ describe("Map Story Tests", () => {
         mount(<Default />);
         cy.get("svg[role='progressbar']")
         cy.get("svg[role='progressbar']", {timeout: 180000}).should("not.exist")
+        cy.wait(1000)
         cy.compareSnapshot('default-map-story')
     });
 });
