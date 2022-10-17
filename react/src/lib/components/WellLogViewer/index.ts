bd145cc51ceb639a4b7a9d2a164d3fa41270b074
Vladimir-Kokin <86053741+Vladimir-Kokin@users.noreply.github.com>
2022-10-17 12:38:05 +0300

    Update index.ts

--- react/src/lib/components/WellLogViewer/index.ts
+++ react/src/lib/components/WellLogViewer/index.ts
@@ -1,8 +1,1 @@
 export { default } from "./WellLogViewer";
-export { default as WellLogViewer } from "./WellLogViewer";
-export { default as SyncLogViewer } from "./SyncLogViewer";
-
-export { default as WellLogView } from "./components/WellLogView";
-export { default as WellLogViewWithScroller } from "./components/WellLogViewWithScroller";
-
-export { default as WellLogSpacer } from "./components/WellLogSpacer";
