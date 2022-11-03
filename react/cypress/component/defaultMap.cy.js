a7e01fd5276d60b06440f8d79d543412203211aa
Sanghavi <Avanti.Sanghavi@alias.local>
2022-11-03 13:36:12 +0530

    remove intentional test fail

--- react/cypress/component/defaultMap.cy.js
+++ react/cypress/component/defaultMap.cy.js
@@ -18,7 +18,7 @@ describe("Map Story Tests", () => {
     } ,() => {
       mount(<Default />);
       cy.get("svg[role='progressbar']")
-      //cy.get("svg[role='progressbar']", {timeout: 180000}).should("not.exist")
+      cy.get("svg[role='progressbar']", {timeout: 30000}).should("not.exist")
       cy.wait(1000)
       cy.compareSnapshot('default-map-story')
   });
