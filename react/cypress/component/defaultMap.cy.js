1cdb921cc1f2da7d0d61239013242866a2ac0dba
Sanghavi <avanti.sanghavi@aspentech.com>
2022-11-25 14:51:22 +0530

    hard waits

--- react/cypress/component/defaultMap.cy.js
+++ react/cypress/component/defaultMap.cy.js
@@ -26,42 +26,29 @@ describe("Map Story Tests", () => {
   it("should diplay default story",() => {
       mount(<Default />);
       cy.get("svg[role='progressbar']")
-      cy.get("svg[role='progressbar']", {timeout: 30000}).should("not.exist")
-      cy.wait(1000)
+      cy.wait(15000)
       cy.compareSnapshot('default-map-story')
   });
   
   it("should diplay default story zoomed in",() => {
     mount(<Default />);
     cy.get("svg[role='progressbar']")
-    cy.get("svg[role='progressbar']", {timeout: 30000}).should("not.exist")
-    cy.wait(1000)
-    for (let i = 0; i < 4; i++) {
-      cy.get("#view-view_1_2D").trigger("wheel", {
-        deltaY: -66.666666,
-        wheelDelta: 120,
-        wheelDeltaX: 0,
-        wheelDeltaY: 120,
-      });
+    cy.wait(15000)
+    for (let i = 0; i < 2; i++) {
+      cy.get(mainView2D).type("{+}");
+      cy.wait(1000);
     }
-    cy.wait(1000);
     cy.compareSnapshot('default-map-story_zoomed_in')
 });
 
 it("should diplay default story zoomed out",() => {
   mount(<Default />);
   cy.get("svg[role='progressbar']")
-  cy.get("svg[role='progressbar']", {timeout: 30000}).should("not.exist")
-  cy.wait(1000)
-  for (let i = 0; i < 5; i++) {
-    cy.get("#view-view_1_2D").trigger("wheel", {
-      deltaY: 66.666666,
-      wheelDelta: 120,
-      wheelDeltaX: 0,
-      wheelDeltaY: 120,
-    });
+  cy.wait(15000)
+  for (let i = 0; i < 2; i++) {
+    cy.get(mainView2D).type("{-}");
+    cy.wait(2000);
   }
-  cy.wait(1000);
   cy.compareSnapshot('default-map-story_zoomed_out')
 });
 });
