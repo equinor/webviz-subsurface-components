ab495986293a73c43d5528f9626a7cdd324f7411
nilscb <nils.christian.bonnevie@gmail.com>
2022-11-29 16:01:22 +0100

    Fixes issue 1138: [NGRM] - Map View Axes - in 2D, the axes should be … (#1329)
    * Fixes issue 1138: [NGRM] - Map View Axes - in 2D, the axes should be static items on the side of the viewport
    
    * typecheck
    
    * Specify margins in pixels.

--- react/src/lib/components/DeckGLMap/layers/index.ts
+++ react/src/lib/components/DeckGLMap/layers/index.ts
@@ -8,12 +8,14 @@ export { default as PieChartLayer } from "./piechart/pieChartLayer";
 export { default as GridLayer } from "./grid/gridLayer";
 export { default as FaultPolygonsLayer } from "./fault_polygons/faultPolygonsLayer";
 export { default as AxesLayer } from "./axes/axesLayer";
+export { default as Axes2DLayer } from "./axes2d/axes2DLayer";
 export { default as SelectableGeoJsonLayer } from "./selectable_geojson/selectableGeoJsonLayer";
 export { default as NorthArrow3DLayer } from "./northarrow/northArrow3DLayer";
 export { default as UnfoldedGeoJsonLayer } from "./intersection/unfoldedGeoJsonLayer";
 export { default as Grid3DLayer } from "./grid3d/grid3dLayer";
 
 export { AxesLayerProps } from "./axes/axesLayer";
+export { Axes2DLayerProps } from "./axes2d/axes2DLayer";
 export { ColormapLayerProps } from "./colormap/colormapLayer";
 export { DrawingLayerProps } from "./drawing/drawingLayer";
 export { FaultPolygonsLayerProps } from "./fault_polygons/faultPolygonsLayer";
