99692f2fb2c99481dda47dd92fa37e6229e196c8
Sanghavi <avanti.sanghavi@aspentech.com>
2022-12-01 11:13:14 +0530

    add two packages and full screen ability

--- react/cypress.config.ts
+++ react/cypress.config.ts
@@ -28,7 +28,19 @@ export default defineConfig({
   e2e: {
     video: false,
     setupNodeEvents(on, config) {
-      getCompareSnapshotsPlugin(on, config)
+      getCompareSnapshotsPlugin(on, config),
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        if (browser.family === "chromium" && browser.name !== "electron") {
+          launchOptions.args.push("--start-fullscreen");
+
+          return launchOptions;
+        }
+        if (browser.name === "electron") {
+          launchOptions.preferences.fullscreen = true;
+
+          return launchOptions;
+        }
+      });
     },
   },
 });
