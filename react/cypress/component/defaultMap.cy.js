27340fdae4e1592e677326944337088a47358101
Sanghavi <avanti.sanghavi@aspentech.com>
2022-12-13 13:45:31 +0530

    improve tests

--- react/cypress/component/defaultMap.cy.js
+++ react/cypress/component/defaultMap.cy.js
@@ -12,43 +12,37 @@ render(<Default />).unmount();
 
 describe("Map Story Tests", () => {
   it("activate hooks",() => {
-    Cypress.on('fail', (error, runnable) => {
-      if (error.message.includes('not to exist in the DOM, but it was continuously found')) {
-      return false
-      }
-    })
-    mount(<Default />)
-    cy.get("svg[role='progressbar']")
-    cy.get("svg[role='progressbar']", {timeout: 15000}).should("not.exist")
+    render(<Default />)
+    cy.wait(5000)
   });
   
   it("should diplay default story",() => {
       mount(<Default />);
       cy.get("svg[role='progressbar']")
-      cy.wait(15000)
-      cy.get("#view-view_1_2D").compareSnapshot('default-map-story')
+      cy.get("svg[role='progressbar']", {timeout: 60000}).should("not.exist")
+      cy.get("#DeckGL-Map-wrapper").compareSnapshot('default-map-story')
   });
   
   it("should diplay default story zoomed in",() => {
     mount(<Default />);
     cy.get("svg[role='progressbar']")
-    cy.wait(15000)
+    cy.get("svg[role='progressbar']", {timeout: 60000}).should("not.exist")
     for (let i = 0; i < 2; i++) {
-      cy.get("#view-view_1_2D").type("{+}");
+      cy.get("#DeckGL-Map-wrapper").type("{+}");
       cy.wait(1000);
     }
-    cy.get("#view-view_1_2D").compareSnapshot('default-map-story_zoomed_in')
+    cy.get("#DeckGL-Map-wrapper").compareSnapshot('default-map-story_zoomed_in')
 });
 
 it("should diplay default story zoomed out",() => {
   mount(<Default />);
   cy.get("svg[role='progressbar']")
-  cy.wait(15000)
+  cy.get("svg[role='progressbar']", {timeout: 60000}).should("not.exist")
   for (let i = 0; i < 2; i++) {
-    cy.get("#view-view_1_2D").type("{-}");
+    cy.get("#DeckGL-Map-wrapper").type("{-}");
     cy.wait(1000);
   }
-  cy.get("#view-view_1_2D").compareSnapshot('default-map-story_zoomed_out')
+  cy.get("#DeckGL-Map-wrapper").compareSnapshot('default-map-story_zoomed_out')
 });
 
 });
