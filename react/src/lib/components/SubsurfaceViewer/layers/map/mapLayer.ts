b8a4df5e3571d097f7818a1e988e4297762fdecb
nilscb <nils.christian.bonnevie@gmail.com>
2023-01-18 11:45:14 +0100

    Typecheck. Forgot some properties.

--- react/src/lib/components/SubsurfaceViewer/layers/map/mapLayer.ts
+++ react/src/lib/components/SubsurfaceViewer/layers/map/mapLayer.ts
@@ -1,4 +1,4 @@
-import { CompositeLayer, UpdateParameters } from "@deck.gl/core/typed";
+import { CompositeLayer, Color, UpdateParameters } from "@deck.gl/core/typed";
 import privateMapLayer, { Material } from "./privateMapLayer";
 import { ExtendedLayerProps, colorMapFunctionType } from "../utils/layerTools";
 import { getModelMatrix } from "../utils/layerTools";
@@ -165,20 +165,21 @@ export interface MapLayerProps<D> extends ExtendedLayerProps<D> {
     frame: Frame;
 
     /**  Url to the properties (ex, poro or perm values).
-     If the number of property values equals the number of depth values
-     the property values will be placed at the nodes and the cell (4 neigboring nodes)
-     color will be linearly interpolated over the cell.
-     If the number of property values equals one less than the depth values in
-     each direction then the property values will be pr cell and the cell will be constant
-     colored.
+     * If the number of property values equals the number of depth values
+     * the property values will be placed at the nodes and the cell (4 neigboring nodes)
+     * color will be linearly interpolated over the cell.
+     * If the number of property values equals one less than the depth values in
+     * each direction then the property values will be pr cell and the cell will be constant
+     * colored.
+     */
     propertiesUrl: string;
 
     /**  Contourlines reference point and interval.
-     A value of [-1.0, -1.0] will disable contour lines.
-     Contour lines will also not be activated if cells are constant colored
-     and "isContoursDepth" is set to false. I.e. constant properties within cells and contourlines
-     to be calculated for properties and not depths.
-     default value: [-1.0, -1.0]
+     * A value of [-1.0, -1.0] will disable contour lines.
+     * Contour lines will also not be activated if cells are constant colored
+     * and "isContoursDepth" is set to false. I.e. constant properties within cells and contourlines
+     * to be calculated for properties and not depths.
+     * default value: [-1.0, -1.0]
      */
     contours: [number, number];
 
@@ -195,23 +196,25 @@ export interface MapLayerProps<D> extends ExtendedLayerProps<D> {
     gridLines: boolean;
 
     /**  Name of color map. E.g "PORO"
+     */
     colorMapName: string;
 
     /**  Use color map in this range.
-    */
+     */
     colorMapRange: [number, number];
 
     /**  Clamp colormap to this color at ends.
-     Given as array of three values (r,g,b) e.g: [255, 0, 0]
-     If not set or set to true, it will clamp to color map min and max values.
-     If set to false the clamp color will be completely transparent.
+     * Given as array of three values (r,g,b) e.g: [255, 0, 0]
+     * If not set or set to true, it will clamp to color map min and max values.
+     * If set to false the clamp color will be completely transparent.
+     */
     colorMapClampColor: Color | undefined | boolean;
 
     /**  Optional function property.
-    * If defined this function will override the color map.
-    * Takes a value in the range [0,1] and returns a color.
-    * E.g. (x) => [x * 255, x * 255, x * 255]
-    */
+     * If defined this function will override the color map.
+     * Takes a value in the range [0,1] and returns a color.
+     * E.g. (x) => [x * 255, x * 255, x * 255]
+     */
     colorMapFunction?: colorMapFunctionType | false;
 
     /**  Surface material properties.
@@ -228,15 +231,17 @@ export interface MapLayerProps<D> extends ExtendedLayerProps<D> {
     material: Material;
 
     /**  Will calculate normals for each vertex and enable phong shading.
-    * If not set the shader will calculate constant normal for each triangle.
+     * If not set the shader will calculate constant normal for each triangle.
+     */
     smoothShading: boolean;
 
-    // Enable/disable depth testing when rendering layer. Default true.
+    /** Enable/disable depth testing when rendering layer. Default true.
+     */
     depthTest: boolean;
 
     /**  If true means that input z values are interpreted as depths.
-    * For example depth of z = 1000 corresponds to -1000 on the z axis. Default true.
-    */
+     * For example depth of z = 1000 corresponds to -1000 on the z axis. Default true.
+     */
     isZDepth: boolean;
 }
 
