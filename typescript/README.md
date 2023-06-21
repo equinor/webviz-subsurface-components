16d3539c090bb5f48f027708cd47f4553bfcbf35
Anders Fredrik Kiær <31612826+anders-kiaer@users.noreply.github.com>
2023-06-21 22:42:59 +0200

    Split mono-repo in different `npm` packages (#1543)

---
+++ typescript/README.md
@@ -0,0 +1,61 @@
+## How to consume the components
+
+You can install the package in your own project using the [`npmjs` deployed version](https://www.npmjs.com/package/@webviz/subsurface-components):
+
+```
+npm install @webviz/subsurface-components
+```
+
+In order to consume this package, some loaders for specific file types are required.
+Your project needs to be able to load `css` and `scss` files.
+
+### Using Webpack
+
+When using Webpack as a bundler, you can simply add
+
+```json
+{
+    test: /\.s[ac]ss$/i,
+    use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+    ],
+}
+```
+
+to the module rules in your `webpack.config.js` file. Make sure you have the required `devDependencies` installed:
+
+-   `style-loader` - https://webpack.js.org/loaders/style-loader/
+-   `css-loader` - https://www.npmjs.com/package/css-loader
+-   `sass-loader` - https://www.npmjs.com/package/sass-loader - requires installation of `sass` as well
+
+### Using Vite
+
+Vite does support both CSS and SCSS/SASS out of the box. You would only need to install `sass`.
+
+```shell
+npm i -D sass
+```
+
+## How to develop the components
+
+You will first need to install dev dependencies by
+```shell
+cd ./react
+npm ci
+```
+
+You can then manually build an components using
+```shell
+npx nx build <package-name>
+```
+where package names is either `group-tree`, `subsurface-viewer`, `well-completions` or `well-log-viewer`.
+
+Alternatively you can build all by running e.g.
+```shell
+npx nx run-many -t build
+```
---
+++ typescript/packages/well-completions/src/components/WellCompletionComponent.tsx
@@ -0,0 +1,59 @@
+import Ajv from "ajv";
+import React, { useMemo } from "react";
+import semver from "semver";
+import { Data } from "../redux/types";
+import ErrorPlaceholder from "./Common/ErrorPlaceholder";
+import DataProvider from "./DataLoader";
+import WellCompletionsViewer from "./WellCompletionsViewer";
+
+import inputSchema from "../inputSchema/wellCompletions.json";
+
+const ajv = new Ajv();
+const minVersion = "1.0.0";
+interface Props {
+    id: string;
+    data: Data;
+}
+/**
+ * Well completions component
+ */
+const WellCompletionComponent: React.FC<Props> = React.memo(
+    ({ id, data }: Props) => {
+        const validate = useMemo(() => ajv.compile(inputSchema), []);
+        //check against the json schema
+        const isSchemaValid = useMemo(() => validate(data), [data, validate]);
+        const isVersionDefined = useMemo(
+            () =>
+                data.version !== undefined &&
+                semver.valid(data.version) !== null,
+            [data.version]
+        );
+        const isVersionValid = useMemo(
+            () =>
+                isVersionDefined &&
+                semver.satisfies(data.version, `>=${minVersion}`),
+            [data, isVersionDefined]
+        );
+        if (!isVersionValid)
+            return (
+                <ErrorPlaceholder
+                    text={
+                        !isVersionDefined
+                            ? `${data.version} is not a valid version`
+                            : `${data.version} is lower than the minimum support version 1.0.0`
+                    }
+                />
+            );
+        //If input data does not satisfy the schema
+        if (!isSchemaValid)
+            return <ErrorPlaceholder text={JSON.stringify(validate.errors)} />;
+        return (
+            <DataProvider id={id} data={data}>
+                <WellCompletionsViewer />
+            </DataProvider>
+        );
+    }
+);
+
+WellCompletionComponent.displayName = "WellCompletionComponent";
+export default WellCompletionComponent;
