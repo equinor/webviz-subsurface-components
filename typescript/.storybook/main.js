657fe39c22a3f0381dfaa9e33ae6f46182dc5e41
Havard Bjerke <havard.bjerke@emerson.com>
2023-06-26 09:44:42 +0200

    Upgrade storybook to latest.

--- typescript/.storybook/main.js
+++ typescript/.storybook/main.js
@@ -1,54 +1,46 @@
 module.exports = {
-    stories: [
-        "../packages/*/src/**/*.stories.mdx",
-        "../packages/*/src/**/*.stories.@(js|jsx|ts|tsx)",
-    ],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-actions",
-		{
-			name: "@storybook/addon-docs",
-			  options: {
-				sourceLoaderOptions: {
-				  injectStoryParameters: false,
-				},
-			  },
-		},
-        "@storybook/addon-storysource",
-    ],
-    core: {
-        builder: "webpack5",
-    },
-    webpackFinal: (config) => {
-        config.module.rules.push(
-            {
-                test: /\.scss$/,
-                use: ["vue-style-loader", "css-loader", "sass-loader"],
-            },
-            {
-                test: /\.(fs|vs).glsl$/i,
-                use: ["raw-loader"],
-            }
-        );
-        return {
-            ...config,
-            resolve: {
-                ...config.resolve,
-                fallback: {
-                    ...config.fallback,
-                    fs: false,
-                    tls: false,
-                    net: false,
-                    path: false,
-                    zlib: false,
-                    http: false,
-                    https: false,
-                    crypto: false,
-                    stream: false,
-                },
-            },
-        };
-    },
-    staticDirs: ["../../example-data"],
+  stories: ["../packages/*/src/**/*.stories.mdx", "../packages/*/src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-actions", {
+    name: "@storybook/addon-docs",
+    options: {
+      sourceLoaderOptions: {
+        injectStoryParameters: false
+      }
+    }
+  }, "@storybook/addon-storysource", "@storybook/addon-mdx-gfm"],
+  webpackFinal: config => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["vue-style-loader", "css-loader", "sass-loader"]
+    }, {
+      test: /\.(fs|vs).glsl$/i,
+      use: ["raw-loader"]
+    });
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        fallback: {
+          ...config.fallback,
+          fs: false,
+          tls: false,
+          net: false,
+          path: false,
+          zlib: false,
+          http: false,
+          https: false,
+          crypto: false,
+          stream: false
+        }
+      }
+    };
+  },
+  staticDirs: ["../../example-data"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
