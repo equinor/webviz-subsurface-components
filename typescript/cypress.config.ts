92280b3577e7e75ac4d398b2a162465173fa8e64
Havard Bjerke <havard.bjerke@aspentech.com>
2023-06-27 10:36:53 +0200

    Fixed type conditions in cypress config.

--- typescript/cypress.config.ts
+++ typescript/cypress.config.ts
@@ -2,26 +2,32 @@ import { defineConfig } from "cypress";
 import getCompareSnapshotsPlugin from "cypress-image-diff-js/dist/plugin";
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+    component: {
+        devServer: {
+            framework: "create-react-app",
+            bundler: "webpack",
+        },
+        video: false,
+        setupNodeEvents(on, config) {
+            getCompareSnapshotsPlugin(on, config),
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                on("before:browser:launch", (browser = {}, launchOptions) => {
+                    if (
+                        browser.family === "chromium" &&
+                        browser.name !== "electron"
+                    ) {
+                        launchOptions.args.push("--start-fullscreen");
+
+                        return launchOptions;
+                    }
+                    if (browser.name === "electron") {
+                        launchOptions.preferences["fullscreen"] = true;
+
+                        return launchOptions;
+                    }
+                    return null;
+                });
+        },
     },
-    video:false,
-    setupNodeEvents(on, config) {
-      getCompareSnapshotsPlugin(on, config),
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        if (browser.family === 'chromium' && browser.name !== 'electron') {
-          launchOptions.args.push('--start-fullscreen')
-      
-          return launchOptions
-        }
-        if (browser.name === 'electron') {
-          launchOptions.preferences.fullscreen = true
-      
-          return launchOptions
-        }
-      })
-    }
-  },
 });
