8f5eacbf58d981fac900535b97a61757c0e5998a
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2023-09-28 16:05:25 +0200

    fix: fix linting issues in DashSubsurfaceViewer

--- typescript/packages/subsurface-viewer/src/DashSubsurfaceViewer.tsx
+++ typescript/packages/subsurface-viewer/src/DashSubsurfaceViewer.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import SubsurfaceViewer, { SubsurfaceViewerProps } from "./SubsurfaceViewer";
+import type { SubsurfaceViewerProps } from "./SubsurfaceViewer";
+import SubsurfaceViewer from "./SubsurfaceViewer";
 import { View } from "@deck.gl/core/typed";
 
 function mapAnnotation(annotationContainers: React.ReactNode) {
     return React.Children.map(annotationContainers, (annotationContainer) => {
         const viewId = (annotationContainer as React.ReactElement).key;
         return (
-            // @ts-expect-error
+            // @ts-expect-error This is proven to work in JavaScript
             <View key={viewId} id={viewId}>
                 {annotationContainer}
             </View>
