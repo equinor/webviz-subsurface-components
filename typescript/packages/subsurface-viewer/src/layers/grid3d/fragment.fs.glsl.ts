108b6601b049eb1cc5ed27fc19f25e61c214d09f
nilscb <nils.christian.bonnevie@gmail.com>
2023-10-23 16:41:07 +0200

    fix: Grid3DLayer should apply flag "ZIncreasingDownwards" for correct readout of depth. (#1729)
    * Fix of:
    - Grid3DLayer should apply flag "ZIncreasingDownwards" for correct readout of depth. #1723
    
    * Partial fix of:
    - Performance: Apply flag "ZIncreasingDownwards" in vertex shaders for relevant layers. #1724

--- typescript/packages/subsurface-viewer/src/layers/grid3d/fragment.fs.glsl.ts
+++ typescript/packages/subsurface-viewer/src/layers/grid3d/fragment.fs.glsl.ts
@@ -20,33 +20,11 @@ uniform vec3 colorMapClampColor;
 uniform bool isClampColor;
 uniform bool isColorMapClampColorTransparent;
 
-vec4 encodeVertexIndexToRGB () {
-
-   float r = 0.0;
-   float g = 0.0;
-   float b = 0.0;
-
-   int idx = vertexIndex;
-
-   if (idx >= (256 * 256) - 1) {
-      r = floor(float(idx) / (256.0 * 256.0));
-      idx -= int(r * (256.0 * 256.0));
-   }
-
-   if (idx >= 256 - 1) {
-      g = floor(float(idx) / 256.0);
-      idx -= int(g * 256.0);
-   }
-
-   b = float(idx);
-
-   return vec4(r / 255.0, g / 255.0, b / 255.0, 1.0);   
-}
 
 void main(void) {
 
    if (picking_uActive && !picking_uAttribute) {
-      gl_FragColor = encodeVertexIndexToRGB ();      
+      gl_FragColor = encodeVertexIndexToRGB(vertexIndex);      
       return;
    }
 
