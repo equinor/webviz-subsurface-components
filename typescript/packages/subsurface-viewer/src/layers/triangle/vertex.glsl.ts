b2248f10062384646c613e1626b93d587f68e672
Nils Christian Bonnevie <nils.christian.bonnevie@gmail.com>
2023-10-25 14:27:08 +0200

    performance:  Apply flag "ZIncreasingDownwards" in vertex shaders for relevant layers. #1724

--- typescript/packages/subsurface-viewer/src/layers/triangle/vertex.glsl.ts
+++ typescript/packages/subsurface-viewer/src/layers/triangle/vertex.glsl.ts
@@ -10,25 +10,37 @@ in float properties;
 in vec3 normals;
 
 // Outputs to fragment shader
-//out vec2 vTexCoord;
 out vec3 cameraPosition;
 out vec3 normals_commonspace;
 out vec4 position_commonspace;
 out vec3 worldPos;
 out float property;
 
+uniform bool ZIncreasingDownwards;
+
+const vec3 pickingColor = vec3(1.0, 1.0, 0.0);
 
 void main(void) {
+   geometry.pickingColor = pickingColor;
+
    cameraPosition = project_uCameraPosition;
 
-   worldPos = positions;
+   vec3 position = positions;
+   position[2] *= ZIncreasingDownwards ? -1.0 : 1.0;
+
+   worldPos = position;
 
    normals_commonspace = normals;
 
    property = properties;
 
-   position_commonspace = vec4(project_position(positions), 0.0);
+   position_commonspace = vec4(project_position(position), 0.0);
    gl_Position = project_common_position_to_clipspace(position_commonspace);
+
+   DECKGL_FILTER_GL_POSITION(gl_Position, geometry);
+
+   vec4 color = vec4(0.0);
+   DECKGL_FILTER_COLOR(color, geometry);
 }
 `;
 
