7f7b0ecccfb2c759cbde529cfd75dd7457c85a36
leonid.polukhin <leonid.polukhin@aspentech.com>
2023-12-05 15:37:14 +0300

    tests(wellMarkerLayer): stories updated, random seed added

--- typescript/packages/subsurface-viewer/src/layers/well_markers/wellMarkersLayer.stories.tsx
+++ typescript/packages/subsurface-viewer/src/layers/well_markers/wellMarkersLayer.stories.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
+import { create, all } from "mathjs";
 
 import type { StoryFn, Meta } from "@storybook/react";
 import SubsurfaceViewer from "../../SubsurfaceViewer";
@@ -8,7 +9,7 @@ import type { WellMarkerDataT } from "./wellMarkersLayer"
 
 export default {
     component: SubsurfaceViewer,
-    title: "SubsurfaceViewer/WellMarkers",
+    title: "SubsurfaceViewer/Well Markers Layer",
 } as Meta<typeof SubsurfaceViewer>;
 
 const Template: StoryFn<typeof SubsurfaceViewer> = (args) => (
@@ -18,59 +19,26 @@ const Template: StoryFn<typeof SubsurfaceViewer> = (args) => (
 const parameters = {
     docs: {
         description: {
-            story: "Simgrid.",
+            story: "Well Markers Layer.",
         },
         inlineStories: false,
         iframeHeight: 500,
     },
 };
 
-export const WellMarkers = Template.bind({});
+const math = create(all, { randomSeed: "1984" });
 
-WellMarkers.args = {
-    bounds: [-25, -25, 50, 30],
-    views: {
-        layout: [1, 1] as [number, number],
-        viewports: [
-            {
-                id: "view_1",
-                show3D: true,
-            },
-        ],
-    },
-    id: "well-markers-tttttt",
-    layers: [
-        {
-            "@@type": "AxesLayer",
-            id: "well-markers-axes",
-            bounds: [-15, -15, -15, 40, 20, 15],
-            ZIncreasingDownwards: true,            
-        },
-        {
-            "@@type": "NorthArrow3DLayer",
-            id: "north-arrow-layer",
-        },
-        {
-            "@@type": "WellMarkersLayer",
-            id: "well-markers-1",
-            pickable: true,
-            shape: "triangle",
-            sizeUnits: "pixels",
-            ZIncreasingDownwards: true,
-            data: [
-                {position: [0.0, 0.0, 0.0], azimuth: 0, inclination: 0,  color: [0, 0, 123], size: 10},
-                {position: [0.0, 0.0, 0.0], azimuth: 0, inclination: 45, color: [200, 100], size: 20},
-                {position: [0.0, 0.0, 0.0], azimuth: 0, inclination: 90, color: [100], size: 30},
+type TRandomNumberFunc = (max: number) => number;
 
-                {position: [5.0, 0.0, 5.0001], azimuth: 0,  inclination: 0, color: [100, 0, 0, 100], size: 5},
-                {position: [5.0, 0.0, 5.0002], azimuth: 30, inclination: 0, color: [100, 100, 0, 100], size: 15},
-                {position: [5.0, 0.0, 5.0003], azimuth: 60, inclination: 0, color: [100, 0, 100, 100], size: 10.5}
-            ]            
-        },
-
-    ],
-};
-WellMarkers.parameters = parameters;
+const randomFunc = ((): TRandomNumberFunc => {
+    if (math?.random) {
+        return (max: number) => {
+            const val = math.random?.(max);
+            return val ? val : 0.0;
+        };
+    }
+    return (max: number) => Math.random() * max;
+})();
 
 const generateMarkers = () : WellMarkerDataT[] => {
 
@@ -94,7 +62,7 @@ const generateMarkers = () : WellMarkerDataT[] => {
                 position: [x, y, z],
                 azimuth: az * 180.0 / Math.PI,
                 inclination: Math.asin (Math.cos(incl)) * 180.0 / Math.PI,
-                color: [255 * Math.random (), 255 * Math.random (), 255 * Math.random (), 100],
+                color: [randomFunc (255), randomFunc(255), randomFunc (255), 100],
                 outlineColor: [0, 0, 100, 255],
                 size: 0.02 * Math.sqrt (x * x + y * y)
             })
@@ -103,9 +71,9 @@ const generateMarkers = () : WellMarkerDataT[] => {
     return res;
 }
 
-export const WellMarkers2 = Template.bind({});
+export const WellMarkers = Template.bind({});
 
-WellMarkers2.args = {
+WellMarkers.args = {
     bounds: [-25, -25, 50, 30],
     views: {
         layout: [1, 1] as [number, number],
@@ -139,5 +107,5 @@ WellMarkers2.args = {
 
     ],
 };
-WellMarkers2.parameters = parameters;
+WellMarkers.parameters = parameters;
 
