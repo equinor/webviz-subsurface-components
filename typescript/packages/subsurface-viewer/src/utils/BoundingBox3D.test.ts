9ac4cce63435b73d7a3b0c4d8a3081d963b30425
Christophe Winkler <christophe.winkler@aspentech.com>
2023-12-08 09:06:50 +0100

    Refactor Map component to remove numerous useEffect
    Main motivation is to fix wrong view state computed when first rendering is done with hidden flag.
    First reproduced by application with tabbed layout where all the tabs are rendered using hidden flag.
    This use case has been added to the storybook to demonstrate the issue, along with a 'triggerHome' control (in a previous PR).
    
    The current PR brings:
    - onResize() function to handle resizing
    - use a React reducer to handle Z scaling
      Add support for PageUp/PageDown and Shift modifier
    - use a React reducer to compute the global 3D bounding box from the reportBoundingBox callback of the layers
    - introduce a ViewController class to generate the DeckGL views and viewState
      This brings all the computations in one place/one step by providing state change allowing to fine-tune the computations
    - use bounding box from the 'cameraPosition' (if set as zoom field) instead of the data bounding box
    
    Fixes:
    - initial viewState is correct, even when rendered with 'hidden' flag set to true
    - triggerHome now respects current zScale
    
    In the end, the final implementation drops down to 6 React useEffect,  from the 16 initial ones !

---
+++ typescript/packages/subsurface-viewer/src/utils/BoundingBox3D.test.ts
@@ -0,0 +1,37 @@
+import "jest";
+
+import type { BoundingBox3D } from "./BoundingBox3D";
+import { boxCenter, boxUnion } from "./BoundingBox3D";
+
+describe("Test BoundingBox3D", () => {
+    it("boxUnion default box", () => {
+        const unitBox: BoundingBox3D = [0, 0, 0, 1, 1, 1];
+        const defaultBox: BoundingBox3D = [-1, -1, -1, 1, 1, 1];
+        const box1: BoundingBox3D = [0, 1, 2, 5, 6, 7];
+        const box2: BoundingBox3D = [1, 2, 3, 4, 5, 6];
+        expect(boxUnion(undefined, undefined)).toEqual(unitBox);
+        expect(boxUnion(undefined, undefined, defaultBox)).toEqual(defaultBox);
+        expect(boxUnion(box1, undefined, defaultBox)).toEqual(box1);
+        expect(boxUnion(undefined, box2, defaultBox)).toEqual(box2);
+        expect(boxUnion(box1, box2, defaultBox)).toEqual(box1);
+    });
+
+    it("boxUnion without default box", () => {
+        const box1: BoundingBox3D = [0, 1, 2, 5, 6, 7];
+        const box2: BoundingBox3D = [1, 2, 3, 4, 5, 6];
+        const box3: BoundingBox3D = [1, 2, 3, 6, 7, 8];
+        expect(boxUnion(box1, undefined)).toEqual(box1);
+        expect(boxUnion(undefined, box2)).toEqual(box2);
+        expect(boxUnion(box1, box2)).toEqual(box1);
+        expect(boxUnion(box1, box3)).toEqual([0, 1, 2, 6, 7, 8]);
+    });
+
+    it("boxCenter", () => {
+        const box1: BoundingBox3D = [0, 1, 2, 5, 6, 7];
+        const box2: BoundingBox3D = [1, 2, 3, 4, 5, 6];
+        const box3: BoundingBox3D = [1, 2, 3, 6, 7, 8];
+        expect(boxCenter(box1)).toEqual([2.5, 3.5, 4.5]);
+        expect(boxCenter(box2)).toEqual([2.5, 3.5, 4.5]);
+        expect(boxCenter(box3)).toEqual([3.5, 4.5, 5.5]);
+    });
+});
