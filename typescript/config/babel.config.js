7c76c2215d9285c889c26c8493b5fd0cd1a2b89b
Håvard Bjerke <havard.bjerke@aspentech.com>
2023-12-14 15:38:01 +0100

    build(deps-dev): upgrade to latest Storybook version (#1831)

--- typescript/config/babel.config.js
+++ typescript/config/babel.config.js
@@ -11,9 +11,4 @@ module.exports = {
         "@babel/preset-react",
         "@babel/preset-typescript"
     ],
-    "plugins": [
-        ["@babel/plugin-proposal-class-properties", { "loose": true }],
-        ["@babel/plugin-proposal-private-methods", { "loose": true }],
-        ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
-    ]
 };
