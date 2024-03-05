8248e2f5770f532b63b35f59b9bb5abeb38ce098
leonid.polukhin <leonid.polukhin@aspentech.com>
2024-03-05 10:24:21 +0300

    refactor(polylines): Private PolylinesLayer extends deck.gl PathLayer

---
+++ typescript/packages/subsurface-viewer/src/layers/polylines/privatePolylinesLayer.ts
@@ -0,0 +1,53 @@
+import { PathLayer, PathLayerProps } from "@deck.gl/layers/typed";
+
+import type { NumericArray } from "@math.gl/types";
+import GL from "@luma.gl/constants";
+
+import type { LayerContext } from "@deck.gl/core/typed";
+import { project32, picking } from "@deck.gl/core/typed";
+
+import vs from "./path-layer-vertex.glsl";
+import fs from "./path-layer-fragment.glsl";
+
+type UniformValue = number | boolean | Readonly<NumericArray>;
+
+export interface ExtendedPathLayerProps {
+    depthTest: boolean;
+    ZIncreasingDownwards: boolean;
+}
+
+export class PrivatePolylinesLayer extends PathLayer<
+    unknown,
+    ExtendedPathLayerProps
+> {
+    constructor(props: PathLayerProps) {
+        super(props);
+    }
+
+    getShaders() {
+        return { vs, fs, modules: [project32, picking] };
+    }
+
+    draw(args: {
+        moduleParameters?: unknown;
+        uniforms: Record<string, UniformValue>;
+        context: LayerContext;
+    }): void {
+        args.uniforms["ZIncreasingDownwards"] = this.props.ZIncreasingDownwards;
+
+        let restoreDepthTest = false;
+        if (
+            typeof this.props.depthTest === "boolean" &&
+            !this.props.depthTest
+        ) {
+            restoreDepthTest = true;
+            this.context.gl.disable(GL.DEPTH_TEST);
+        }
+        super.draw({ uniforms: args.uniforms });
+        if (restoreDepthTest) {
+            this.context.gl.enable(GL.DEPTH_TEST);
+        }
+    }
+}
+
+PrivatePolylinesLayer.layerName = "PrivatePolylinesLayer";
