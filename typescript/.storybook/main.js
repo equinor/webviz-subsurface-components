7b58b1c8b708ea42748d45ffd296a2d962536634
Christophe Winkler <christophe.winkler@aspentech.com>
2024-03-26 15:10:49 +0100

    Automatic migration to storybook 8

--- typescript/.storybook/main.js
+++ typescript/.storybook/main.js
@@ -1,15 +1,17 @@
+import { dirname, join } from "path";
 module.exports = {
     stories: [
-        "../packages/*/src/**/*.stories.mdx",
-        "../packages/*/src/**/*.stories.@(js|jsx|ts|tsx)",
+        "../packages/*/src/**/*.mdx",
+        "../packages/*/src/**/*.stories.@(js|jsx|ts|tsx)"
     ],
     addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-actions",
-        "@storybook/addon-docs",
-        "@storybook/addon-storysource",
-        "@storybook/addon-mdx-gfm",
+        getAbsolutePath("@storybook/addon-links"),
+        getAbsolutePath("@storybook/addon-essentials"),
+        getAbsolutePath("@storybook/addon-actions"),
+        getAbsolutePath("@storybook/addon-docs"),
+        getAbsolutePath("@storybook/addon-storysource"),
+        getAbsolutePath("@storybook/addon-mdx-gfm"),
+        "@storybook/addon-webpack5-compiler-babel"
     ],
     webpackFinal: (config) => {
         config.module.rules.push(
@@ -70,10 +72,14 @@ module.exports = {
     },
     staticDirs: ["../../example-data"],
     framework: {
-        name: "@storybook/react-webpack5",
+        name: getAbsolutePath("@storybook/react-webpack5"),
         options: {},
     },
     docs: {
         autodocs: true,
     },
 };
+
+function getAbsolutePath(value) {
+    return dirname(require.resolve(join(value, "package.json")));
+}
