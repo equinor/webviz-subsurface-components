fc4e196d5897983cbbb7dbdfe21f9279f933015f
LeonidPolukhin <117165489+LeonidPolukhin@users.noreply.github.com>
2024-04-24 20:52:41 +0300

    feat(grid3d): Discrete property value in grid readouts (#2032)
    1) Grid3dLayer API takes an array of discrete property value-name pairs.
    2) The array is used to display both discrete property names and values
    in readouts.
    3) A grid story updated.
    4) minimal npm version is bumped a little because of eslint-plugin
    requirements.
    
    ---------
    
    Co-authored-by: leonid.polukhin <leonid.polukhin@aspentech.com>

--- typescript/packages/subsurface-viewer/src/layers/grid3d/privateGrid3dLayer.ts
+++ typescript/packages/subsurface-viewer/src/layers/grid3d/privateGrid3dLayer.ts
@@ -23,6 +23,7 @@ import vsLineShader from "./vertex_lines.glsl";
 import fsLineShader from "./fragment_lines.glsl";
 import { localPhongLighting, utilities } from "../shader_modules";
 import { TGrid3DColoringMode } from "./grid3dLayer";
+import type { IDiscretePropertyValueName } from "./grid3dLayer";
 
 const DEFAULT_TEXTURE_PARAMETERS = {
     [GL.TEXTURE_MIN_FILTER]: GL.LINEAR,
@@ -77,7 +78,7 @@ export interface PrivateLayerProps extends ExtendedLayerProps {
     coloringMode: TGrid3DColoringMode.Property;
     gridLines: boolean;
     propertyValueRange: [number, number];
-    discretePropertyNames?: string[];
+    discretePropertyValueNames?: IDiscretePropertyValueName[];
     depthTest: boolean;
     ZIncreasingDownwards: boolean;
 }
@@ -258,12 +259,26 @@ export default class PrivateLayer extends Layer<PrivateLayerProps> {
         }
 
         const properties = this.props.mesh.attributes.properties.value;
-        const propertyValue = properties[vertexIndex];
-        if (Number.isFinite(propertyValue)) {
-            const propertyText =
-                this.props.discretePropertyNames?.[propertyValue] ??
-                propertyValue;
-            layer_properties.push(createPropertyData("Property", propertyText));
+        const propertyIndex = properties[vertexIndex];
+        if (Number.isFinite(propertyIndex)) {
+            if (this.props.discretePropertyValueNames) {
+                const propertyText =
+                    this.props.discretePropertyValueNames[propertyIndex].name ??
+                    propertyIndex;
+                const propertyValue =
+                    this.props.discretePropertyValueNames[propertyIndex]
+                        .value ?? propertyIndex;
+                layer_properties.push(
+                    createPropertyData("Property", propertyText)
+                );
+                layer_properties.push(
+                    createPropertyData("Value", propertyValue)
+                );
+            } else {
+                layer_properties.push(
+                    createPropertyData("Property", propertyIndex)
+                );
+            }
         }
 
         return {
