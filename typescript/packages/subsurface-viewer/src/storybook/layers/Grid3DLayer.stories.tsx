7ea9bf7774c727db71884aad96847cfe44249398
leonid.polukhin <leonid.polukhin@aspentech.com>
2024-06-07 10:06:48 +0300

    tests(grid3d): material added

--- typescript/packages/subsurface-viewer/src/storybook/layers/Grid3DLayer.stories.tsx
+++ typescript/packages/subsurface-viewer/src/storybook/layers/Grid3DLayer.stories.tsx
@@ -334,8 +334,13 @@ export const DiscretePropertyWithClamping: StoryObj<typeof SubsurfaceViewer> = {
                 ZIncreasingDownwards: true,
                 colorMapFunction:
                     layerArrays[discretePropsLayerId].colorMapFunction,
-                material: false,
-                colorMapRange: [3, 8],
+                material: {
+                    ambient: 0.5,
+                    diffuse: 0.5,
+                    shininess: 32,
+                    specularColor: [255, 255, 255],
+                },
+                colorMapRange: [0, 20],
                 colorMapClampColor: [100, 100, 100],
             },
         ],
