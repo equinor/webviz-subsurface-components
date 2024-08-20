187bc94b079e361493352de05a8b53ddfab20ac9
Miri Sachs <miri.sachs@aspentech.com>
2024-08-20 08:19:20 +0300

    add a story and handle style copy

--- typescript/packages/well-log-viewer/src/utils/deepcopy.ts
+++ typescript/packages/well-log-viewer/src/utils/deepcopy.ts
@@ -1,3 +1,26 @@
+import type { Template } from "../components/WellLogTemplateTypes";
+
 export function deepCopy<T>(source: T): T {
     return JSON.parse(JSON.stringify(source)) as T;
+}
+
+export function deepCopyTemplate(source: Template): Template {
+    const target = deepCopy(source);
+
+    source?.styles?.forEach((style, index) => {
+        const target_style = target.styles?.at(index);
+        if (target_style) {
+            if (style.colorTable && typeof style.colorTable == "function") {
+                target_style.colorTable = style.colorTable;
+            }
+            if (
+                style.inverseColorTable &&
+                typeof style.inverseColorTable == "function"
+            ) {
+                target_style.inverseColorTable = style.inverseColorTable;
+            }
+        }
+    });
+
+    return target;
 }
