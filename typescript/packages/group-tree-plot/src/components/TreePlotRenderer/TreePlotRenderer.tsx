a7cfab2e0789790d932feee9d6cdbc5e45791226
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2025-02-03 08:16:05 +0100

    feat: GroupTreePlot --- Refactor to typescript + better D3 and React integration + some small features (#2367)
    Resolves #1829 
    
    * `refactor`: Fully rewrites the old D3-only `GroupTreePlot` component
    to more seemlessly use both D3 and React to build the graph
    * In essence; D3 is now purely used to calculate hierarchies and node
    positions, whilst React handles the rendering of said nodes
    * D3 is also used to animate transitions between graph states (assisted
    by React to trigger the animation changes)
    * ~`dependency`: Installed the `react-transition-group` package to
    facilitate leave-enter transitions for graph elements~
    * `dependency`: Installed the [`motion` library](https://motion.dev/) to
    facilitate leave-enter transitions for graph elements
    * `refactor`: `groupTreeAssembler` has been replaced; it has been split
    it into by react components (to handle svg rendering), and the new
    `DataAssembler` class, to handle the data itself. Rewritten in proper
    Typescript
    * _Note, the old assembler would display dummy data if an empty data
    tree list was fed to the constructor. The new version is more picky, and
    will throw an error if this is done. The Core component will instead
    display an error showing that the data is invalid, to make it more clear
    that something is wrong_
    * `fix`: The rendered tree is now more centered in the SVG
    * `fix`: The tree will now fill the entire width of the SVG
    * `feature/fix`: Child nodes can now be minimized and expanded, as
    intended
    * `feature`: Added `initialVisibleDepth`; renders the tree with all
    nodes from the given depth collapsed
    * `feature`: The plot is now fully resizable.
    * An example of resizing is show in the "Resizable" Story
    (`/?path=/story/grouptreeplot-demo--resizable`)
    
    ### Known issue (slight animation regression)
    Because each component initializes their own enter/leave animations in
    their enter/leave-callbakcs, the D3 animations get slightly
    de-synchronized, so new/old tree edges get slightly "detached" from the
    tree when it grows shrinks (only during the animation). The d3 only
    version did not have this issue (as it set up all animations in one
    single update), so it's a regression.
    
    However, this is only noticable in slower animations, and with a 200ms
    transition duration, it's more or less impossible to notice, so I'm
    leaving it unresolved for now

---
+++ typescript/packages/group-tree-plot/src/components/TreePlotRenderer/TreePlotRenderer.tsx
@@ -0,0 +1,146 @@
+import React from "react";
+import type { ReactNode } from "react";
+import _ from "lodash";
+import * as d3 from "d3";
+
+import { AnimatePresence } from "motion/react";
+
+import type { D3TreeNode, RecursiveTreeNode } from "../../types";
+import type { DataAssembler } from "../../utils/DataAssembler";
+
+import { makeLinkId, makeNodeId } from "../../utils/treePlot";
+import { TransitionTreeEdge } from "./privateComponents/TransitionTreeEdge";
+import { TransitionTreeNode } from "./privateComponents/TransitionTreeNode";
+
+export type TreePlotRendererProps = {
+    dataAssembler: DataAssembler;
+    primaryEdgeProperty: string;
+    primaryNodeProperty: string;
+    height: number;
+    width: number;
+
+    initialVisibleDepth?: number;
+};
+
+const PLOT_MARGINS = {
+    top: 10,
+    right: 120,
+    bottom: 10,
+    left: 70,
+};
+
+export function TreePlotRenderer(props: TreePlotRendererProps): ReactNode {
+    const activeTree = props.dataAssembler.getActiveTree();
+    const rootTreeNode = activeTree.tree;
+
+    const [nodeCollapseFlags, setNodeCollapseFlags] = React.useState<
+        Record<string, boolean>
+    >({});
+
+    const heightPadding = PLOT_MARGINS.top + PLOT_MARGINS.bottom;
+    const widthPadding = PLOT_MARGINS.left + PLOT_MARGINS.right;
+    const layoutHeight = props.height - heightPadding;
+    const layoutWidth = props.width - widthPadding;
+
+    const treeLayout = React.useMemo(
+        function computeLayout() {
+            // Note that we invert height / width to render the tree sideways
+            return d3
+                .tree<RecursiveTreeNode>()
+                .size([layoutHeight, layoutWidth]);
+        },
+        [layoutHeight, layoutWidth]
+    );
+
+    const nodeTree = React.useMemo(
+        function computeTree() {
+            const hierarchy = d3.hierarchy(rootTreeNode).each((node) => {
+                // Collapse nodes based on collapse flags and minimum depth
+                // ! I'd argue that it'd be more correct to collapse nodes using the hierarchy constructor:
+                //      d3.hierarchy(rootTreeNode, (datum) => {
+                //        if(someCheck) return null
+                //        else return datum.children
+                //      })
+                // However, nodes are being collapsed after-the-fact here, since we need the depth value for implicit collapses, and it's not available in the constructor
+
+                const collapseFlag = nodeCollapseFlags[makeNodeId(node)];
+                const visibleDepth =
+                    props.initialVisibleDepth ?? Number.MAX_SAFE_INTEGER;
+
+                // Return only if collapse flag is *explicitly* set to false
+                if (collapseFlag === false) return;
+                if (node.depth >= visibleDepth || collapseFlag === true) {
+                    node.children = undefined;
+                }
+            });
+
+            return treeLayout(hierarchy);
+        },
+        [treeLayout, rootTreeNode, nodeCollapseFlags, props.initialVisibleDepth]
+    );
+
+    // Storing the previous value so entering nodes know where to expand from
+    const oldNodeTree = usePreviousTree(nodeTree);
+
+    function toggleNodeCollapse(node: D3TreeNode) {
+        const nodeIdent = makeNodeId(node);
+        // Might be collapsed implicitly due to visibleDepth prop
+        const collapsed = Boolean(node.children?.length);
+
+        setNodeCollapseFlags((prev) => {
+            const existingVal = prev[nodeIdent];
+
+            const newVal = collapsed && !existingVal;
+            const newFlags = { ...prev, [nodeIdent]: newVal };
+
+            // When closing a node, reset any stored flag for all children
+            if (newVal) {
+                node.descendants()
+                    // descendants() includes this node, slice to skip it
+                    .slice(1)
+                    .forEach((child) => delete newFlags[makeNodeId(child)]);
+            }
+
+            return newFlags;
+        });
+    }
+
+    return (
+        <g transform={`translate(${PLOT_MARGINS.left},${PLOT_MARGINS.top})`}>
+            <AnimatePresence custom={nodeTree}>
+                {nodeTree.links().map((link) => (
+                    <TransitionTreeEdge
+                        key={makeLinkId(link)}
+                        link={link}
+                        dataAssembler={props.dataAssembler}
+                        primaryEdgeProperty={props.primaryEdgeProperty}
+                        oldNodeTree={oldNodeTree}
+                    />
+                ))}
+                {nodeTree.descendants().map((node) => (
+                    <TransitionTreeNode
+                        key={makeNodeId(node)}
+                        primaryNodeProperty={props.primaryNodeProperty}
+                        dataAssembler={props.dataAssembler}
+                        node={node}
+                        oldNodeTree={oldNodeTree}
+                        onNodeClick={toggleNodeCollapse}
+                    />
+                ))}
+            </AnimatePresence>
+        </g>
+    );
+}
+
+function usePreviousTree(treeRootNode: D3TreeNode): D3TreeNode {
+    // Make the first "old" tree be just the root, so everything expands from the root
+    const initRoot: D3TreeNode = _.clone(treeRootNode);
+    initRoot.children = undefined;
+
+    const oldNodeTree = React.useRef<D3TreeNode>(initRoot);
+    React.useEffect(() => {
+        oldNodeTree.current = treeRootNode;
+    }, [treeRootNode]);
+
+    return oldNodeTree.current;
+}
---
+++ typescript/packages/group-tree-plot/src/utils/DataAssembler.ts
@@ -0,0 +1,246 @@
+import * as d3 from "d3";
+
+import type {
+    DatedTree,
+    EdgeData,
+    EdgeMetadata,
+    NodeData,
+    NodeMetadata,
+} from "../types";
+import _ from "lodash";
+import { printTreeValue } from "./treePlot";
+import React from "react";
+
+/**
+ * Utility class to assemble and combine data for one or more dated flow network trees.
+ * The instance allows setting an active date, and provides methods to get data and labels pertaining to the given date
+ */
+export class DataAssembler {
+    datedTrees: DatedTree[];
+    edgeMetadataList: EdgeMetadata[];
+    nodeMetadataList: NodeMetadata[];
+
+    private _propertyToLabelMap: Map<string, [label?: string, unit?: string]>;
+    private _propertyMaxVals: Map<string, number>;
+
+    private _currentTreeIndex = 0;
+    private _currentDateIndex = 0;
+
+    /**
+     * @param datedTrees A list of dated tree objects. List cannot be empty
+     * @param edgeMetadataList A list of labels and units to use when representing values from tree edges
+     * @param nodeMetadataList A list of labels and units to use when representing values from tree nodes
+     * @throws Throws if dated tree list is empty
+     */
+    constructor(
+        datedTrees: DatedTree[],
+        edgeMetadataList: EdgeMetadata[],
+        nodeMetadataList: NodeMetadata[]
+    ) {
+        // Guard - Require at least one tree to be given.
+        if (!datedTrees.length) throw new Error("Tree-list is empty");
+
+        this.datedTrees = datedTrees;
+
+        this.edgeMetadataList = edgeMetadataList;
+        this.nodeMetadataList = nodeMetadataList;
+
+        this._propertyToLabelMap = new Map();
+        [...edgeMetadataList, ...nodeMetadataList].forEach((elm) => {
+            this._propertyToLabelMap.set(elm.key, [elm.label, elm.unit]);
+        });
+
+        this._propertyMaxVals = new Map();
+        this.datedTrees.forEach(({ tree }) => {
+            // Utilizing d3 to iterate over each child node from our tree-root
+            d3.hierarchy(tree, (d) => d.children).each((node) => {
+                const { edge_data } = node.data;
+
+                Object.entries(edge_data).forEach(([key, vals]) => {
+                    const existingMax =
+                        this._propertyMaxVals.get(key) ?? Number.MIN_VALUE;
+
+                    const newMax = Math.max(...vals, existingMax);
+
+                    this._propertyMaxVals.set(key, newMax);
+                });
+            });
+        });
+    }
+
+    /**
+     * Changes the active date for the instance. Raises if date is invalid
+     * @param newDate A date string, formatted as YYYY-MM-DD (eg. 2001-01-30)
+     * @throws Will throw if the date is invalid, or if no tree uses the given date
+     */
+    setActiveDate(newDate: string) {
+        const [newTreeIdx, newDateIdx] = findTreeAndDateIndex(
+            newDate,
+            this.datedTrees
+        );
+
+        // I do think these will always both be -1, or not -1, so checking both might be excessive
+        if (newTreeIdx === -1 || newDateIdx === -1) {
+            throw new Error("Invalid date for data assembler");
+        }
+
+        this._currentTreeIndex = newTreeIdx;
+        this._currentDateIndex = newDateIdx;
+    }
+
+    /**
+     * Gets the currently active tree
+     * @returns A dated tree
+     */
+    getActiveTree(): DatedTree {
+        return this.datedTrees[this._currentTreeIndex];
+    }
+
+    /**
+     * Pretty-prints all values in the given object with labels and units added
+     * @param data Data from a node or edge
+     * @returns A formatted string presentation of the data value
+     */
+    getTooltip(data: NodeData | EdgeData): string {
+        if (this._currentDateIndex === -1) return "";
+
+        let text = "";
+
+        for (const propName in data) {
+            const [label, unit] = this.getPropertyInfo(propName);
+            const value = this.getPropertyValue(data, propName);
+            const valueString = printTreeValue(value);
+
+            text += `${label}: ${valueString} ${unit}\n`;
+        }
+
+        return text.trimEnd();
+    }
+
+    /**
+     * Gets a property's value at the currently active date
+     * @param data Data from a node or edge
+     * @param property A key for a property
+     * @returns The value, if found
+     */
+    getPropertyValue(
+        data: EdgeData | NodeData,
+        property: string
+    ): number | null {
+        if (this._currentDateIndex === -1) return null;
+
+        const value = data[property]?.[this._currentDateIndex];
+
+        return value ?? null;
+    }
+
+    /**
+     * Gets the label and unit strings for a given property
+     * @param propertyKey The key for the property
+     * @returns The label and unit for the property. If not found, label defaults to "", and unit defaults to "?"
+     */
+    getPropertyInfo(propertyKey: string): [label: string, unit: string] {
+        const [label, unit] = this._propertyToLabelMap.get(propertyKey) ?? [];
+
+        const sanitizedLabel = _.upperFirst(label ?? "");
+        const sanitizedUnit = unit ?? "?";
+
+        return [sanitizedLabel, sanitizedUnit];
+    }
+
+    /**
+     * Returns a property's value to a normalized value between 2 and 100. A value of 0 will be returned as is.
+     * Will also return 0 if no data is found
+     * @param data Data object to take data from
+     * @param property The property key to take from
+     * @returns The normalized value.
+     */
+    normalizeValue(data: EdgeData | NodeData, property: string): number {
+        const value = this.getPropertyValue(data, property) ?? 0;
+        const maxVal = this._propertyMaxVals.get(property);
+
+        // Don't normalize 0 or invalid properties
+        if (!maxVal || value === 0) return 0;
+
+        return d3.scaleLinear().domain([0, maxVal]).range([2, 100])(value);
+    }
+}
+
+function findTreeAndDateIndex(
+    targetDate: string,
+    datedTrees: DatedTree[]
+): [treeIndex: number, dateIndex: number] {
+    // Using standard for loop so we can return early when we find a matching tree
+    for (let treeIdx = 0; treeIdx < datedTrees.length; treeIdx++) {
+        const datedTree = datedTrees[treeIdx];
+        const dateIdx = datedTree.dates.findIndex(
+            (date) => date === targetDate
+        );
+
+        if (dateIdx !== -1) {
+            return [treeIdx, dateIdx];
+        }
+    }
+
+    // No matching entry found
+    return [-1, -1];
+}
+/**
+ * Updates the active date in a data-assembler instance.
+ * @param dataAssembler An instance of DataAssembler
+ * @param targetDate A date string
+ * @returns An error message, if the date was invalid
+ */
+export function useUpdateAssemblerDate(
+    dataAssembler: DataAssembler | null,
+    targetDate: string
+): string | void {
+    const [prevDate, setPrevDate] = React.useState<string | null>(null);
+
+    if (!dataAssembler || targetDate === prevDate) return;
+
+    try {
+        dataAssembler.setActiveDate(targetDate);
+        setPrevDate(targetDate);
+    } catch (error) {
+        return (error as Error).message;
+    }
+}
+
+/**
+ * Initializes a memoized data-assembler instance. Creates a new instance every time data changes, but returns the last valid instance if the new data is invalid.
+ * @param datedTrees A list of dated trees. *Assumes all node labels are unique across all node types*
+ * @param edgeMetadataList Dated metadata for each tree edge
+ * @param nodeMetadataList Dated metadata for each tree node
+ * @returns A data-assembler instance, and an error message (if the instance could not be created)
+ */
+export function useDataAssembler(
+    datedTrees: DatedTree[],
+    edgeMetadataList: EdgeMetadata[],
+    nodeMetadataList: NodeMetadata[]
+): [DataAssembler | null, string] {
+    // Store a status message to track if data was valid
+    let errorMsg = "";
+    // Storing a copy of the last successfully assembled data to render when data becomes invalid
+    const lastValidDataAssembler = React.useRef<DataAssembler | null>(null);
+
+    const dataAssembler = React.useMemo(() => {
+        if (datedTrees.length === 0) return null;
+
+        const assembler = new DataAssembler(
+            datedTrees,
+            edgeMetadataList,
+            nodeMetadataList
+        );
+
+        return assembler;
+    }, [datedTrees, edgeMetadataList, nodeMetadataList]);
+
+    if (dataAssembler === null) {
+        errorMsg = "Invalid data for assembler";
+    } else if (dataAssembler !== lastValidDataAssembler.current) {
+        lastValidDataAssembler.current = dataAssembler;
+    }
+
+    return [lastValidDataAssembler.current, errorMsg];
+}
