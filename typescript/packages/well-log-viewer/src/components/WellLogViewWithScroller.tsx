882434707904f687fbc649c03f5272940fadd315
Havard Bjerke <havard.bjerke@aspentech.com>
2025-08-29 13:24:43 +0200

    refactor: revert WellLogViewWithScroller changes

--- typescript/packages/well-log-viewer/src/components/WellLogViewWithScroller.tsx
+++ typescript/packages/well-log-viewer/src/components/WellLogViewWithScroller.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback, useEffect } from "react";
+import React, { Component } from "react";
 
 import WellLogView from "./WellLogView";
 import type { WellLogViewProps } from "./WellLogView";
@@ -7,63 +7,143 @@ import { _propTypesWellLogView } from "./WellLogView";
 
 import type { WellLogController } from "./WellLogView";
 
-import Scroller from "./Scroller";
 import type { ScrollerRef } from "./Scroller";
+import Scroller from "./Scroller";
 
 export type WellLogViewWithScrollerProps = WellLogViewProps;
 export const argTypesWellLogViewScrollerProp = argTypesWellLogViewProp;
 
-const WellLogViewWithScroller: React.FC<WellLogViewWithScrollerProps> = (
-    props
-) => {
-    const controllerRef = useRef<WellLogController | null>(null);
-    const scrollerRef = useRef<ScrollerRef>(null);
-    const skipScrollNotificationRef = useRef<number>(0);
+class WellLogViewWithScroller extends Component<WellLogViewWithScrollerProps> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public static propTypes: Record<string, any>;
 
-    const calcPosTrack = useCallback((f: number): number => {
-        const controller = controllerRef.current;
+    controller: WellLogController | null;
+    scroller: React.RefObject<ScrollerRef>;
+
+    constructor(props: WellLogViewWithScrollerProps) {
+        super(props);
+
+        this.controller = null;
+        this.scroller = React.createRef<ScrollerRef>();
+
+        this.onCreateController = this.onCreateController.bind(this);
+
+        this.onScrollerScroll = this.onScrollerScroll.bind(this);
+        this.onTrackScroll = this.onTrackScroll.bind(this);
+        this.onTrackSelection = this.onTrackSelection.bind(this);
+
+        this.onContentRescale = this.onContentRescale.bind(this);
+        this.onContentSelection = this.onContentSelection.bind(this);
+    }
+
+    componentDidMount(): void {
+        this.setScrollerPosAndZoom();
+    }
+
+    shouldComponentUpdate(nextProps: WellLogViewWithScrollerProps): boolean {
+        return !Object.is(this.props, nextProps);
+    }
+
+    updateReadoutPanel(): void {
+        const controller = this.controller;
+        if (controller) controller.updateInfo(); // force to update readout panel
+    }
+
+    // callback function from WellLogView
+    onCreateController(controller: WellLogController): void {
+        this.controller = controller;
+        this.props.onCreateController?.(controller);
+    }
+    // callback function from WellLogView
+    onTrackScroll(): void {
+        this.setScrollerPosAndZoom();
+        this.props.onTrackScroll?.();
+    }
+    // callback function from WellLogView
+    onTrackSelection(): void {
+        this.props.onTrackSelection?.();
+    }
+    // callback function from WellLogView
+    onContentRescale(): void {
+        this.setScrollerPosAndZoom();
+
+        this.props.onContentRescale?.();
+    }
+    // callback function from WellLogView
+    onContentSelection(): void {
+        this.props.onContentSelection?.();
+    }
+
+    skipScrollNotification: number = 0;
+    // callback function from Scroller
+    onScrollerScroll(x: number, y: number): void {
+        if (this.skipScrollNotification) {
+            // the notification is self-induced
+            this.skipScrollNotification--;
+            return;
+        }
+
+        const controller = this.controller;
+        if (!controller) return;
+
+        const fContent = this.props.horizontal ? x : y; // fraction
+        controller.scrollContentTo(fContent);
+
+        const posTrack = this.calcPosTrack(this.props.horizontal ? y : x);
+        controller.scrollTrackTo(posTrack);
+    }
+
+    calcPosTrack(f: number): number {
+        const controller = this.controller;
         if (!controller) return 0;
         const posMax = controller.getTrackScrollPosMax();
         const posTrack = f * posMax;
         return Math.round(posTrack);
-    }, []);
+    }
 
-    const getContentPosFraction = useCallback((): number => {
-        const controller = controllerRef.current;
+    getContentPosFraction(): number {
+        const controller = this.controller;
         if (!controller) return 0;
         const baseDomain = controller.getContentBaseDomain();
         const domain = controller.getContentDomain();
         const w = baseDomain[1] - baseDomain[0] - (domain[1] - domain[0]);
         return w ? (domain[0] - baseDomain[0]) / w : 0;
-    }, []);
-
-    const getTrackPosFraction = useCallback((): number => {
-        const controller = controllerRef.current;
+    }
+    getTrackPosFraction(): number {
+        const controller = this.controller;
         if (!controller) return 0;
         return controller.getTrackScrollPosMax()
             ? controller.getTrackScrollPos() / controller.getTrackScrollPosMax()
             : 0.0;
-    }, []);
+    }
 
-    const setScrollerPosAndZoom = useCallback((): void => {
+    setScrollerPosAndZoom(): void {
         let x: number, y: number;
         let xZoom: number, yZoom: number;
-        const scroller = scrollerRef.current;
+        const scroller = this.scroller?.current;
         if (!scroller) return;
-        const controller = controllerRef.current;
+        const controller = this.controller;
         if (!controller) {
             x = y = 0.0;
             xZoom = yZoom = 1.0;
         } else {
             const contentZoom = controller.getContentZoom();
             const trackZoom = controller.getTrackZoom();
-            xZoom = props.horizontal ? contentZoom : trackZoom;
-            yZoom = props.horizontal ? trackZoom : contentZoom;
+            // TODO: Fix this the next time the file is edited.
+            // eslint-disable-next-line react/prop-types
+            xZoom = this.props.horizontal ? contentZoom : trackZoom;
+            // TODO: Fix this the next time the file is edited.
+            // eslint-disable-next-line react/prop-types
+            yZoom = this.props.horizontal ? trackZoom : contentZoom;
 
-            const fContent = getContentPosFraction();
-            const fTrack = getTrackPosFraction();
-            x = props.horizontal ? fContent : fTrack;
-            y = props.horizontal ? fTrack : fContent;
+            const fContent = this.getContentPosFraction();
+            const fTrack = this.getTrackPosFraction();
+            // TODO: Fix this the next time the file is edited.
+            // eslint-disable-next-line react/prop-types
+            x = this.props.horizontal ? fContent : fTrack;
+            // TODO: Fix this the next time the file is edited.
+            // eslint-disable-next-line react/prop-types
+            y = this.props.horizontal ? fTrack : fContent;
         }
 
         scroller.zoom(xZoom, yZoom);
@@ -73,111 +153,56 @@ const WellLogViewWithScroller: React.FC<WellLogViewWithScrollerProps> = (
             // compare with current values from scroller
             const _x = scroller.getScrollX();
             const _y = scroller.getScrollY();
-            const _posTrack = calcPosTrack(props.horizontal ? _y : _x);
-            const posTrack = calcPosTrack(props.horizontal ? y : x);
+            const _posTrack = this.calcPosTrack(
+                // TODO: Fix this the next time the file is edited.
+                // eslint-disable-next-line react/prop-types
+                this.props.horizontal ? _y : _x
+            );
+            // TODO: Fix this the next time the file is edited.
+            // eslint-disable-next-line react/prop-types
+            const posTrack = this.calcPosTrack(this.props.horizontal ? y : x);
             if (posTrack === _posTrack) {
                 shouldUpdateScroller--;
-                if (props.horizontal) {
-                    y = _y;
-                } else {
-                    x = _x;
-                }
+                // TODO: Fix this the next time the file is edited.
+                // eslint-disable-next-line @typescript-eslint/no-unused-expressions, react/prop-types
+                this.props.horizontal ? (y = _y) : (x = _x);
             }
-            const _fContent = props.horizontal ? _x : _y;
-            const fContent = props.horizontal ? x : y;
+            // TODO: Fix this the next time the file is edited.
+            // eslint-disable-next-line react/prop-types
+            const _fContent = this.props.horizontal ? _x : _y;
+            // TODO: Fix this the next time the file is edited.
+            // eslint-disable-next-line react/prop-types
+            const fContent = this.props.horizontal ? x : y;
             if (Math.abs(fContent - _fContent) < 0.001) {
                 shouldUpdateScroller--;
-                if (props.horizontal) {
-                    x = _x;
-                } else {
-                    y = _y;
-                }
+                // TODO: Fix this the next time the file is edited.
+                // eslint-disable-next-line @typescript-eslint/no-unused-expressions, react/prop-types
+                this.props.horizontal ? (x = _x) : (y = _y);
             }
         }
         if (shouldUpdateScroller) {
-            if (scroller.scrollTo(x, y)) skipScrollNotificationRef.current++; // skip self-induced notification
+            if (scroller.scrollTo(x, y)) this.skipScrollNotification++; // skip self-induced notification
         }
-    }, [
-        props.horizontal,
-        getContentPosFraction,
-        getTrackPosFraction,
-        calcPosTrack,
-    ]);
+    }
 
-    // callback function from WellLogView
-    const onCreateController = useCallback(
-        (controller: WellLogController) => {
-            controllerRef.current = controller;
-            props.onCreateController?.(controller);
-        },
-        [props]
-    );
+    render(): JSX.Element {
+        return (
+            <Scroller ref={this.scroller} onScroll={this.onScrollerScroll}>
+                <WellLogView
+                    // copy all props
+                    {...this.props}
+                    // redefine some callbacks
+                    onCreateController={this.onCreateController}
+                    onTrackScroll={this.onTrackScroll}
+                    onTrackSelection={this.onTrackSelection}
+                    onContentRescale={this.onContentRescale}
+                    onContentSelection={this.onContentSelection}
+                />
+            </Scroller>
+        );
+    }
+}
 
-    // callback function from WellLogView
-    const onTrackScroll = useCallback(() => {
-        setScrollerPosAndZoom();
-        props.onTrackScroll?.();
-    }, [setScrollerPosAndZoom, props]);
-
-    // callback function from WellLogView
-    const onTrackSelection = useCallback(() => {
-        props.onTrackSelection?.();
-    }, [props]);
-
-    // callback function from WellLogView
-    const onContentRescale = useCallback(() => {
-        setScrollerPosAndZoom();
-        props.onContentRescale?.();
-    }, [setScrollerPosAndZoom, props]);
-
-    // callback function from WellLogView
-    const onContentSelection = useCallback(() => {
-        props.onContentSelection?.();
-    }, [props]);
-
-    // callback function from Scroller
-    const onScrollerScroll = useCallback(
-        (x: number, y: number) => {
-            if (skipScrollNotificationRef.current) {
-                // the notification is self-induced
-                skipScrollNotificationRef.current--;
-                return;
-            }
-
-            const controller = controllerRef.current;
-            if (!controller) return;
-
-            const fContent = props.horizontal ? x : y; // fraction
-            controller.scrollContentTo(fContent);
-
-            const posTrack = calcPosTrack(props.horizontal ? y : x);
-            controller.scrollTrackTo(posTrack);
-        },
-        [props.horizontal, calcPosTrack]
-    );
-
-    useEffect(() => {
-        setScrollerPosAndZoom();
-    }, [setScrollerPosAndZoom]); // componentDidMount equivalent
-
-    return (
-        <Scroller ref={scrollerRef} onScroll={onScrollerScroll}>
-            <WellLogView
-                // copy all props
-                {...props}
-                // redefine some callbacks
-                onCreateController={onCreateController}
-                onTrackScroll={onTrackScroll}
-                onTrackSelection={onTrackSelection}
-                onContentRescale={onContentRescale}
-                onContentSelection={onContentSelection}
-            />
-        </Scroller>
-    );
-};
-
-// Add propTypes for backward compatibility
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(WellLogViewWithScroller as any).propTypes = _propTypesWellLogView();
+WellLogViewWithScroller.propTypes = _propTypesWellLogView(); // WellLogView.propTypes;
 
 export default WellLogViewWithScroller;
