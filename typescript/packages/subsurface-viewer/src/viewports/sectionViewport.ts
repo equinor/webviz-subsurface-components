8231308559e6eabb629326cc88dc82abb796e3bc
Nils Christian Bonnevie <nils.christian.bonnevie@gmail.com>
2025-10-10 16:04:42 +0200

    code comment fixes

--- typescript/packages/subsurface-viewer/src/viewports/sectionViewport.ts
+++ typescript/packages/subsurface-viewer/src/viewports/sectionViewport.ts
@@ -1,22 +1,8 @@
 import { OrthographicViewport } from "@deck.gl/core";
 import type { OrthographicViewportOptions } from "@deck.gl/core/dist/viewports/orthographic-viewport";
 
-export type Padding = {
-    left?: number;
-    right?: number;
-    top?: number;
-    bottom?: number;
-};
 export class SectionViewport extends OrthographicViewport {
     constructor(props: OrthographicViewportOptions) {
-        const orthographicProps = {
-            ...props,
-            target: props.target as
-                | [number, number, number]
-                | [number, number]
-                | undefined,
-        };
-
-        super(orthographicProps);
+        super(props);
     }
 }
