8229d29bd15e3193dcc31fc38ef9b92a91a0adb4
Nils Christian Bonnevie <nils.christian.bonnevie@gmail.com>
2025-10-10 12:19:59 +0200

    Simplify Section view and viewport to extend OrgographicViiew/Port directly.

--- typescript/packages/subsurface-viewer/src/views/sectionView.ts
+++ typescript/packages/subsurface-viewer/src/views/sectionView.ts
@@ -1,32 +1,15 @@
-import { OrthographicController, View } from "@deck.gl/core";
+import { OrthographicView } from "@deck.gl/core";
 import type {
-    CommonViewProps,
-    CommonViewState,
-} from "@deck.gl/core/dist/views/view";
+    OrthographicViewState,
+    OrthographicViewProps,
+} from "@deck.gl/core";
 import { SectionViewport } from "../viewports/sectionViewport";
 
-export type SectionViewState = {
-    /** The world position at the center of the viewport. Default `[0, 0, 0]`. */
-    target?: [number, number, number] | [number, number];
-    /**  The zoom level of the viewport. `zoom: 0` maps one unit distance to one pixel on screen, and increasing `zoom` by `1` scales the same object to twice as large.
-     *   To apply independent zoom levels to the X and Y axes, supply an array `[zoomX, zoomY]`. Default `0`. */
-    zoom?: number | [number, number];
-    /** The min zoom level of the viewport. Default `-Infinity`. */
-    minZoom?: number;
-    /** The max zoom level of the viewport. Default `Infinity`. */
-    maxZoom?: number;
-} & CommonViewState;
+export type SectionViewState = OrthographicViewState;
 
-export type SectionViewProps = {
-    /** Distance of near clipping plane. Default `0.1`. */
-    near?: number;
-    /** Distance of far clipping plane. Default `1000`. */
-    far?: number;
-    /** Whether to use top-left coordinates (`true`) or bottom-left coordinates (`false`). Default `true`. */
-    flipY?: boolean;
-} & CommonViewProps<SectionViewState>;
+export type SectionViewProps = OrthographicViewProps;
 
-export class SectionView extends View<SectionViewState, SectionViewProps> {
+export class SectionView extends OrthographicView {
     static displayName = "SectionView";
 
     constructor(props: SectionViewProps = {}) {
@@ -35,9 +18,5 @@ export class SectionView extends View<SectionViewState, SectionViewProps> {
 
     getViewportType() {
         return SectionViewport;
-    }
-
-    get ControllerType() {
-        return OrthographicController;
     }
 }
