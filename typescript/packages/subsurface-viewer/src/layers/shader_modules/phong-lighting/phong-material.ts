ee78647f2b6939f3f7150c681c959b8452e89ab2
t0oF <93762994+w1nklr@users.noreply.github.com>
2026-07-08 13:59:48 +0200

    fix: lighting issues (#2805)
    Update local @luma.gl lighting code copy modified to support double side
    lighting.
    The original code changed in @luma.gl 9.3.

--- typescript/packages/subsurface-viewer/src/layers/shader_modules/phong-lighting/phong-material.ts
+++ typescript/packages/subsurface-viewer/src/layers/shader_modules/phong-lighting/phong-material.ts
@@ -2,18 +2,19 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
-// Note: Copied this module from luma.gl.
-// https://github.com/visgl/luma.gl branch 9.1-release
+// Webviz note: Copied this module from luma.gl.
+// https://github.com/visgl/luma.gl branch 9.3-release
+// Modified imports and commented PHONG_WGSL in file "phong-material.ts".
 // Modified function "lighting_getLightColor" in file "phong-shaders-glsl.ts" to get two sided phong lighting.
 
 import type { NumberArray3 } from "@math.gl/types";
-
 import type { ShaderModule } from "@luma.gl/shadertools";
-import { lighting } from "@luma.gl/shadertools";
-
+import { floatColors, lighting } from "@luma.gl/shadertools";
+//import {PHONG_WGSL} from './phong-shaders-wgsl';
 import { PHONG_VS, PHONG_FS } from "./phong-shaders-glsl";
 
 export type PhongMaterialProps = {
+    unlit?: boolean;
     ambient?: number;
     diffuse?: number;
     /** Specularity exponent */
@@ -21,35 +22,36 @@ export type PhongMaterialProps = {
     specularColor?: NumberArray3;
 };
 
+const DEFAULT_SPECULAR_COLOR: NumberArray3 = [38.25, 38.25, 38.25];
+
 /** In Phong shading, the normal vector is linearly interpolated across the surface of the polygon from the polygon's vertex normals. */
 export const phongMaterial: ShaderModule<PhongMaterialProps> = {
     name: "phongMaterial",
-    dependencies: [lighting],
+    firstBindingSlot: 0,
+    bindingLayout: [{ name: "phongMaterial", group: 3 }],
+    dependencies: [lighting, floatColors],
     // Note these are switched between phong and gouraud
+    //source: PHONG_WGSL,
     vs: PHONG_VS,
     fs: PHONG_FS,
     defines: {
         LIGHTING_FRAGMENT: true,
     },
     uniformTypes: {
+        unlit: "i32",
         ambient: "f32",
         diffuse: "f32",
         shininess: "f32",
         specularColor: "vec3<f32>",
     },
     defaultUniforms: {
+        unlit: false,
         ambient: 0.35,
         diffuse: 0.6,
         shininess: 32,
-        specularColor: [0.15, 0.15, 0.15],
+        specularColor: DEFAULT_SPECULAR_COLOR,
     },
     getUniforms(props?: PhongMaterialProps) {
-        const uniforms = { ...props };
-        if (uniforms.specularColor) {
-            uniforms.specularColor = uniforms.specularColor.map(
-                (x) => x / 255
-            ) as NumberArray3;
-        }
-        return { ...phongMaterial.defaultUniforms, ...uniforms };
+        return { ...phongMaterial.defaultUniforms, ...props };
     },
 };
